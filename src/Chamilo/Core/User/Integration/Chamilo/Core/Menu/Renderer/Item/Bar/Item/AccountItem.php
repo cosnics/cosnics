1e41c087caee6bc5ad46518d4b2b1bc7e397ca26
Scaramanga <hans.de.bisschop@ehb.be>
2015-03-20 12:37:52 +0100

    Fix: Cleanup of administrative menu items

--- src/Chamilo/Core/User/Integration/Chamilo/Core/Menu/Renderer/Item/Bar/Item/AccountItem.php
+++ src/Chamilo/Core/User/Integration/Chamilo/Core/Menu/Renderer/Item/Bar/Item/AccountItem.php
@@ -4,9 +4,6 @@ namespace Chamilo\Core\User\Integration\Chamilo\Core\Menu\Renderer\Item\Bar\Item
 use Chamilo\Libraries\File\Redirect;
 use Chamilo\Libraries\Architecture\Application\Application;
 use Chamilo\Core\User\Manager;
-use Chamilo\Libraries\Platform\Translation;
-use Chamilo\Libraries\Format\Theme;
-use Chamilo\Libraries\Architecture\ClassnameUtilities;
 use Chamilo\Core\User\Integration\Chamilo\Core\Menu\Renderer\Item\Bar\MenuItem;
 
 /**
@@ -18,51 +15,6 @@ use Chamilo\Core\User\Integration\Chamilo\Core\Menu\Renderer\Item\Bar\MenuItem;
  */
 class AccountItem extends MenuItem
 {
-
-    /**
-     *
-     * @return string
-     */
-    public function getContent()
-    {
-        $html = array();
-
-        if ($this->get_item()->get_parent() == 0)
-        {
-            $selected = $this->get_item()->is_selected();
-        }
-
-        $html[] = '<a' . ($selected ? ' class="current"' : '') . ' href="' . $this->get_url() . '">';
-
-        $title = $this->get_item()->get_titles()->get_translation(Translation :: get_instance()->get_language());
-
-        if ($this->get_item()->show_icon())
-        {
-            $itemNamespace = ClassnameUtilities :: getInstance()->getNamespaceFromClassname(
-                $this->get_item()->get_type());
-            $itemNamespace = ClassnameUtilities :: getInstance()->getNamespaceParent($itemNamespace, 2);
-            $itemType = ClassnameUtilities :: getInstance()->getClassnameFromNamespace($this->get_item()->get_type());
-            $imagePath = Theme :: getInstance()->getImagePath($itemNamespace, $itemType . ($selected ? 'Selected' : ''));
-
-            $profilePhotoUrl = new Redirect(
-                array(
-                    Application :: PARAM_CONTEXT => \Chamilo\Core\User\Ajax\Manager :: context(),
-                    Application :: PARAM_ACTION => \Chamilo\Core\User\Ajax\Manager :: ACTION_USER_PICTURE,
-                    \Chamilo\Core\User\Manager :: PARAM_USER_USER_ID => $this->get_menu_renderer()->get_user()->get_id()));
-
-            $html[] = '<img class="item-icon item-icon-account" src="' . $profilePhotoUrl->getUrl() . '" title="' .
-                 $title . '" alt="' . $title . '" />';
-        }
-
-        if ($this->get_item()->show_title())
-        {
-            $html[] = $title;
-        }
-
-        $html[] = '</a>';
-
-        return implode(PHP_EOL, $html);
-    }
 
     public function get_url()
     {
