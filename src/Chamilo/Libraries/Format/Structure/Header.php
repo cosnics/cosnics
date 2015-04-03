fb708c47ca9a75427818713b838b13f3aca0d3f9
Scaramanga <hans.de.bisschop@ehb.be>
2015-04-03 11:49:49 +0200

    Some tweaks and fixes for metadata

--- src/Chamilo/Libraries/Format/Structure/Header.php
+++ src/Chamilo/Libraries/Format/Structure/Header.php
@@ -241,7 +241,7 @@ class Header
     {
         $html = array();
 
-        $html[] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+        $html[] = '<!DOCTYPE html>';
         $html[] = '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="' . $this->getLanguageCode() . '" lang="' .
              $this->getLanguageCode() . '">';
         $html[] = '<head>';
