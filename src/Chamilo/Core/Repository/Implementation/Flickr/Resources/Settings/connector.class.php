f4c41c42034f2e49062132e42bbe9fee431c9eb2
magali.gillard <magali.gillard@ehb.be>
2015-04-24 11:37:15 +0200

    fix : external repositories menu (new ImplementationRepositoryItem)

--- src/Chamilo/Core/Repository/Implementation/Flickr/Resources/Settings/connector.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace core\repository\implementation\flickr;
-
-class SettingsConnector
-{
-}
---
+++ src/Chamilo/Core/Repository/Integration/Chamilo/Core/Menu/Storage/DataClass/RepositoryImplementationCategoryItem.php
@@ -0,0 +1,15 @@
+<?php
+namespace Chamilo\Core\Repository\Integration\Chamilo\Core\Menu\Storage\DataClass;
+
+use Chamilo\Core\Menu\Storage\DataClass\CategoryItem;
+
+/**
+ *
+ * @package Chamilo\Core\User\Integration\Chamilo\Core\Menu\Storage\DataClass
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ * @author Eduard Vossen <eduard.vossen@ehb.be>
+ */
+class RepositoryImplementationCategoryItem extends CategoryItem
+{
+}
