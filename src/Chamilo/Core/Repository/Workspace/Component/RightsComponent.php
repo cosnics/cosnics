249779dee16beda4d1f81e4834d4d22c50ddfaf7
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-05-26 12:30:38 +0200

    Feature: Expanding workspace management

---
+++ src/Chamilo/Core/Repository/Workspace/Component/RightsComponent.php
@@ -0,0 +1,19 @@
+<?php
+namespace Chamilo\Core\Repository\Workspace\Component;
+
+use Chamilo\Core\Repository\Workspace\Manager;
+
+/**
+ *
+ * @package Chamilo\Core\Repository\Workspace\Component
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ * @author Eduard Vossen <eduard.vossen@ehb.be>
+ */
+class RightsComponent extends Manager
+{
+
+    public function run()
+    {
+    }
+}
