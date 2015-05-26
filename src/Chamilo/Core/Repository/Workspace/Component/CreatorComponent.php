beb08aff9450a3db569a3e9e3f972a9bffe03325
Scaramanga <hans.de.bisschop@ehb.be>
2015-05-26 12:30:38 +0200

    Feature: Expanding workspace management

--- src/Chamilo/Core/Repository/Workspace/Component/CreatorComponent.php
+++ src/Chamilo/Core/Repository/Workspace/Component/CreatorComponent.php
@@ -2,13 +2,12 @@
 namespace Chamilo\Core\Repository\Workspace\Component;
 
 use Chamilo\Core\Repository\Workspace\Manager;
-use Chamilo\Core\Repository\Workspace\Table\Workspace\WorkspaceTable;
-use Chamilo\Libraries\Format\Table\Interfaces\TableSupport;
-use Chamilo\Libraries\Format\Structure\ActionBarRenderer;
-use Chamilo\Libraries\Format\Structure\ToolbarItem;
 use Chamilo\Libraries\Platform\Translation;
-use Chamilo\Libraries\Format\Theme;
 use Chamilo\Libraries\Utilities\Utilities;
+use Chamilo\Core\Repository\Workspace\Storage\DataClass\Workspace;
+use Chamilo\Core\Repository\Workspace\Form\WorkspaceForm;
+use Chamilo\Core\Repository\Workspace\Repository\WorkspaceRepository;
+use Chamilo\Core\Repository\Workspace\Service\WorkspaceService;
 
 /**
  *
@@ -17,53 +16,48 @@ use Chamilo\Libraries\Utilities\Utilities;
  * @author Magali Gillard <magali.gillard@ehb.be>
  * @author Eduard Vossen <eduard.vossen@ehb.be>
  */
-class BrowserComponent extends Manager implements TableSupport
+class CreatorComponent extends Manager
 {
-
-    /**
-     *
-     * @var \Chamilo\Libraries\Format\Structure\ActionBarRenderer
-     */
-    private $actionBar;
 
     public function run()
     {
-        $table = new WorkspaceTable($this);
+        $form = new WorkspaceForm($this->get_url());
 
-        $html = array();
-
-        $html[] = $this->render_header();
-        $html[] = $this->getActionBar()->as_html();
-        $html[] = $table->as_html();
-        $html[] = $this->render_footer();
-
-        return implode(PHP_EOL, $html);
-    }
-
-    public function get_table_condition($table_class_name)
-    {
-    }
-
-    /**
-     *
-     * @return \Chamilo\Libraries\Format\Structure\ActionBarRenderer
-     */
-    public function getActionBar()
-    {
-        if (! isset($this->actionBar))
+        if ($form->validate())
         {
-            $this->actionBar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
+            try
+            {
+                $values = $form->exportValues();
+                $values[Workspace :: PROPERTY_CREATOR_ID] = $this->get_user_id();
+                $values[Workspace :: PROPERTY_CREATION_DATE] = time();
 
-            $this->actionBar->set_search_url($this->get_url());
+                $workspaceService = new WorkspaceService(new WorkspaceRepository());
+                $success = $workspaceService->createWorkspace($values);
 
-            $this->actionBar->add_common_action(
-                new ToolbarItem(
-                    Translation :: get('Create', null, Utilities :: COMMON_LIBRARIES),
-                    Theme :: getInstance()->getCommonImagePath('Action/Create'),
-                    $this->get_url(array(self :: PARAM_ACTION => self :: ACTION_CREATE)),
-                    ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+                $translation = $success ? 'ObjectCreated' : 'ObjectNotCreated';
+
+                $message = Translation :: get(
+                    $translation,
+                    array('OBJECT' => Translation :: get('Workspace')),
+                    Utilities :: COMMON_LIBRARIES);
+            }
+            catch (\Exception $ex)
+            {
+                $success = false;
+                $message = $ex->getMessage();
+            }
+
+            $this->redirect($message, ! $success, array(self :: PARAM_ACTION => self :: ACTION_RIGHTS));
         }
+        else
+        {
+            $html = array();
 
-        return $this->actionBar;
+            $html[] = $this->render_header();
+            $html[] = $form->toHtml();
+            $html[] = $this->render_footer();
+
+            return implode(PHP_EOL, $html);
+        }
     }
 }
