17b449c4c3cbce628af0209e9858c85611f86693
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-05-27 11:55:19 +0200

    Feature: Workspace rights management

--- src/Chamilo/Core/Repository/Workspace/Component/RightsComponent.php
+++ src/Chamilo/Core/Repository/Workspace/Component/RightsComponent.php
@@ -2,13 +2,7 @@
 namespace Chamilo\Core\Repository\Workspace\Component;
 
 use Chamilo\Core\Repository\Workspace\Manager;
-use Chamilo\Core\Repository\Workspace\Storage\DataClass\Workspace;
-use Chamilo\Core\Repository\Workspace\Storage\DataManager;
-use Chamilo\Libraries\Platform\Session\Request;
-use Chamilo\Libraries\Platform\Translation;
-use Chamilo\Core\Repository\Workspace\Form\RightsForm;
-use Chamilo\Core\Repository\Workspace\Repository\EntityRelationRepository;
-use Chamilo\Core\Repository\Workspace\Service\EntityRelationService;
+use Chamilo\Libraries\Architecture\Application\ApplicationFactory;
 
 /**
  *
@@ -22,48 +16,16 @@ class RightsComponent extends Manager
 
     public function run()
     {
-        $workspaceId = Request :: get(self :: PARAM_WORKSPACE_ID);
-        $workspace = DataManager :: retrieve_by_id(Workspace :: class_name(), $workspaceId);
+        $factory = new ApplicationFactory(
+            $this->getRequest(),
+            \Chamilo\Core\Repository\Workspace\Rights\Manager :: context(),
+            $this->get_user(),
+            $this);
+        return $factory->run();
+    }
 
-        $form = new RightsForm($this->get_url(array(self :: PARAM_WORKSPACE_ID => $workspace->getId())), $workspace);
-
-        if ($form->validate())
-        {
-            try
-            {
-                $values = $form->exportValues();
-
-                $entityRelationService = new EntityRelationService(new EntityRelationRepository());
-                $success = $entityRelationService->setEntityRelations(
-                    $workspace,
-                    $values[RightsForm :: PROPERTY_ACCESS],
-                    (int) $values[RightsForm :: PROPERTY_VIEW],
-                    (int) $values[RightsForm :: PROPERTY_USE],
-                    (int) $values[RightsForm :: PROPERTY_COPY]);
-
-                $translation = $success ? 'RightsSet' : 'RightsNotSet';
-                $message = Translation :: get($translation);
-            }
-            catch (\Exception $ex)
-            {
-                $success = false;
-                $message = $ex->getMessage();
-            }
-
-            $this->redirect(
-                $message,
-                ! $success,
-                array(self :: PARAM_ACTION => self :: ACTION_RIGHTS, self :: PARAM_WORKSPACE_ID => $workspace->getId()));
-        }
-        else
-        {
-            $html = array();
-
-            $html[] = $this->render_header();
-            $html[] = $form->toHtml();
-            $html[] = $this->render_footer();
-
-            return implode(PHP_EOL, $html);
-        }
+    public function get_additional_parameters()
+    {
+        return array(self :: PARAM_WORKSPACE_ID);
     }
 }
