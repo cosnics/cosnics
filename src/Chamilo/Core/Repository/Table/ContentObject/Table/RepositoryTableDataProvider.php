f968cf0d1bde7ff0aca6426e4b7ef4519949a5b4
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-06-02 08:31:58 +0200

    Fix: Small bugfixes

--- src/Chamilo/Core/Repository/Table/ContentObject/Table/RepositoryTableDataProvider.php
+++ src/Chamilo/Core/Repository/Table/ContentObject/Table/RepositoryTableDataProvider.php
@@ -3,8 +3,8 @@ namespace Chamilo\Core\Repository\Table\ContentObject\Table;
 
 // use Chamilo\Core\Repository\Storage\DataManager;
 use Chamilo\Libraries\Format\Table\Extension\DataClassTable\DataClassTableDataProvider;
-use Chamilo\Libraries\Storage\Parameters\DataClassCountParameters;
-use Chamilo\Libraries\Storage\Parameters\DataClassRetrievesParameters;
+// use Chamilo\Libraries\Storage\Parameters\DataClassCountParameters;
+// use Chamilo\Libraries\Storage\Parameters\DataClassRetrievesParameters;
 use Chamilo\Core\Repository\Workspace\Repository\ContentObjectRepository;
 use Chamilo\Core\Repository\Workspace\Service\ContentObjectService;
 use Chamilo\Core\Repository\Filter\Renderer\ConditionFilterRenderer;
@@ -15,8 +15,6 @@ class RepositoryTableDataProvider extends DataClassTableDataProvider
 
     public function retrieve_data($condition, $offset, $count, $orderProperty = null)
     {
-        $parameters = new DataClassRetrievesParameters($condition, $count, $offset, $orderProperty);
-
         $contentObjectService = new ContentObjectService(new ContentObjectRepository());
         return $contentObjectService->getContentObjectsForWorkspace(
             $this->get_component()->get_repository_browser()->getWorkspace(),
@@ -26,14 +24,12 @@ class RepositoryTableDataProvider extends DataClassTableDataProvider
             $count,
             $offset,
             $orderProperty);
-
+        // $parameters = new DataClassRetrievesParameters($condition, $count, $offset, $orderProperty);
         // return DataManager :: retrieve_active_content_objects($this->get_table()->get_type(), $parameters);
     }
 
     public function count_data($condition)
     {
-        $parameters = new DataClassCountParameters($condition);
-
         $contentObjectService = new ContentObjectService(new ContentObjectRepository());
         return $contentObjectService->countContentObjectsForWorkspace(
             $this->get_component()->get_repository_browser()->getWorkspace(),
@@ -41,6 +37,7 @@ class RepositoryTableDataProvider extends DataClassTableDataProvider
                 FilterData :: get_instance($this->get_component()->get_repository_browser()->getWorkspace()),
                 $this->get_component()->get_repository_browser()->getWorkspace()));
 
+        // $parameters = new DataClassCountParameters($condition);
         // return DataManager :: count_active_content_objects($this->get_table()->get_type(), $parameters);
     }
 }
