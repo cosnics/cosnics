1b85af613735218c5efdcbd98f4c7933d2974da3
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-06-02 14:16:41 +0200

    Fix: Repository category impact view Fix: Repository importers Fix: Potential invalid category filter data value

--- src/Chamilo/Core/Repository/Common/Import/Youtube/YoutubeContentObjectImportController.php
+++ src/Chamilo/Core/Repository/Common/Import/Youtube/YoutubeContentObjectImportController.php
@@ -11,6 +11,10 @@ use Chamilo\Libraries\Storage\Query\Condition\AndCondition;
 use Chamilo\Libraries\Storage\Query\Condition\EqualityCondition;
 use Chamilo\Libraries\Storage\Query\Variable\PropertyConditionVariable;
 use Chamilo\Libraries\Storage\Query\Variable\StaticConditionVariable;
+use Chamilo\Core\Repository\Workspace\PersonalWorkspace;
+use Chamilo\Core\Repository\Workspace\Storage\DataClass\Workspace;
+use Chamilo\Core\Repository\Workspace\Service\ContentObjectRelationService;
+use Chamilo\Core\Repository\Workspace\Repository\ContentObjectRelationRepository;
 
 class YoutubeContentObjectImportController extends ContentObjectImportController
 {
@@ -67,9 +71,12 @@ class YoutubeContentObjectImportController extends ContentObjectImportController
                 $youtube->set_title($external_object->get_title());
                 $youtube->set_description($external_object->get_description());
                 $youtube->set_owner_id($this->get_parameters()->get_user());
+                $youtube->set_parent_id($this->determine_parent_id());
 
                 if ($youtube->create())
                 {
+                    $this->process_workspace($youtube);
+
                     \Chamilo\Core\Repository\Instance\Storage\DataClass\SynchronizationData :: quicksave(
                         $youtube,
                         $external_object,
@@ -112,5 +119,37 @@ class YoutubeContentObjectImportController extends ContentObjectImportController
         $youtube_connector_available = $external_repositories->size() == 1;
 
         return $youtube_object_available && $youtube_connector_available;
+    }
+
+    /**
+     *
+     * @return integer
+     */
+    public function determine_parent_id()
+    {
+        if ($this->get_parameters()->getWorkspace() instanceof PersonalWorkspace)
+        {
+            return $this->get_parameters()->get_category();
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    /**
+     *
+     * @param ContentObject $contentObject
+     */
+    public function process_workspace(ContentObject $contentObject)
+    {
+        if ($this->get_parameters()->getWorkspace() instanceof Workspace)
+        {
+            $contentObjectRelationService = new ContentObjectRelationService(new ContentObjectRelationRepository());
+            $contentObjectRelationService->createContentObjectRelation(
+                $this->get_parameters()->getWorkspace()->getId(),
+                $contentObject->getId(),
+                $this->get_parameters()->get_category());
+        }
     }
 }
