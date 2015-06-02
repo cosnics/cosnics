57936032d67024b261fb5d1032b8abb0d0268105
Scaramanga <hans.de.bisschop@ehb.be>
2015-06-02 12:15:45 +0200

    Fix: Breadcrumb based on user when there might not be a user

--- src/Chamilo/Core/Repository/BreadcrumbGenerator.php
+++ src/Chamilo/Core/Repository/BreadcrumbGenerator.php
@@ -5,6 +5,7 @@ use Chamilo\Libraries\Format\Structure\Breadcrumb;
 use Chamilo\Libraries\Architecture\Application\Application;
 use Chamilo\Libraries\File\Redirect;
 use Chamilo\Core\Repository\Workspace\Storage\DataClass\Workspace;
+use Chamilo\Core\User\Storage\DataClass\User;
 
 class BreadcrumbGenerator extends \Chamilo\Libraries\Format\Structure\BreadcrumbGenerator
 {
@@ -14,18 +15,22 @@ class BreadcrumbGenerator extends \Chamilo\Libraries\Format\Structure\Breadcrumb
      */
     protected function generate_package_breadcrumb()
     {
-        $breadcrumb_trail = $this->get_breadcrumb_trail();
-        
-        $parameters = array();
-        $parameters[Application :: PARAM_CONTEXT] = Manager :: context();
-        
-        if ($this->get_component()->getWorkspace() instanceof Workspace)
+        if ($this->get_component()->get_user() instanceof User)
         {
-            $parameters[Manager :: PARAM_WORKSPACE_ID] = $this->get_component()->getWorkspace()->getId();
+            $breadcrumb_trail = $this->get_breadcrumb_trail();
+
+            $parameters = array();
+            $parameters[Application :: PARAM_CONTEXT] = Manager :: context();
+
+            if ($this->get_component()->getWorkspace() instanceof Workspace)
+            {
+                $parameters[Manager :: PARAM_WORKSPACE_ID] = $this->get_component()->getWorkspace()->getId();
+            }
+
+            $redirect = new Redirect($parameters);
+
+            $breadcrumb_trail->add(
+                new Breadcrumb($redirect->getUrl(), $this->get_component()->getWorkspace()->getTitle()));
         }
-        
-        $redirect = new Redirect($parameters);
-        
-        $breadcrumb_trail->add(new Breadcrumb($redirect->getUrl(), $this->get_component()->getWorkspace()->getTitle()));
     }
 }
