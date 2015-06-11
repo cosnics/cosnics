4e02e492618a442442ba50c1744d925dbbddc01f
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-06-11 17:41:09 +0200

    Cleanup: Removed the old metadata folder(s)

--- src/Chamilo/Core/MetadataOld/Attribute/Table/Entity/AttributeEntityTable.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Chamilo\Core\MetadataOld\Attribute\Table\Entity;
-
-use Chamilo\Core\Rights\Editor\Table\LocationEntity\LocationEntityTable;
-
-class AttributeEntityTable extends LocationEntityTable
-{
-}
--- src/Chamilo/Core/MetadataOld/Element/Table/Entity/ElementEntityTableDataProvider.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Chamilo\Core\MetadataOld\Element\Table\Entity;
-
-use Chamilo\Core\Rights\Editor\Table\LocationEntity\LocationEntityTableDataProvider;
-
-class ElementEntityTableDataProvider extends LocationEntityTableDataProvider
-{
-}
