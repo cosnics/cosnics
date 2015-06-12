1e23d9ea0288a8a4a7900fa566965a0c56bc3c99
eduardvossen <eduard.vossen@ehb.be>
2015-06-12 13:56:52 +0200

    Merge

--- src/Chamilo/Configuration/Configuration.php
+++ src/Chamilo/Configuration/Configuration.php
@@ -418,6 +418,9 @@ class Configuration
 
         $this->set(array('Chamilo\Configuration', 'general', 'hashing_algorithm'), 'sha1');
         $this->set(array('Chamilo\Configuration', 'debug', 'show_errors'), false);
+        $this->set(
+            array('Chamilo\Configuration', 'repository', 'storage_path'),
+            Path :: getInstance()->getStoragePath('repository'));
     }
 
     /**
