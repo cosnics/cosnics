656c402c3b1b761c8ed33bc72b1c3d55617fa5bc
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-06-15 12:25:29 +0200

    Fix: Invalid link for duplicate webpages

--- src/Chamilo/Core/Repository/Common/Import/Webpage/WebpageContentObjectImportController.php
+++ src/Chamilo/Core/Repository/Common/Import/Webpage/WebpageContentObjectImportController.php
@@ -124,6 +124,7 @@ class WebpageContentObjectImportController extends ContentObjectImportController
 
                     $redirect = new Redirect(
                         array(
+                            Application :: PARAM_CONTEXT => Manager :: package(),
                             Application :: PARAM_ACTION => Manager :: ACTION_VIEW_CONTENT_OBJECTS,
                             Manager :: PARAM_CONTENT_OBJECT_ID => $content_object->get_id()));
 
