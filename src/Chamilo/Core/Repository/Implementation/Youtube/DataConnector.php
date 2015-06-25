825b699f03fbbed49e42f003ebc01f96404a143d
magali.gillard <magali.gillard@ehb.be>
2015-06-25 09:55:02 +0200

    youtube

--- src/Chamilo/Core/Repository/Implementation/Youtube/DataConnector.php
+++ src/Chamilo/Core/Repository/Implementation/Youtube/DataConnector.php
@@ -140,7 +140,7 @@ class DataConnector extends \Chamilo\Core\Repository\External\DataConnector
 
     public function retrieve_categories()
     {
-        $categories = $this->youtube->videoCategories->listVideoCategories('snippet', 'id');
+        $categories = $this->youtube->videoCategories->listVideoCategories('id,snippet', array('id' => 'test'));
 var_dump($categories);
         return $categories;
     }
