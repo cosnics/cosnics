e1072a25f32747adf7c8fa839ef9a9276c48b710
magali.gillard <magali.gillard@ehb.be>
2015-06-26 12:04:42 +0200

    youtube

--- src/Chamilo/Core/Repository/Implementation/Youtube/Form/ExternalObjectForm.php
+++ src/Chamilo/Core/Repository/Implementation/Youtube/Form/ExternalObjectForm.php
@@ -182,7 +182,7 @@ class ExternalObjectForm extends FormValidator
 
     public function upload_video()
     {
-        var_dump($_FILES[self :: FILE]);
+        var_dump($_FILES['file']);
         $values = $this->exportValues();
         $connector = $this->application->get_external_repository_manager_connector();
         return $connector->upload_video($values);
