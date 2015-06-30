8602c415ef2d4497d0b7a24ac44e15deddd65d02
Scaramanga <hans.de.bisschop@ehb.be>
2015-06-30 10:32:30 +0200

    Fix: An incorrect CSS stylesheet path

--- src/Chamilo/Libraries/Ajax/Component/CkeditorCssComponent.php
+++ src/Chamilo/Libraries/Ajax/Component/CkeditorCssComponent.php
@@ -35,7 +35,7 @@ class CkeditorCssComponent extends \Chamilo\Libraries\Ajax\Manager implements No
 
         foreach ($contentObjectTypes as $contentObjectType)
         {
-            $relativeEditorPath = 'HtmlEditor/Ckeditor/' . $theme . '.css';
+            $relativeEditorPath = 'HtmlEditor/Ckeditor/' . $theme . '/Stylesheet.css';
             $namespace = $classnameUtilities->getNamespaceFromClassname($contentObjectType);
             $namespace = $classnameUtilities->getNamespaceParent($namespace, 2);
 
