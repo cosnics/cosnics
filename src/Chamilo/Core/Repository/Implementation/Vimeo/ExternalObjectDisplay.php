bd64c5d70d3f178057e6e79aff8ecb753cf3da74
magali.gillard <magali.gillard@ehb.be>
2015-07-03 14:42:56 +0200

    Vimeo implementation

--- src/Chamilo/Core/Repository/Implementation/Vimeo/ExternalObjectDisplay.php
+++ src/Chamilo/Core/Repository/Implementation/Vimeo/ExternalObjectDisplay.php
@@ -18,7 +18,7 @@ class ExternalObjectDisplay extends StreamingMediaExternalObjectDisplay
     public function get_display_properties()
     {
         $properties = parent :: get_display_properties();
-        $properties[Translation :: get('Tags')] = $this->get_object()->get_tags();
+        $properties[Translation :: get('Tags')] = $this->get_object()->get_tags_string();
 
         return $properties;
     }
