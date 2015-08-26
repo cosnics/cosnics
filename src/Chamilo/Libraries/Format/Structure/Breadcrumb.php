f247796715f23f89687f96d032f80aacf27b041e
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-08-26 15:39:12 +0200

    Fix: Menu bar icons resized

--- src/Chamilo/Libraries/Format/Structure/Breadcrumb.php
+++ src/Chamilo/Libraries/Format/Structure/Breadcrumb.php
@@ -2,40 +2,94 @@
 namespace Chamilo\Libraries\Format\Structure;
 
 /**
- * $Id: breadcrumb.class.php 128 2009-11-09 13:13:20Z vanpouckesven $
- * 
- * @package common.html
+ *
+ * @package Chamilo\Libraries\Format\Structure
  */
 class Breadcrumb
 {
 
+    /**
+     *
+     * @var string
+     */
     private $url;
 
+    /**
+     *
+     * @var string
+     */
     private $name;
 
-    public function __construct($url, $name)
+    /**
+     *
+     * @var string
+     */
+    private $image;
+
+    /**
+     *
+     * @param string $url
+     * @param string $name
+     * @param string $image
+     */
+    public function __construct($url, $name, $image)
     {
         $this->url = $url;
         $this->name = $name;
+        $this->image = $image;
     }
 
+    /**
+     *
+     * @return string
+     */
     public function get_url()
     {
         return $this->url;
     }
 
+    /**
+     *
+     * @return string
+     */
     public function get_name()
     {
         return $this->name;
     }
 
+    /**
+     *
+     * @param string $url
+     */
     public function set_url($url)
     {
         $this->url = $url;
     }
 
+    /**
+     *
+     * @param string $name
+     */
     public function set_name($name)
     {
         $this->name = $name;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     *
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
     }
 }
