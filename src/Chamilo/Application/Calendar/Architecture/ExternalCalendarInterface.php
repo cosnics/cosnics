989cfd908c36324e0b3287efb0a96c570ee68086
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-09-01 10:47:58 +0200

    Fix: Calendar flexibility and errors

---
+++ src/Chamilo/Application/Calendar/Architecture/ExternalCalendarInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace Chamilo\Application\Calendar\Architecture;
+
+/**
+ *
+ * @package Chamilo\Application\Calendar\Architecture
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ * @author Eduard Vossen <eduard.vossen@ehb.be>
+ */
+interface ExternalCalendarInterface
+{
+}
