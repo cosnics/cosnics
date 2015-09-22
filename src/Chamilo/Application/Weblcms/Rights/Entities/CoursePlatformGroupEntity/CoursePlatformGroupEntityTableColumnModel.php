0cafa993024a0e2b5947c4d7e2559d47b7f208fe
magali.gillard <magali.gillard@ehb.be>
2015-09-22 15:26:01 +0200

    weblcms rights bug fixed

--- src/Chamilo/Application/Weblcms/Rights/Entities/CoursePlatformGroupEntity/CoursePlatformGroupEntityTableColumnModel.php
+++ src/Chamilo/Application/Weblcms/Rights/Entities/CoursePlatformGroupEntity/CoursePlatformGroupEntityTableColumnModel.php
@@ -2,7 +2,17 @@
 namespace Chamilo\Application\Weblcms\Rights\Entities\CoursePlatformGroupEntity;
 
 use Chamilo\Core\Rights\Editor\Table\LocationEntity\LocationEntityTableColumnModel;
+use Chamilo\Core\Group\Storage\DataClass\Group;
+use Chamilo\Libraries\Format\Table\Column\DataClassPropertyTableColumn;
 
 class CoursePlatformGroupEntityTableColumnModel extends LocationEntityTableColumnModel
 {
+
+    public function initialize_columns()
+    {
+        $this->add_column(new DataClassPropertyTableColumn(Group::class_name(), Group :: PROPERTY_NAME));
+        $this->add_column(new DataClassPropertyTableColumn(Group::class_name(), Group :: PROPERTY_CODE));
+         parent :: initialize_columns();
+
+    }
 }
