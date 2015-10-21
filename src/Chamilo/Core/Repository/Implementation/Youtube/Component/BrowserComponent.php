769153e0de153de1350e12270821073f1728b863
magali.gillard <magali.gillard@ehb.be>
2015-10-21 12:08:50 +0200

    youtube implementation fixed

--- src/Chamilo/Core/Repository/Implementation/Youtube/Component/BrowserComponent.php
+++ src/Chamilo/Core/Repository/Implementation/Youtube/Component/BrowserComponent.php
@@ -3,19 +3,7 @@ namespace Chamilo\Core\Repository\Implementation\Youtube\Component;
 
 use Chamilo\Core\Repository\Implementation\Youtube\Manager;
 use Chamilo\Libraries\Architecture\Interfaces\DelegateComponent;
-use Chamilo\Libraries\Platform\Session\Request;
 
 class BrowserComponent extends Manager implements DelegateComponent
 {
-
-    public function run()
-    {
-        if (Request :: get(Manager :: PARAM_FEED_TYPE) == Manager :: FEED_STANDARD_TYPE)
-        {
-
-            $this->set_parameter(Manager :: PARAM_FEED_IDENTIFIER, Request :: get(Manager :: PARAM_FEED_IDENTIFIER));
-        }
-
-        return parent :: run();
-    }
 }
