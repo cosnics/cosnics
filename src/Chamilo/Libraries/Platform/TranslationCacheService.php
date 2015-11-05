f8a701e27d9c7c1db98c9d4f94dcbfc07c57ed01
Scaramanga <hans.de.bisschop@ehb.be>
2015-11-05 09:39:44 +0100

    Feature: Started work on a TranslationCacheService

---
+++ src/Chamilo/Libraries/Platform/TranslationCacheService.php
@@ -0,0 +1,52 @@
+<?php
+namespace Chamilo\Libraries\Platform;
+
+use Chamilo\Libraries\Cache\Doctrine\Service\DoctrinePhpFileCacheService;
+use Chamilo\Configuration\Configuration;
+
+/**
+ *
+ * @package Chamilo\Libraries\Platform
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ * @author Eduard Vossen <eduard.vossen@ehb.be>
+ */
+class TranslationCacheService extends DoctrinePhpFileCacheService
+{
+
+    /**
+     *
+     * @see \Chamilo\Libraries\Cache\Doctrine\DoctrineCacheService::getCacheIdentifiers()
+     */
+    public function getCacheIdentifiers()
+    {
+        return array_keys(Configuration :: get_instance()->getLanguages());
+    }
+
+    /**
+     *
+     * @see \Chamilo\Libraries\Cache\Doctrine\DoctrineCacheService::getCachePathNamespace()
+     */
+    public function getCachePathNamespace()
+    {
+        return __NAMESPACE__ . '\Translation';
+    }
+
+    /**
+     *
+     * @see \Chamilo\Libraries\Cache\CacheServiceInterface::fillCache()
+     */
+    public function fillCache()
+    {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     *
+     * @see \Chamilo\Libraries\Cache\CacheServiceInterface::fillCacheForIdentifier()
+     */
+    public function fillCacheForIdentifier($identifier)
+    {
+        // TODO Auto-generated method stub
+    }
+}
