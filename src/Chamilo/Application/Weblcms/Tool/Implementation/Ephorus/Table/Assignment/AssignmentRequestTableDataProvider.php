a7f58371655ca2b7801a39b87398a565f1b4ed2a
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-11-12 15:48:58 +0100

    Fix: Ephorus plagiarism tool fixes

--- src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Table/Assignment/AssignmentRequestTableDataProvider.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Table/Assignment/AssignmentRequestTableDataProvider.php
@@ -3,26 +3,28 @@ namespace Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Table\Assignme
 
 use Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Storage\DataClass\Request;
 use Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Storage\DataManager;
+use Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Storage\DataManager\Implementation\DoctrineExtension;
 use Chamilo\Libraries\Format\Table\Extension\DataClassTable\DataClassTableDataProvider;
-use Chamilo\Libraries\Storage\Parameters\DataClassCountParameters;
 use Chamilo\Libraries\Storage\Parameters\DataClassRetrievesParameters;
 use Chamilo\Libraries\Storage\Query\OrderBy;
 use Chamilo\Libraries\Storage\Query\Variable\PropertyConditionVariable;
 
 /**
  * Data provider for ephorus requests browser table.
- * 
+ *
  * @author Tom Goethals - Hogeschool Gent
  * @author Sven Vanpoucke - Hogeschool Gent
  */
 class AssignmentRequestTableDataProvider extends DataClassTableDataProvider
 {
 
+    private $extension;
+
     /**
      * Gets the objects to display in the table.
      * For now, objects are composed in the code itself from several source
      * objects.
-     * 
+     *
      * @param $offset
      * @param $count
      * @param null $order_property
@@ -36,18 +38,28 @@ class AssignmentRequestTableDataProvider extends DataClassTableDataProvider
             $order_property = new OrderBy(
                 new PropertyConditionVariable(Request :: class_name(), Request :: PROPERTY_REQUEST_TIME));
         }
-        
-        return DataManager :: retrieve_results_by_assignment(
+
+        return $this->getExtension()->retrieve_results_by_assignment(
             new DataClassRetrievesParameters($condition, $count, $offset, $order_property));
     }
 
     /**
      * Returns the count of the objects
-     * 
+     *
      * @return int
      */
     public function count_data($condition)
     {
-        return DataManager :: count_results_content_objects_by_assignment(new DataClassCountParameters($condition));
+        return $this->getExtension()->count_results_content_objects_by_assignment($condition);
+    }
+
+    public function getExtension()
+    {
+        if (! isset($this->extension))
+        {
+            $this->extension = new DoctrineExtension(DataManager :: get_instance());
+        }
+
+        return $this->extension;
     }
 }
