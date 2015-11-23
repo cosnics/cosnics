19d3748eac725503e975840deae6d736d5aeff65
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2015-11-23 09:34:24 +0100

    Fix: Incorrect classname composition

--- src/Chamilo/Core/Repository/Filter/Renderer/ConditionFilterRenderer.php
+++ src/Chamilo/Core/Repository/Filter/Renderer/ConditionFilterRenderer.php
@@ -35,7 +35,7 @@ class ConditionFilterRenderer extends FilterRenderer
     {
         $filter_data = $this->get_filter_data();
         $conditions = array();
-
+        
         // Text
         if ($filter_data->has_filter_property(FilterData :: FILTER_TEXT))
         {
@@ -43,65 +43,64 @@ class ConditionFilterRenderer extends FilterRenderer
             {
                 $template_registration = \Chamilo\Core\Repository\Configuration :: registration_by_id(
                     $filter_data->get_filter_property(FilterData :: FILTER_TYPE));
-                $class_name = $template_registration->get_content_object_type() . '\\' . ClassnameUtilities :: getInstance()->getPackageNameFromNamespace(
-                    $template_registration->get_content_object_type(),
-                    true);
+                $class_name = $template_registration->get_content_object_type() . '\Storage\DataClass\\' . ClassnameUtilities :: getInstance()->getPackageNameFromNamespace(
+                    $template_registration->get_content_object_type());
             }
             else
             {
                 $class_name = ContentObject :: class_name();
             }
-
+            
             $text = $filter_data->get_filter_property(FilterData :: FILTER_TEXT);
             $searchable_property_names = $class_name :: get_searchable_property_names();
-
+            
             $text_conditions = array();
             $text_conditions[] = new PatternMatchCondition(
-                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_TITLE),
+                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_TITLE), 
                 '*' . $text . '*');
             $text_conditions[] = new PatternMatchCondition(
-                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_DESCRIPTION),
+                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_DESCRIPTION), 
                 '*' . $text . '*');
-
+            
             foreach ($searchable_property_names as $searchable_property_name)
             {
                 $text_conditions[] = new PatternMatchCondition(
-                    new PropertyConditionVariable($class_name, $searchable_property_name),
+                    new PropertyConditionVariable($class_name, $searchable_property_name), 
                     '*' . $text . '*');
             }
-
+            
             $words = explode(' ', $text);
             if (count($words) > 1)
             {
                 foreach ($words as $word)
                 {
                     $text_conditions[] = new PatternMatchCondition(
-                        new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_TITLE),
+                        new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_TITLE), 
                         '*' . $word . '*');
                     $text_conditions[] = new PatternMatchCondition(
                         new PropertyConditionVariable(
-                            ContentObject :: class_name(),
-                            ContentObject :: PROPERTY_DESCRIPTION),
+                            ContentObject :: class_name(), 
+                            ContentObject :: PROPERTY_DESCRIPTION), 
                         '*' . $word . '*');
-
+                    
                     foreach ($searchable_property_names as $searchable_property_name)
                     {
                         $text_conditions[] = new PatternMatchCondition(
-                            new PropertyConditionVariable($class_name, $searchable_property_name),
+                            new PropertyConditionVariable($class_name, $searchable_property_name), 
                             '*' . $word . '*');
                     }
                 }
             }
             $conditions[] = new OrCondition($text_conditions);
         }
-
+        
         // Category id
         $category_id = $filter_data->get_filter_property(FilterData :: FILTER_CATEGORY);
-
+        
         if (isset($category_id) && $category_id >= 0)
         {
             $recursive = (boolean) $filter_data->get_filter_property(FilterData :: FILTER_CATEGORY_RECURSIVE);
-
+            
             if ($recursive)
             {
                 if ($category_id == 0)
@@ -111,26 +110,26 @@ class ConditionFilterRenderer extends FilterRenderer
                 else
                 {
                     $category = DataManager :: retrieve_by_id(RepositoryCategory :: class_name(), $category_id);
-
+                    
                     if ($category instanceof RepositoryCategory)
                     {
                         $category_ids = $category->get_children_ids();
                         $category_ids[] = $category_id;
-
+                        
                         if ($this->get_workspace() instanceof PersonalWorkspace)
                         {
                             $conditions[] = new InCondition(
                                 new PropertyConditionVariable(
-                                    ContentObject :: class_name(),
-                                    ContentObject :: PROPERTY_PARENT_ID),
+                                    ContentObject :: class_name(), 
+                                    ContentObject :: PROPERTY_PARENT_ID), 
                                 $category_ids);
                         }
                         else
                         {
                             $conditions[] = new InCondition(
                                 new PropertyConditionVariable(
-                                    WorkspaceContentObjectRelation :: class_name(),
-                                    WorkspaceContentObjectRelation :: PROPERTY_CATEGORY_ID),
+                                    WorkspaceContentObjectRelation :: class_name(), 
+                                    WorkspaceContentObjectRelation :: PROPERTY_CATEGORY_ID), 
                                 $category_ids);
                         }
                     }
@@ -148,39 +147,39 @@ class ConditionFilterRenderer extends FilterRenderer
                     {
                         $conditions[] = new EqualityCondition(
                             new PropertyConditionVariable(
-                                ContentObject :: class_name(),
-                                ContentObject :: PROPERTY_PARENT_ID),
+                                ContentObject :: class_name(), 
+                                ContentObject :: PROPERTY_PARENT_ID), 
                             new StaticConditionVariable($category_id));
                     }
                     else
                     {
                         $conditions[] = new EqualityCondition(
                             new PropertyConditionVariable(
-                                WorkspaceContentObjectRelation :: class_name(),
-                                WorkspaceContentObjectRelation :: PROPERTY_CATEGORY_ID),
+                                WorkspaceContentObjectRelation :: class_name(), 
+                                WorkspaceContentObjectRelation :: PROPERTY_CATEGORY_ID), 
                             new StaticConditionVariable($category_id));
                     }
                 }
                 else
                 {
                     $category = DataManager :: retrieve_by_id(RepositoryCategory :: class_name(), $category_id);
-
+                    
                     if ($category instanceof RepositoryCategory || $category == 0)
                     {
                         if ($this->get_workspace() instanceof PersonalWorkspace)
                         {
                             $conditions[] = new EqualityCondition(
                                 new PropertyConditionVariable(
-                                    ContentObject :: class_name(),
-                                    ContentObject :: PROPERTY_PARENT_ID),
+                                    ContentObject :: class_name(), 
+                                    ContentObject :: PROPERTY_PARENT_ID), 
                                 new StaticConditionVariable($category_id));
                         }
                         else
                         {
                             $conditions[] = new EqualityCondition(
                                 new PropertyConditionVariable(
-                                    WorkspaceContentObjectRelation :: class_name(),
-                                    WorkspaceContentObjectRelation :: PROPERTY_CATEGORY_ID),
+                                    WorkspaceContentObjectRelation :: class_name(), 
+                                    WorkspaceContentObjectRelation :: PROPERTY_CATEGORY_ID), 
                                 new StaticConditionVariable($category_id));
                         }
                     }
@@ -191,18 +190,18 @@ class ConditionFilterRenderer extends FilterRenderer
                 }
             }
         }
-
+        
         // Type
         if ($filter_data->has_filter_property(FilterData :: FILTER_TYPE))
         {
             $type = $filter_data->get_filter_property(FilterData :: FILTER_TYPE);
-
+            
             // Category
             if (! is_numeric($type) && ! empty($type))
             {
                 $typeSelectorFactory = new TypeSelectorFactory(DataManager :: get_registered_types());
                 $type_selector = $typeSelectorFactory->getTypeSelector();
-
+                
                 try
                 {
                     $types = $type_selector->get_category_by_type($type)->get_unique_content_object_template_ids();
@@ -211,11 +210,11 @@ class ConditionFilterRenderer extends FilterRenderer
                 {
                     $types = array();
                 }
-
+                
                 $conditions[] = new InCondition(
                     new PropertyConditionVariable(
-                        ContentObject :: class_name(),
-                        ContentObject :: PROPERTY_TEMPLATE_REGISTRATION_ID),
+                        ContentObject :: class_name(), 
+                        ContentObject :: PROPERTY_TEMPLATE_REGISTRATION_ID), 
                     $types);
             }
             // Template id
@@ -223,49 +222,49 @@ class ConditionFilterRenderer extends FilterRenderer
             {
                 $conditions[] = new EqualityCondition(
                     new PropertyConditionVariable(
-                        ContentObject :: class_name(),
-                        ContentObject :: PROPERTY_TEMPLATE_REGISTRATION_ID),
+                        ContentObject :: class_name(), 
+                        ContentObject :: PROPERTY_TEMPLATE_REGISTRATION_ID), 
                     new StaticConditionVariable($type));
             }
         }
-
+        
         // User view id
         if ($filter_data->has_filter_property(FilterData :: FILTER_USER_VIEW))
         {
             $user_view_rel_content_objects = DataManager :: retrieves(
-                UserViewRelContentObject :: class_name(),
+                UserViewRelContentObject :: class_name(), 
                 new EqualityCondition(
                     new PropertyConditionVariable(
-                        UserViewRelContentObject :: class_name(),
-                        UserViewRelContentObject :: PROPERTY_USER_VIEW_ID),
+                        UserViewRelContentObject :: class_name(), 
+                        UserViewRelContentObject :: PROPERTY_USER_VIEW_ID), 
                     new StaticConditionVariable($filter_data->get_filter_property(FilterData :: FILTER_USER_VIEW))));
-
+            
             while ($user_view_rel_content_object = $user_view_rel_content_objects->next_result())
             {
                 $visible_template_ids[] = $user_view_rel_content_object->get_content_object_template_id();
             }
-
+            
             if (count($visible_template_ids) > 0)
             {
                 $conditions[] = new InCondition(
                     new PropertyConditionVariable(
-                        ContentObject :: class_name(),
-                        ContentObject :: PROPERTY_TEMPLATE_REGISTRATION_ID),
+                        ContentObject :: class_name(), 
+                        ContentObject :: PROPERTY_TEMPLATE_REGISTRATION_ID), 
                     $visible_template_ids);
             }
         }
-
+        
         // Creation date
         if ($filter_data->has_date(FilterData :: FILTER_CREATION_DATE))
         {
             $creation_date_conditions = array();
             $creation_date_conditions[] = new InequalityCondition(
-                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_CREATION_DATE),
-                InequalityCondition :: GREATER_THAN_OR_EQUAL,
+                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_CREATION_DATE), 
+                InequalityCondition :: GREATER_THAN_OR_EQUAL, 
                 new StaticConditionVariable(strtotime($filter_data->get_creation_date(FilterData :: FILTER_FROM_DATE))));
             $creation_date_conditions[] = new InequalityCondition(
-                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_CREATION_DATE),
-                InequalityCondition :: LESS_THAN_OR_EQUAL,
+                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_CREATION_DATE), 
+                InequalityCondition :: LESS_THAN_OR_EQUAL, 
                 new StaticConditionVariable(strtotime($filter_data->get_creation_date(FilterData :: FILTER_TO_DATE))));
             $conditions[] = new AndCondition($creation_date_conditions);
         }
@@ -274,32 +273,32 @@ class ConditionFilterRenderer extends FilterRenderer
             if ($filter_data->get_creation_date(FilterData :: FILTER_FROM_DATE))
             {
                 $conditions[] = new InequalityCondition(
-                    new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_CREATION_DATE),
-                    InequalityCondition :: GREATER_THAN_OR_EQUAL,
+                    new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_CREATION_DATE), 
+                    InequalityCondition :: GREATER_THAN_OR_EQUAL, 
                     new StaticConditionVariable(
                         strtotime($filter_data->get_creation_date(FilterData :: FILTER_FROM_DATE))));
             }
             elseif ($filter_data->get_creation_date(FilterData :: FILTER_TO_DATE))
             {
                 $conditions[] = new InequalityCondition(
-                    new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_CREATION_DATE),
-                    InequalityCondition :: LESS_THAN_OR_EQUAL,
+                    new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_CREATION_DATE), 
+                    InequalityCondition :: LESS_THAN_OR_EQUAL, 
                     new StaticConditionVariable(strtotime($filter_data->get_creation_date(FilterData :: FILTER_TO_DATE))));
             }
         }
-
+        
         // Modification date
         if ($filter_data->has_date(FilterData :: FILTER_MODIFICATION_DATE))
         {
             $modification_date_conditions = array();
             $modification_date_conditions[] = new InequalityCondition(
-                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_MODIFICATION_DATE),
-                InequalityCondition :: GREATER_THAN_OR_EQUAL,
+                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_MODIFICATION_DATE), 
+                InequalityCondition :: GREATER_THAN_OR_EQUAL, 
                 new StaticConditionVariable(
                     strtotime($filter_data->get_modification_date(FilterData :: FILTER_FROM_DATE))));
             $modification_date_conditions[] = new InequalityCondition(
-                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_MODIFICATION_DATE),
-                InequalityCondition :: LESS_THAN_OR_EQUAL,
+                new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_MODIFICATION_DATE), 
+                InequalityCondition :: LESS_THAN_OR_EQUAL, 
                 new StaticConditionVariable(strtotime($filter_data->get_modification_date(FilterData :: FILTER_TO_DATE))));
             $conditions[] = new AndCondition($modification_date_conditions);
         }
@@ -309,9 +308,9 @@ class ConditionFilterRenderer extends FilterRenderer
             {
                 $conditions[] = new InequalityCondition(
                     new PropertyConditionVariable(
-                        ContentObject :: class_name(),
-                        ContentObject :: PROPERTY_MODIFICATION_DATE),
-                    InequalityCondition :: GREATER_THAN_OR_EQUAL,
+                        ContentObject :: class_name(), 
+                        ContentObject :: PROPERTY_MODIFICATION_DATE), 
+                    InequalityCondition :: GREATER_THAN_OR_EQUAL, 
                     new StaticConditionVariable(
                         strtotime($filter_data->get_modification_date(FilterData :: FILTER_FROM_DATE))));
             }
@@ -319,14 +318,14 @@ class ConditionFilterRenderer extends FilterRenderer
             {
                 $conditions[] = new InequalityCondition(
                     new PropertyConditionVariable(
-                        ContentObject :: class_name(),
-                        ContentObject :: PROPERTY_MODIFICATION_DATE),
-                    InequalityCondition :: LESS_THAN_OR_EQUAL,
+                        ContentObject :: class_name(), 
+                        ContentObject :: PROPERTY_MODIFICATION_DATE), 
+                    InequalityCondition :: LESS_THAN_OR_EQUAL, 
                     new StaticConditionVariable(
                         strtotime($filter_data->get_modification_date(FilterData :: FILTER_TO_DATE))));
             }
         }
-
+        
         if (count($conditions) > 0)
         {
             return new AndCondition($conditions);
