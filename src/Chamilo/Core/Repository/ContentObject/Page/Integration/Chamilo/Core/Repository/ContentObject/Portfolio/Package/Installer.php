661f496ff6ca76ab2a35fd09425118cb0dd76430
magali.gillard <magali.gillard@ehb.be>
2015-12-14 15:09:04 +0100

    refactoring : login

--- src/Chamilo/Core/Repository/ContentObject/Page/Integration/Chamilo/Core/Repository/ContentObject/Portfolio/Package/Installer.php
+++ src/Chamilo/Core/Repository/ContentObject/Page/Integration/Chamilo/Core/Repository/ContentObject/Portfolio/Package/Installer.php
@@ -1,5 +1,5 @@
 <?php
-namespace Chamilo\Core\Repository\ContentObject\Page\Integration\Chamilo\Core\Repository\ContentObject\FrequentlyAskedQuestions\Package;
+namespace Chamilo\Core\Repository\ContentObject\Page\Integration\Chamilo\Core\Repository\ContentObject\Portfolio\Package;
 
 class Installer extends \Chamilo\Configuration\Package\Action\Installer
 {
