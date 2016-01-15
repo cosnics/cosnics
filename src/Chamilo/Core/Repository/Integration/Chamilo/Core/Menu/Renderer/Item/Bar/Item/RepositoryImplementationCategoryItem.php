6a9179185761a06a5a3e127b35514f300637fb3a
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-01-15 15:06:46 +0100

    Icon Bugfixes for External Repository Instances

--- src/Chamilo/Core/Repository/Integration/Chamilo/Core/Menu/Renderer/Item/Bar/Item/RepositoryImplementationCategoryItem.php
+++ src/Chamilo/Core/Repository/Integration/Chamilo/Core/Menu/Renderer/Item/Bar/Item/RepositoryImplementationCategoryItem.php
@@ -2,6 +2,7 @@
 namespace Chamilo\Core\Repository\Integration\Chamilo\Core\Menu\Renderer\Item\Bar\Item;
 
 use Chamilo\Core\Menu\Renderer\Item\Bar\Item\CategoryItem;
+use Chamilo\Core\Menu\Storage\DataClass\Item;
 use Chamilo\Core\Repository\Instance\Storage\DataClass\Instance;
 use Chamilo\Libraries\Format\Theme;
 use Chamilo\Libraries\Platform\Translation;
@@ -28,9 +29,10 @@ class RepositoryImplementationCategoryItem extends CategoryItem
     {
         $html = array();
         $sub_html = array();
-        $instances = \Chamilo\Core\Repository\Instance\Storage\DataManager :: retrieves(
-            Instance :: class_name(),
-            new DataClassRetrievesParameters());
+        $instances = \Chamilo\Core\Repository\Instance\Storage\DataManager:: retrieves(
+            Instance:: class_name(),
+            new DataClassRetrievesParameters()
+        );
 
         if ($instances->size())
         {
@@ -38,15 +40,19 @@ class RepositoryImplementationCategoryItem extends CategoryItem
 
             while ($instance = $instances->next_result())
             {
+                $sysImagePath =
+                    Theme:: getInstance()->getImagePath($instance->get_implementation(), 'Menu', 'png', false);
+
+                $display = file_exists($sysImagePath) ? Item::DISPLAY_ICON : Item::DISPLAY_TEXT;
 
                 $instanceItem = new \Chamilo\Core\Repository\Integration\Chamilo\Core\Menu\Storage\DataClass\RepositoryImplementationItem();
                 $instanceItem->set_implementation($instance->get_implementation());
                 $instanceItem->set_instance_id($instance->get_id());
                 $instanceItem->set_name($instance->get_title());
                 $instanceItem->set_parent($this->getItem()->get_id());
-                $instanceItem->set_display();
+                $instanceItem->set_display($display);
 
-                $sub_html[] = Renderer :: toHtml($this->getMenuRenderer(), $instanceItem, $this);
+                $sub_html[] = Renderer:: toHtml($this->getMenuRenderer(), $instanceItem, $this);
             }
 
             $sub_html[] = '</ul>';
@@ -61,23 +67,24 @@ class RepositoryImplementationCategoryItem extends CategoryItem
         $html[] = '<li' . ($selected ? ' class="chamilo-menu-item-current"' : '') . '>';
         $html[] = '<a ' . $class . 'href="#">';
 
-        $title = Translation :: get('Instance');
+        $title = Translation:: get('Instance');
 
         if ($this->getItem()->show_icon())
         {
             $integrationNamespace = 'Chamilo\Core\Repository\Integration\Chamilo\Core\Menu';
-            $imagePath = Theme :: getInstance()->getImagePath(
+            $imagePath = Theme:: getInstance()->getImagePath(
                 $integrationNamespace,
-                'RepositoryImplementationCategory' . ($selected ? 'Selected' : ''));
+                'RepositoryImplementationCategory' . ($selected ? 'Selected' : '')
+            );
 
             $html[] = '<img class="chamilo-menu-item-icon" src="' . $imagePath . '" title="' . $title . '" alt="' .
-                 $title . '" />';
+                $title . '" />';
         }
 
         if ($this->getItem()->show_title())
         {
             $html[] = '<div class="chamilo-menu-item-label' .
-                 ($this->getItem()->show_icon() ? ' chamilo-menu-item-label-with-image' : '') . '">' . $title . '</div>';
+                ($this->getItem()->show_icon() ? ' chamilo-menu-item-label-with-image' : '') . '">' . $title . '</div>';
         }
 
         $html[] = '<!--[if IE 7]><!--></a><!--<![endif]-->';
