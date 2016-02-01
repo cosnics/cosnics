ea67778d8f2648f0590b10c12a0f63688dbfe74d
vanpouckesven <vanpouckesven@localhost>
2016-02-01 13:51:11 +0100

    Refactoring of the course user importer to services + added possibility to import groups in courses

--- src/Chamilo/Application/Weblcms/Component/CourseUserImporterComponent.php
+++ src/Chamilo/Application/Weblcms/Component/CourseUserImporterComponent.php
@@ -1,16 +1,23 @@
 <?php
 namespace Chamilo\Application\Weblcms\Component;
 
-use Chamilo\Application\Weblcms\Form\CourseUserImportForm;
+use Chamilo\Application\Weblcms\Form\CourseEntityImportForm;
 use Chamilo\Application\Weblcms\Manager;
 use Chamilo\Libraries\Architecture\Application\Application;
 use Chamilo\Libraries\File\Redirect;
 use Chamilo\Libraries\Format\Structure\Breadcrumb;
 use Chamilo\Libraries\Format\Structure\BreadcrumbTrail;
-use Chamilo\Libraries\Format\Tabs\DynamicTabsRenderer;
 use Chamilo\Libraries\Platform\Translation;
 use Chamilo\Libraries\Utilities\Utilities;
-use Chamilo\Libraries\Architecture\ClassnameUtilities;
+use Chamilo\Application\Weblcms\Service\Import\CourseEntity\CourseEntityImporter;
+use Chamilo\Application\Weblcms\Service\Import\CourseEntity\Format\Csv;
+use Chamilo\Application\Weblcms\Service\Import\CourseEntity\Format\ImportFormatFactory;
+use Chamilo\Application\Weblcms\Storage\Repository\WeblcmsRepository;
+use Chamilo\Libraries\File\Import;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+ini_set("max_execution_time", - 1);
+ini_set("memory_limit", - 1);
 
 /**
  * $Id: course_user_importer.class.php 218 2009-11-13 14:21:26Z kariboe $
@@ -33,15 +40,37 @@ class CourseUserImporterComponent extends Manager
             throw new \Chamilo\Libraries\Architecture\Exceptions\NotAllowedException();
         }
 
-        $form = new CourseUserImportForm(CourseUserImportForm :: TYPE_IMPORT, $this->get_url());
+        $form = new CourseEntityImportForm($this->get_url());
 
         if ($form->validate())
         {
-            $success = $form->import_course_users();
+            $importers = array();
+
+            $importers[] = new Csv(new Import());
+
+            $courseEntityImporter = new CourseEntityImporter(
+                new ImportFormatFactory(
+                    $importers
+                ),
+                new WeblcmsRepository()
+            );
+
+            $file = new UploadedFile($_FILES['file']['tmp_name'], $_FILES['file']['name'], $_FILES['file']['type']);
+
+            try
+            {
+                $courseEntityImporter->importCourseEntitiesFromFile($file);
+                $success = true;
+            }
+            catch (\Exception $ex)
+            {
+                $success = false;
+                $failedMessage = $ex->getMessage();
+            }
+
             $this->redirect(
                 Translation :: get($success ? 'CsvUsersProcessed' : 'CsvUsersNotProcessed') . '<br />' .
-                     $form->get_failed_csv(),
-                    ($success ? false : true));
+                $failedMessage, ($success ? false : true));
         }
         else
         {
@@ -59,12 +88,19 @@ class CourseUserImporterComponent extends Manager
     public function display_extra_information()
     {
         $html = array();
-        $html[] = '<p>' . Translation :: get('CSVMustLookLike') . ' (' . Translation :: get('MandatoryFields') . ')</p>';
+        $html[] = '<p>' . Translation :: get('CSVMustLookLikeForUsers') . ' (' . Translation :: get('MandatoryFields') . ')</p>';
         $html[] = '<blockquote>';
         $html[] = '<pre>';
         $html[] = '<b>action</b>;<b>username</b>;<b>coursecode</b>;<b>status</b>';
-        $html[] = 'A;jdoe;course01;1';
-        $html[] = 'D;a.dam;course01;5';
+        $html[] = 'A;jdoe;course01;Teacher';
+        $html[] = 'D;a.dam;course01;Student';
+        $html[] = '</pre></blockquote>';
+        $html[] = '<p>' . Translation :: get('CSVMustLookLikeForGroups') . ' (' . Translation :: get('MandatoryFields') . ')</p>';
+        $html[] = '<blockquote>';
+        $html[] = '<pre>';
+        $html[] = '<b>action</b>;<b>groupcode</b>;<b>coursecode</b>;<b>status</b>';
+        $html[] = 'A;group1;course01;Teacher';
+        $html[] = 'D;group2;course01;Student';
         $html[] = '</pre></blockquote>';
         $html[] = '<p>' . Translation :: get('Details') . '</p>';
         $html[] = '<blockquote>';
@@ -74,8 +110,8 @@ class CourseUserImporterComponent extends Manager
         $html[] = '<br />D: ' . Translation :: get('Delete', null, Utilities :: COMMON_LIBRARIES);
         $html[] = '<br /><br />';
         $html[] = '<u><b>' . Translation :: get('Status') . '</u></b>';
-        $html[] = '<br />1: ' . Translation :: get('Teacher');
-        $html[] = '<br />5: ' . Translation :: get('Student');
+        $html[] = '<br />Teacher';
+        $html[] = '<br />Student';
         $html[] = '</blockquote>';
 
         return implode(PHP_EOL, $html);
@@ -92,13 +128,13 @@ class CourseUserImporterComponent extends Manager
             $breadcrumbtrail->add(
                 new Breadcrumb($redirect->getUrl(), Translation :: get('TypeName', null, 'Chamilo\Core\Admin')));
 
-            $redirect = new Redirect(
-                array(
-                    Application :: PARAM_CONTEXT => \Chamilo\Core\Admin\Manager :: context(),
-                    \Chamilo\Core\Admin\Manager :: PARAM_ACTION => \Chamilo\Core\Admin\Manager :: ACTION_ADMIN_BROWSER,
-                    DynamicTabsRenderer :: PARAM_SELECTED_TAB => ClassnameUtilities :: getInstance()->getNamespaceId(
-                        self :: package())));
-            $breadcrumbtrail->add(new Breadcrumb($redirect->getUrl(), Translation :: get('Courses')));
+//            $redirect = new Redirect(
+//                array(
+//                    Application :: PARAM_CONTEXT => \Chamilo\Core\Admin\Manager :: context(),
+//                    \Chamilo\Core\Admin\Manager :: PARAM_ACTION => \Chamilo\Core\Admin\Manager :: ACTION_ADMIN_BROWSER,
+//                    DynamicTabsRenderer :: PARAM_SELECTED_TAB => ClassnameUtilities :: getInstance()->getNamespaceId(
+//                        self :: package())));
+//            $breadcrumbtrail->add(new Breadcrumb($redirect->getUrl(), Translation :: get('Courses')));
         }
 
         $breadcrumbtrail->add_help('weblcms_course_user_importer');
