9358f3a422c732ed0b2c9859457ab98da6b2625f
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2016-02-10 09:09:48 +0100

    Some cleanup of the SortableTable

---
+++ src/Chamilo/Core/Home/Form/ConfigurationForm.php
@@ -0,0 +1,212 @@
+<?php
+namespace Chamilo\Core\Home\Form;
+
+use Chamilo\Core\Home\Storage\DataClass\Block;
+use Chamilo\Libraries\Format\Form\FormValidator;
+use Chamilo\Libraries\Platform\Translation;
+use Chamilo\Libraries\Utilities\StringUtilities;
+use Chamilo\Libraries\Utilities\Utilities;
+
+/**
+ *
+ * @package Chamilo\Core\Home\Form
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ * @author Eduard Vossen <eduard.vossen@ehb.be>
+ */
+abstract class ConfigurationForm extends FormValidator
+{
+    const RESULT_SUCCESS = 'ObjectUpdated';
+    const RESULT_ERROR = 'ObjectUpdateFailed';
+
+    /**
+     *
+     * @var \Chamilo\Core\Home\Storage\DataClass\Block
+     */
+    private $block;
+
+    /**
+     *
+     * @param \Chamilo\Core\Home\Storage\DataClass\Block $block
+     * @param string $action
+     */
+    public function __construct(Block $block)
+    {
+        parent :: __construct('block', 'post', '');
+
+        $this->block = $block;
+        $this->buildForm();
+        $this->setDefaults();
+    }
+
+    /**
+     *
+     * @return \Chamilo\Core\Home\Storage\DataClass\Block
+     */
+    public function getBlock()
+    {
+        return $this->block;
+    }
+
+    public function buildForm()
+    {
+        $this->addSettings();
+
+        $this->addElement('hidden', Block :: PROPERTY_ID, $this->getBlock()->get_id());
+
+        $buttons[] = $this->createElement(
+            'style_submit_button',
+            'submit',
+            Translation :: get('Save', null, Utilities :: COMMON_LIBRARIES));
+        $buttons[] = $this->createElement(
+            'style_reset_button',
+            'reset',
+            Translation :: get('Reset', null, Utilities :: COMMON_LIBRARIES));
+
+        $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
+    }
+
+    abstract function addSettings();
+
+    public function build_form()
+    {
+        $homeblock = $this->homeblock;
+
+        $context = $homeblock->get_context();
+        $component = $homeblock->get_component();
+
+        $homeblock_config = $this->homeblock_config;
+
+        if (count($homeblock_config['settings']) > 0)
+        {
+            foreach ($homeblock_config['settings'] as $category_name => $settings)
+            {
+                $this->addElement('html', '<div class="configuration_form">');
+                $this->addElement(
+                    'html',
+                    '<span class="category">' . Translation :: get(
+                        (string) StringUtilities :: getInstance()->createString($category_name)->upperCamelize()) .
+                         '</span>');
+
+                foreach ($settings as $name => $setting)
+                {
+                    if ($setting['locked'] == 'true')
+                    {
+                        $this->addElement(
+                            'static',
+                            $name,
+                            Translation :: get(
+                                (string) StringUtilities :: getInstance()->createString($name)->upperCamelize()));
+                    }
+                    elseif ($setting['field'] == 'text')
+                    {
+                        $this->add_textfield(
+                            $name,
+                            Translation :: get(
+                                (string) StringUtilities :: getInstance()->createString($name)->upperCamelize()),
+                            true);
+                    }
+                    else
+                    {
+                        $options_type = $setting['options']['type'];
+                        if ($options_type == 'dynamic')
+                        {
+                            $options_source = $setting['options']['source'];
+                            $class = $context . '\Connector';
+                            $options = call_user_func(array($class, $options_source));
+                        }
+                        else
+                        {
+                            $options = $setting['options']['values'];
+                        }
+
+                        if ($setting['field'] == 'radio' || $setting['field'] == 'checkbox')
+                        {
+                            $group = array();
+                            foreach ($options as $option_value => $option_name)
+                            {
+                                $group[] = & $this->createElement(
+                                    $setting['field'],
+                                    $name,
+                                    null,
+                                    Translation :: get(
+                                        (string) StringUtilities :: getInstance()->createString($option_name)->upperCamelize()),
+                                    $option_value);
+                            }
+                            $this->addGroup(
+                                $group,
+                                $name,
+                                Translation :: get(
+                                    (string) StringUtilities :: getInstance()->createString($name)->upperCamelize()),
+                                '<br/>',
+                                false);
+                        }
+                        elseif ($setting['field'] == 'select')
+                        {
+                            $this->addElement(
+                                'select',
+                                $name,
+                                Translation :: get(
+                                    (string) StringUtilities :: getInstance()->createString($name)->upperCamelize()),
+                                $options);
+                        }
+                    }
+                }
+
+                $this->addElement('html', '<div style="clear: both;"></div>');
+                $this->addElement('html', '</div>');
+            }
+
+            $this->addElement('hidden', Block :: PROPERTY_ID, $this->homeblock->get_id());
+
+            // $this->addElement('submit', 'submit', Translation :: get('Ok', null, Utilities :: COMMON_LIBRARIES));
+            $buttons[] = $this->createElement(
+                'style_submit_button',
+                'submit',
+                Translation :: get('Save', null, Utilities :: COMMON_LIBRARIES),
+                array('class' => 'positive'));
+            $buttons[] = $this->createElement(
+                'style_reset_button',
+                'reset',
+                Translation :: get('Reset', null, Utilities :: COMMON_LIBRARIES),
+                array('class' => 'normal empty'));
+
+            $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
+        }
+        else
+        {
+            $this->addElement('html', Translation :: get('NoConfigurableSettings'));
+        }
+    }
+
+    // /**
+    // * Sets default values.
+    // * Traditionally, you will want to extend this method so it sets default for your learning
+    // * object type's additional properties.
+    // *
+    // * @param array $defaults Default values for this form's parameters.
+    // */
+    // public function setDefaults($defaults = array ())
+    // {
+    // $homeblock_config = $this->homeblock_config;
+    // $homeblock_current_config = $this->homeblock->get_configuration();
+
+    // foreach ($homeblock_config['settings'] as $category_name => $settings)
+    // {
+    // foreach ($settings as $name => $setting)
+    // {
+    // $configuration_value = $homeblock_current_config[$name];
+    // if (isset($configuration_value))
+    // {
+    // $defaults[$name] = $configuration_value;
+    // }
+    // else
+    // {
+    // $defaults[$name] = $setting['default'];
+    // }
+    // }
+    // }
+
+    // parent :: setDefaults($defaults);
+    // }
+}
---
+++ src/Chamilo/Core/Repository/Component/BrowserComponent.php
@@ -0,0 +1,314 @@
+<?php
+namespace Chamilo\Core\Repository\Component;
+
+use Chamilo\Core\Repository\Common\Renderer\ContentObjectRenderer;
+use Chamilo\Core\Repository\Filter\FilterData;
+use Chamilo\Core\Repository\Filter\Renderer\ConditionFilterRenderer;
+use Chamilo\Core\Repository\Manager;
+use Chamilo\Core\Repository\Selector\TypeSelector;
+use Chamilo\Core\Repository\Storage\DataClass\ContentObject;
+use Chamilo\Core\Repository\Storage\DataManager;
+use Chamilo\Core\Repository\Workspace\Service\RightsService;
+use Chamilo\Libraries\Architecture\Application\Application;
+use Chamilo\Libraries\Architecture\Exceptions\NotAllowedException;
+use Chamilo\Libraries\Architecture\Interfaces\DelegateComponent;
+use Chamilo\Libraries\File\Path;
+use Chamilo\Libraries\Format\Structure\ActionBar\ActionBarRenderer;
+use Chamilo\Libraries\Format\Structure\Breadcrumb;
+use Chamilo\Libraries\Format\Structure\BreadcrumbTrail;
+use Chamilo\Libraries\Format\Structure\ToolbarItem;
+use Chamilo\Libraries\Format\Theme;
+use Chamilo\Libraries\Format\Utilities\ResourceManager;
+use Chamilo\Libraries\Platform\Session\Request;
+use Chamilo\Libraries\Platform\Translation;
+use Chamilo\Libraries\Storage\Query\Condition\AndCondition;
+use Chamilo\Libraries\Storage\Query\Condition\Condition;
+use Chamilo\Libraries\Storage\Query\Condition\EqualityCondition;
+use Chamilo\Libraries\Storage\Query\Condition\NotCondition;
+use Chamilo\Libraries\Storage\Query\Variable\PropertyConditionVariable;
+use Chamilo\Libraries\Storage\Query\Variable\StaticConditionVariable;
+use Chamilo\Libraries\Utilities\StringUtilities;
+use Chamilo\Libraries\Utilities\Utilities;
+
+/**
+ * $Id: browser.class.php 204 2009-11-13 12:51:30Z kariboe $
+ *
+ * @package repository.lib.repository_manager.component
+ */
+/**
+ * Default repository manager component which allows the user to browse through the different categories and content
+ * objects in the repository.
+ */
+class BrowserComponent extends Manager implements DelegateComponent
+{
+
+    private $form;
+
+    private $action_bar;
+
+    /**
+     * Runs this component and displays its output.
+     */
+    public function run()
+    {
+        if (! RightsService :: getInstance()->canViewContentObjects($this->get_user(), $this->getWorkspace()))
+        {
+            throw new NotAllowedException();
+        }
+
+        $trail = BreadcrumbTrail :: get_instance();
+
+        $output = $this->get_content_objects_html();
+
+        $query = $this->get_action_bar()->get_query();
+        if (isset($query) && $query != '')
+        {
+            $trail->add(
+                new Breadcrumb(
+                    $this->get_url(),
+                    Translation :: get('SearchResultsFor', null, Utilities :: COMMON_LIBRARIES) . ' ' . $query));
+        }
+
+        $html = array();
+
+        $html[] = $this->render_header();
+
+        $html[] = $this->get_action_bar()->as_html();
+        $html[] = $output;
+        $html[] = ResourceManager :: get_instance()->get_resource_html(
+            Path :: getInstance()->getJavascriptPath('Chamilo\Core\Repository', true) . 'Faq.js');
+        $html[] = $this->render_footer();
+
+        return implode(PHP_EOL, $html);
+    }
+
+    /**
+     * Gets the table which shows the learning objects in the currently active category
+     */
+    private function get_content_objects_html()
+    {
+        $renderer = ContentObjectRenderer :: factory($this->get_renderer(), $this);
+        return $renderer->as_html();
+    }
+
+    public function get_renderer()
+    {
+        $renderer = Request :: get(self :: PARAM_RENDERER);
+
+        if ($renderer && in_array($renderer, $this->get_available_renderers()))
+        {
+            return $renderer;
+        }
+        else
+        {
+            $renderers = $this->get_available_renderers();
+            return $renderers[0];
+        }
+    }
+
+    public function get_available_renderers()
+    {
+        return array(
+            ContentObjectRenderer :: TYPE_TABLE,
+            ContentObjectRenderer :: TYPE_GALLERY,
+            ContentObjectRenderer :: TYPE_SLIDESHOW);
+    }
+
+    public function get_action_bar()
+    {
+        if (! isset($this->action_bar))
+        {
+            $this->action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
+
+            if ($this->has_filter_type())
+            {
+                $filter_type = $this->get_filter_type();
+                $template_registration = \Chamilo\Core\Repository\Configuration :: registration_by_id(
+                    (int) $filter_type);
+
+                $this->action_bar->add_common_action(
+                    new ToolbarItem(
+                        Translation :: get(
+                            'CreateObjectType',
+                            array('TYPE' => $template_registration->get_template()->translate('TypeName'))),
+                        Theme :: getInstance()->getCommonImagePath('Action/Create'),
+                        $this->get_url(
+                            array(
+                                Application :: PARAM_ACTION => self :: ACTION_CREATE_CONTENT_OBJECTS,
+                                TypeSelector :: PARAM_SELECTION => $filter_type)),
+                        ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+            }
+
+            $renderers = $this->get_available_renderers();
+
+            if (count($renderers) > 1)
+            {
+                foreach ($renderers as $renderer)
+                {
+                    $this->action_bar->add_common_action(
+                        new ToolbarItem(
+                            Translation :: get(
+                                (string) StringUtilities :: getInstance()->createString($renderer)->upperCamelize() .
+                                     'View',
+                                    null,
+                                    Utilities :: COMMON_LIBRARIES),
+                            Theme :: getInstance()->getImagePath(
+                                'Chamilo\Core\Repository',
+                                'View/' . StringUtilities :: getInstance()->createString($renderer)->upperCamelize()),
+                            $this->get_url(array(self :: PARAM_RENDERER => $renderer)),
+                            ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+                }
+            }
+
+            $this->action_bar->add_tool_action(
+                new ToolbarItem(
+                    Translation :: get('ManageCategories'),
+                    Theme :: getInstance()->getCommonImagePath('Action/Category'),
+                    $this->get_url(array(Application :: PARAM_ACTION => self :: ACTION_MANAGE_CATEGORIES)),
+                    ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+
+            // $currentCategoryIdentifier = FilterData :: get_instance($this->getWorkspace())->get_filter_property(
+            // FilterData :: FILTER_CATEGORY);
+
+            // $this->action_bar->add_tool_action(
+            // new ToolbarItem(
+            // Translation :: get('AddCategory', null, Utilities :: COMMON_LIBRARIES),
+            // Theme :: getInstance()->getCommonImagePath('Action/Add'),
+            // $this->get_url(
+            // array(
+            // self :: PARAM_ACTION => self :: ACTION_MANAGE_CATEGORIES,
+            // \Chamilo\Configuration\Category\Manager :: PARAM_ACTION => \Chamilo\Configuration\Category\Manager ::
+            // ACTION_CREATE_CATEGORY,
+            // \Chamilo\Configuration\Category\Manager :: PARAM_CATEGORY_ID => $currentCategoryIdentifier,
+            // FilterData :: FILTER_CATEGORY => $currentCategoryIdentifier)),
+            // ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+
+            // if ($currentCategoryIdentifier)
+            // {
+            // $this->action_bar->add_tool_action(
+            // new ToolbarItem(
+            // Translation :: get('EditCategory', null, Utilities :: COMMON_LIBRARIES),
+            // Theme :: getInstance()->getCommonImagePath('Action/Edit'),
+            // $this->get_url(
+            // array(
+            // self :: PARAM_ACTION => self :: ACTION_MANAGE_CATEGORIES,
+            // \Chamilo\Configuration\Category\Manager :: PARAM_ACTION => \Chamilo\Configuration\Category\Manager ::
+            // ACTION_UPDATE_CATEGORY,
+            // \Chamilo\Configuration\Category\Manager :: PARAM_CATEGORY_ID => $currentCategoryIdentifier,
+            // FilterData :: FILTER_CATEGORY => $currentCategoryIdentifier)),
+            // ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+
+            // $this->action_bar->add_tool_action(
+            // new ToolbarItem(
+            // Translation :: get('MoveCategory', null, Utilities :: COMMON_LIBRARIES),
+            // Theme :: getInstance()->getCommonImagePath('Action/Move'),
+            // $this->get_url(
+            // array(
+            // self :: PARAM_ACTION => self :: ACTION_MANAGE_CATEGORIES,
+            // \Chamilo\Configuration\Category\Manager :: PARAM_ACTION => \Chamilo\Configuration\Category\Manager ::
+            // ACTION_MOVE_CATEGORY,
+            // \Chamilo\Configuration\Category\Manager :: PARAM_CATEGORY_ID => $currentCategoryIdentifier,
+            // FilterData :: FILTER_CATEGORY => $currentCategoryIdentifier)),
+            // ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+
+            // $this->action_bar->add_tool_action(
+            // new ToolbarItem(
+            // Translation :: get('DeleteCategory', null, Utilities :: COMMON_LIBRARIES),
+            // Theme :: getInstance()->getCommonImagePath('Action/Delete'),
+            // $this->get_url(
+            // array(
+            // self :: PARAM_ACTION => self :: ACTION_MANAGE_CATEGORIES,
+            // \Chamilo\Configuration\Category\Manager :: PARAM_ACTION => \Chamilo\Configuration\Category\Manager ::
+            // ACTION_DELETE_CATEGORY,
+            // \Chamilo\Configuration\Category\Manager :: PARAM_CATEGORY_ID => $currentCategoryIdentifier,
+            // FilterData :: FILTER_CATEGORY => $currentCategoryIdentifier)),
+            // ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+            // }
+
+            $this->action_bar->add_tool_action(
+                new ToolbarItem(
+                    Translation :: get('ExportCategory'),
+                    Theme :: getInstance()->getCommonImagePath('Action/Backup'),
+                    $this->get_url(
+                        array(
+                            Application :: PARAM_ACTION => self :: ACTION_EXPORT_CONTENT_OBJECTS,
+                            FilterData :: FILTER_CATEGORY => FilterData :: get_instance($this->getWorkspace())->get_filter_property(
+                                FilterData :: FILTER_CATEGORY))),
+                    ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+        }
+
+        return $this->action_bar;
+    }
+
+    public function get_condition()
+    {
+        $conditions = array();
+        $conditions[] = new EqualityCondition(
+            new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_STATE),
+            new StaticConditionVariable(ContentObject :: STATE_NORMAL));
+
+        $conditions[] = new EqualityCondition(
+            new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_OWNER_ID),
+            new StaticConditionVariable($this->get_user_id()));
+
+        $types = DataManager :: get_active_helper_types();
+
+        foreach ($types as $type)
+        {
+            $conditions[] = new NotCondition(
+                new EqualityCondition(
+                    new PropertyConditionVariable(ContentObject :: class_name(), ContentObject :: PROPERTY_TYPE),
+                    new StaticConditionVariable($type)));
+        }
+
+        $filter_condition_renderer = ConditionFilterRenderer :: factory(
+            FilterData :: get_instance($this->getWorkspace()),
+            $this->getWorkspace());
+        $filter_condition = $filter_condition_renderer->render();
+
+        if ($filter_condition instanceof Condition)
+        {
+            $conditions[] = $filter_condition;
+        }
+
+        return new AndCondition($conditions);
+    }
+
+    private function get_parent_id()
+    {
+        return FilterData :: get_instance($this->getWorkspace())->get_filter_property(FilterData :: FILTER_CATEGORY);
+    }
+
+    public function add_additional_breadcrumbs(BreadcrumbTrail $breadcrumbtrail)
+    {
+        $breadcrumbtrail->add_help('repository_browser');
+    }
+
+    public function get_additional_parameters()
+    {
+        return parent :: get_additional_parameters(
+            array(
+                self :: PARAM_RENDERER,
+                ContentObject :: PROPERTY_PARENT_ID,
+                \Chamilo\Configuration\Category\Manager :: PARAM_CATEGORY_ID));
+    }
+
+    /**
+     *
+     * @return int
+     */
+    public function get_filter_type()
+    {
+        return TypeSelector :: get_selection();
+    }
+
+    /**
+     *
+     * @return boolean
+     */
+    public function has_filter_type()
+    {
+        $filter_type = $this->get_filter_type();
+        return isset($filter_type);
+    }
+}
