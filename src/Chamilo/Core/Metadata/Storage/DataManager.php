b414fc6ab334eea1f5d6d813824f05bfb8b38216
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2016-02-11 15:17:26 +0100

    Enable usage of glyphs in the button toolbar + cleanup of css and the course overview menu

---
+++ src/Chamilo/Core/Metadata/Storage/DataManager.php
@@ -0,0 +1,16 @@
+<?php
+namespace Chamilo\Core\Metadata\Storage;
+
+/**
+ * This class represents the data manager for this package
+ * 
+ * @package Chamilo\Core\Metadata\Storage
+ * @author Sven Vanpoucke - Hogeschool Gent
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ * @author Eduard Vossen <eduard.vossen@ehb.be>
+ */
+class DataManager extends \Chamilo\Libraries\Storage\DataManager\DataManager
+{
+    const PREFIX = 'metadata_';
+}
---
+++ src/Chamilo/Core/Repository/ContentObject/File/Implementation/Rendition/Html/Extension/HtmlInlineFlvRenditionImplementation.php
@@ -0,0 +1,48 @@
+<?php
+namespace Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\Extension;
+
+use Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\HtmlInlineRenditionImplementation;
+
+class HtmlInlineFlvRenditionImplementation extends HtmlInlineRenditionImplementation
+{
+
+    public function render($parameters)
+    {
+        $object = $this->get_content_object();
+        $url = \Chamilo\Core\Repository\Manager :: get_document_downloader_url(
+            $object->get_id(),
+            $object->calculate_security_code()) . '&display=1';
+
+        $html = array();
+
+        $html[] = '<object codebase="http://www.videolan.org/" height="' . $parameters[self :: PARAM_HEIGHT] .
+             '" width="' . $parameters[self :: PARAM_WIDTH] . '">';
+        $html[] = '	<param name="showstatusbar" value="true" />';
+        $html[] = '	<param name="showgotobar" value="true" />';
+        $html[] = '	<param name="showaudiocontrols" value="true" />';
+        $html[] = '	<param name="showtracker" value="true" />';
+        $html[] = '	<param name="showpositioncontrols" value="true" />';
+        $html[] = '	<param name="showcontrols" value="true" />';
+        $html[] = '	<param name="autostart" value="true" />';
+        $html[] = '	<param name="url" value="' . $url . '" />';
+        $html[] = '	<param name="height" value="' . $parameters[self :: PARAM_HEIGHT] . '" />';
+        $html[] = '	<param name="width" value="' . $parameters[self :: PARAM_WIDTH] . '" />';
+        $html[] = '	<embed  autosize="true"
+                            autostart="true"
+                            height="' . $parameters[self :: PARAM_HEIGHT] . '"
+                            pluginspage="http://www.videolan.org/"
+                            showaudiocontrols="true"
+                            showcontrols="true"
+                            showgotobar="true"
+                            showpositioncontrols="true"
+                            showstatusbar="true"
+                            showtracker="true"
+                            src="' . $url . '"
+                            type="video/x-flv"
+                            width="' . $parameters[self :: PARAM_WIDTH] . '">';
+        $html[] = '	</embed>';
+        $html[] = '</object>';
+
+        return implode(PHP_EOL, $html);
+    }
+}
