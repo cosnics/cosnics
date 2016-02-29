c8423ff65fd3567db8caf7c9241db2aac45a6196
vanpouckesven <vanpouckesven@localhost>
2016-02-29 15:47:34 +0100

    Bugfixes for the course list

--- src/Chamilo/Application/Weblcms/Resources/Javascript/CourseHome.js
+++ src/Chamilo/Application/Weblcms/Resources/Javascript/CourseHome.js
@@ -1,5 +1,5 @@
-(function($) {
-    var handle_drop = function(ev, ui) {
+(function ($) {
+    var handle_drop = function (ev, ui) {
         alert('yuw');
         // $(this).empty();
         var target = $(this).attr("id");
@@ -8,20 +8,23 @@
 
         $(ui.draggable).parent().remove();
 
-        $.post("./index.php?go=courseviewer&course=" + course_code + "&tool=course_sections&application=weblcms&tool_action=change_section", {
-            target : target,
-            source : source
-        }, function(data) {
-            alert(data);
-            $("#" + target + " > * > .description").empty();
-            $("#" + target + " > * > .description").append(data);
-            $(".tooldrag").css('display', 'inline');
-        });
+        $.post(
+            "./index.php?go=courseviewer&course=" + course_code +
+            "&tool=course_sections&application=weblcms&tool_action=change_section", {
+                target: target,
+                source: source
+            }, function (data) {
+                alert(data);
+                $("#" + target + " > * > .description").empty();
+                $("#" + target + " > * > .description").append(data);
+                $(".tooldrag").css('display', 'inline');
+            }
+        );
     }
 
-    var handle_visible_clicked = function(ev, ui) {
+    var handle_visible_clicked = function (ev, ui) {
         var parent = $(this).parent().parent();
-        var old_parent = parent.parent();
+        var old_parent = parent.parent().parent().parent();
         var tool = parent.attr('id');
         tool = tool.substring(5, tool.length);
 
@@ -38,67 +41,73 @@
         var tool_text = $("#tool_text", parent);
 
         var isInvisible = tool_text.hasClass('invisible-tool');
-        
+
         if (isInvisible) {
-        var new_visible = 1;
-        } else {
-        var new_visible = 0;
+            var new_visible = 1;
+        }
+        else {
+            var new_visible = 0;
         }
 
         // Determine course id
         var course_code = $("#coursecode").html();
 
-        $.post("./index.php?application=Chamilo\\Application\\Weblcms\\Ajax&go=ChangeCourseModuleVisibility", {
-            tool : tool,
-            visible : new_visible,
-            course : course_code
-        }, function(data) {
-            if (data.result_code == 200) {
-            // If succeeded : change the icons and classes
-            // Changes icons and classes
-            var is_visible = imgtag.attr('src');
-            // imgtag.attr('src', new_img);
+        $.post(
+            "./index.php?application=Chamilo\\Application\\Weblcms\\Ajax&go=ChangeCourseModuleVisibility", {
+                tool: tool,
+                visible: new_visible,
+                course: course_code
+            }, function (data) {
+                if (data.result_code == 200) {
+                    // If succeeded : change the icons and classes
+                    // Changes icons and classes
+                    var is_visible = imgtag.attr('src');
+                    // imgtag.attr('src', new_img);
 
-            if (!isInvisible) {
-            tool_text.addClass('invisible-tool');
-            var new_src = src.replace('New', '');
-            new_src = new_src.replace('.png', 'Na.png');
-            console.log(new_src);
-            var new_parent = $('div.disabledblock');
-            imgtag.attr('src', is_visible.replace('Visible.png', 'Invisible.png'));
-            } else {
-            tool_text.removeClass('invisible-tool');
-            var new_src = src.replace('Na.png', '.png');
-            var new_parent = $('div.toolblock:first');
-            imgtag.attr('src', is_visible.replace('Invisible.png', 'Visible.png'));
+                    var new_parent = $('div.removed-tools');
+
+                    if (!isInvisible) {
+                        tool_text.addClass('invisible-tool');
+                        var new_src = src.replace('New', '');
+                        new_src = new_src.replace('.png', 'Na.png');
+
+                        imgtag.attr('src', is_visible.replace('Visible.png', 'Invisible.png'));
+                    }
+                    else {
+                        tool_text.removeClass('invisible-tool');
+                        var new_src = src.replace('Na.png', '.png');
+                        imgtag.attr('src', is_visible.replace('Invisible.png', 'Visible.png'));
+                    }
+
+
+                    // If we use disabled section we should move the
+                    // tool to the correct section
+                    if (new_parent) {
+                        var clear_div = new_parent.children(".clear")[0];
+                        if (clear_div) {
+                            new_parent.children(".clear")[0].remove;
+                        }
+
+                        var message = $('div.normal-message', new_parent);
+                        if (message) {
+                            message.remove();
+                        }
+
+                        new_parent.append(parent.parent());
+                        new_parent.append(clear_div);
+
+                        if (old_parent.find('.tool').size() == 0) {
+                            old_parent.prepend(
+                                '<div class="alert alert-info">' + getTranslation('NoToolsAvailable', {}, 'Chamilo\\Application\\Weblcms') +
+                                '</div>'
+                            );
+                        }
+                    }
+
+                    tool_img.attr('src', new_src);
+                }
             }
-
-            var disabled_block = $('div.disabledblock')
-
-            // If we use disabled section we should move the
-            // tool to the correct section
-            if (disabled_block.attr('class') == 'disabledblock') {
-            var clear_div = new_parent.children(".clear")[0];
-            if (clear_div) {
-            new_parent.children(".clear")[0].remove;
-            }
-
-            var message = $('div.normal-message', new_parent);
-            if (message) {
-            message.remove();
-            }
-
-            new_parent.append(parent);
-            new_parent.append(clear_div);
-
-            if (old_parent.children('.tool').size() == 0) {
-            old_parent.prepend('<div class="normal-message">' + getTranslation('NoToolsAvailable', 'weblcms') + '</div>');
-            }
-            }
-
-            tool_img.attr('src', new_src);
-            }
-        });
+        );
 
         return false;
     }
@@ -115,40 +124,46 @@
         var section = $(this).attr("id");
         var order = $(this).sortable("serialize");
 
-        $.post("./application/weblcms/php/ajax/block_sort.php", {
-            section : section,
-            order : order
-        }, function(data) {
+        $.post(
+            "./application/weblcms/php/ajax/block_sort.php", {
+                section: section,
+                order: order
+            }, function (data) {
 
-        });
+            }
+        );
 
     }
 
     function toolsSortable() {
         $(".toolblock .block .description").sortable("destroy");
-        $(".toolblock .block .description").sortable({
-            cancel : 'a',
-            opacity : 0.8,
-            forceHelperSize : true,
-            forcePlaceholderSize : true,
-            cursor : 'move',
-            helper : 'original',
-            placeholder : 'toolSortHelper',
-            revert : true,
-            scroll : false,
-            start : toolsSortableStart,
-            beforeStop : toolSortableBeforeStop,
-            update : toolsSortableUpdate
-        });
+        $(".toolblock .block .description").sortable(
+            {
+                cancel: 'a',
+                opacity: 0.8,
+                forceHelperSize: true,
+                forcePlaceholderSize: true,
+                cursor: 'move',
+                helper: 'original',
+                placeholder: 'toolSortHelper',
+                revert: true,
+                scroll: false,
+                start: toolsSortableStart,
+                beforeStop: toolSortableBeforeStop,
+                update: toolsSortableUpdate
+            }
+        );
     }
 
-    $(document).ready(function() {
-        toolsSortable();
+    $(document).ready(
+        function () {
+            toolsSortable();
 
-        $(".tool_visible").bind('click', handle_visible_clicked);
+            $(".tool_visible").bind('click', handle_visible_clicked);
 
-        $(".tooldrag").css('display', 'inline');
+            $(".tooldrag").css('display', 'inline');
 
-    });
+        }
+    );
 
 })(jQuery);
