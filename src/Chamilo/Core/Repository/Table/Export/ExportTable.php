003150e7b8189db71c05d94b186d35997f3db968
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-02-29 11:21:16 +0100

    a bunch of bugfixes for the exporter component in the repository

--- src/Chamilo/Core/Repository/Table/Export/ExportTable.php
+++ src/Chamilo/Core/Repository/Table/Export/ExportTable.php
@@ -7,10 +7,11 @@ use HTML_Table;
 
 class ExportTable extends SortableTableFromArray
 {
-
     public function toHtml($totalValue, $totalColumn)
     {
-        $tableData = $this->getData($this->getFrom());
+        $this->initializeTable();
+
+        $tableData = $this->getData();
 
         foreach ($tableData as $index => & $row)
         {
@@ -22,12 +23,14 @@ class ExportTable extends SortableTableFromArray
 
         $this->altRowAttributes(0, array('class' => 'row_even'), array('class' => 'row_odd'), true);
 
-        foreach ($this->getHeaderAttributes() as $column => & $attributes)
+        $headerAttributes = $this->getHeaderAttributes();
+        foreach ($headerAttributes as $column => & $attributes)
         {
             $this->setCellAttributes(0, $column, $attributes);
         }
 
-        foreach ($this->getContentCellAttributes() as $column => & $attributes)
+        $contentCellAttributes = $this->getContentCellAttributes();
+        foreach ($contentCellAttributes as $column => & $attributes)
         {
             $this->setColAttributes($column, $attributes);
         }
@@ -51,6 +54,10 @@ class ExportTable extends SortableTableFromArray
                      '" style="font-weight:bold; text-transform:uppercase;"');
         }
 
-        return \HTML_Table :: toHtml();
+        $html[] = '<div class="table-responsive">';
+        $html[] = \HTML_Table :: toHtml();
+        $html[] = '</div>';
+
+        return implode("\n", $html);
     }
 }
