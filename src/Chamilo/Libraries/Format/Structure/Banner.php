e61227b81c4b802f70e398e5922b0f454be8b0cd
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-03-11 15:32:35 +0100

    A small layout bugfix

--- src/Chamilo/Libraries/Format/Structure/Banner.php
+++ src/Chamilo/Libraries/Format/Structure/Banner.php
@@ -105,7 +105,7 @@ class Banner
                     Application :: PARAM_ACTION => \Chamilo\Core\User\Manager :: ACTION_ADMIN_USER));
             $link = $redirect->getUrl();
 
-            $html[] = '<div class="row warning-banner bg-warning text-warning">' .
+            $html[] = '<div class="warning-banner bg-warning text-warning">' .
                  Translation :: get('LoggedInAsUser', null, \Chamilo\Core\User\Manager :: context()) . ' ' .
                  $this->getApplication()->getUser()->get_fullname() . ' <a href="' . $link . '">' .
                  Translation :: get('Back', null, Utilities :: COMMON_LIBRARIES) . '</a></div>';
