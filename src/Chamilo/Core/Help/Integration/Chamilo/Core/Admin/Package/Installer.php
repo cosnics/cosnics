21b0e27952461a31f6440e8f9d8fe05f4858f371
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-04-15 16:06:17 +0200

    [Publications] Bug with the rights checks on table actions

---
+++ src/Chamilo/Core/Help/Integration/Chamilo/Core/Admin/Package/Installer.php
@@ -0,0 +1,6 @@
+<?php
+namespace Chamilo\Core\Help\Integration\Chamilo\Core\Admin\Package;
+
+class Installer extends \Chamilo\Configuration\Package\Action\Installer
+{
+}
---
+++ src/Chamilo/Core/Metadata/Relation/Instance/Table/Relation/RelationTableDataProvider.php
@@ -0,0 +1,46 @@
+<?php
+namespace Chamilo\Core\Metadata\Relation\Instance\Table\Relation;
+
+use Chamilo\Core\Metadata\Relation\Instance\Storage\DataManager;
+use Chamilo\Core\Metadata\Storage\DataClass\RelationInstance;
+use Chamilo\Libraries\Format\Table\Extension\DataClassTable\DataClassTableDataProvider;
+use Chamilo\Libraries\Storage\Parameters\DataClassRetrievesParameters;
+
+/**
+ *
+ * @package Chamilo\Core\Metadata\Relation\Instance\Table\Relation
+ * @author Sven Vanpoucke - Hogeschool Gent
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ * @author Eduard Vossen <eduard.vossen@ehb.be>
+ */
+class RelationTableDataProvider extends DataClassTableDataProvider
+{
+
+    /**
+     * Returns the data as a resultset
+     *
+     * @param \Chamilo\Libraries\Storage\Query\Condition\Condition $condition
+     * @param integer $offset
+     * @param integer $count
+     * @param \Chamilo\Libraries\Storage\Query\OrderBy[] $order_property
+     * @return \Chamilo\Libraries\Storage\ResultSet\ResultSet
+     */
+    public function retrieve_data($condition, $offset, $count, $order_property = null)
+    {
+        $parameters = new DataClassRetrievesParameters($condition, $count, $offset, $order_property);
+
+        return DataManager :: retrieves(RelationInstance :: class_name(), $parameters);
+    }
+
+    /**
+     * Counts the data
+     *
+     * @param \Chamilo\Libraries\Storage\Query\Condition\Condition $condition
+     * @return integer
+     */
+    public function count_data($condition)
+    {
+        return DataManager :: count(RelationInstance :: class_name(), $condition);
+    }
+}
