76298699604b0f24f492743474ef54eea121bce6
vanpouckesven <vanpouckesven@localhost>
2016-04-20 13:07:37 +0200

    [User Tool] Added a check if the user is allowed to subscribe the group into the course

--- src/Chamilo/Application/Weblcms/Tool/Implementation/User/Component/SubscribeGroupsDetailsComponent.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/User/Component/SubscribeGroupsDetailsComponent.php
@@ -89,7 +89,15 @@ class SubscribeGroupsDetailsComponent extends SubscribeGroupsTabComponent
     {
         $buttonToolbar = new ButtonToolBar();
 
-        if (! in_array($group->getId(), $this->subscribedGroups))
+        $courseManagementRights = CourseManagementRights::get_instance();
+
+        $isAllowed = $courseManagementRights->is_allowed_for_platform_group(
+            CourseManagementRights :: TEACHER_DIRECT_SUBSCRIBE_RIGHT,
+            $group->getId(),
+            $this->get_course_id()
+        );
+
+        if (!in_array($group->getId(), $this->subscribedGroups) && $isAllowed)
         {
             $buttonToolbar->addItem(
                 new Button(
