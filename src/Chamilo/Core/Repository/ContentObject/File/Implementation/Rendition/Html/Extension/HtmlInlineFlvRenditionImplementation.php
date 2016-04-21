6f6cae929a44bb553f62f41c85d976c8734284fc
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2016-04-21 10:23:51 +0200

    Use MediaElementJs to attempt to play back some basic html5 supported video and audio formats inline. If not, offer a download option

--- src/Chamilo/Core/Repository/ContentObject/File/Implementation/Rendition/Html/Extension/HtmlInlineFlvRenditionImplementation.php
+++ src/Chamilo/Core/Repository/ContentObject/File/Implementation/Rendition/Html/Extension/HtmlInlineFlvRenditionImplementation.php
@@ -1,48 +1,24 @@
 <?php
 namespace Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\Extension;
 
-use Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\HtmlInlineRenditionImplementation;
-
-class HtmlInlineFlvRenditionImplementation extends HtmlInlineRenditionImplementation
+class HtmlInlineFlvRenditionImplementation extends HtmlInlineMediaElementVideoRenditionImplementation
 {
 
-    public function render($parameters)
+    /**
+     *
+     * @see \Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\Extension\HtmlInlineMediaElementVideoRenditionImplementation::getMediaElement()
+     */
+    public function getSources($parameters)
     {
-        $object = $this->get_content_object();
-        $url = \Chamilo\Core\Repository\Manager :: get_document_downloader_url(
-            $object->get_id(),
-            $object->calculate_security_code()) . '&display=1';
+        return '<source type="video/flv" src="' . $this->getMediaUrl() . '" />';
+    }
 
-        $html = array();
-
-        $html[] = '<object codebase="http://www.videolan.org/" height="' . $parameters[self :: PARAM_HEIGHT] .
-             '" width="' . $parameters[self :: PARAM_WIDTH] . '">';
-        $html[] = '	<param name="showstatusbar" value="true" />';
-        $html[] = '	<param name="showgotobar" value="true" />';
-        $html[] = '	<param name="showaudiocontrols" value="true" />';
-        $html[] = '	<param name="showtracker" value="true" />';
-        $html[] = '	<param name="showpositioncontrols" value="true" />';
-        $html[] = '	<param name="showcontrols" value="true" />';
-        $html[] = '	<param name="autostart" value="true" />';
-        $html[] = '	<param name="url" value="' . $url . '" />';
-        $html[] = '	<param name="height" value="' . $parameters[self :: PARAM_HEIGHT] . '" />';
-        $html[] = '	<param name="width" value="' . $parameters[self :: PARAM_WIDTH] . '" />';
-        $html[] = '	<embed  autosize="true"
-                            autostart="true"
-                            height="' . $parameters[self :: PARAM_HEIGHT] . '"
-                            pluginspage="http://www.videolan.org/"
-                            showaudiocontrols="true"
-                            showcontrols="true"
-                            showgotobar="true"
-                            showpositioncontrols="true"
-                            showstatusbar="true"
-                            showtracker="true"
-                            src="' . $url . '"
-                            type="video/x-flv"
-                            width="' . $parameters[self :: PARAM_WIDTH] . '">';
-        $html[] = '	</embed>';
-        $html[] = '</object>';
-
-        return implode(PHP_EOL, $html);
+    /**
+     *
+     * @see \Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\Extension\HtmlInlineMediaElementVideoRenditionImplementation::getMediaElementType()
+     */
+    public function getMediaElementType()
+    {
+        return 'video';
     }
 }
---
+++ src/Chamilo/Core/Repository/ContentObject/File/Implementation/Rendition/Html/Extension/HtmlInlineMp4RenditionImplementation.php
@@ -0,0 +1,30 @@
+<?php
+namespace Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\Extension;
+
+/**
+ *
+ * @package Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\Extension
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ */
+class HtmlInlineMp4RenditionImplementation extends HtmlInlineMediaElementVideoRenditionImplementation
+{
+
+    /**
+     *
+     * @see \Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\Extension\HtmlInlineMediaElementVideoRenditionImplementation::getMediaElement()
+     */
+    public function getSources($parameters)
+    {
+        return '<source type="video/mp4" src="' . $this->getMediaUrl() . '" />';
+    }
+
+    /**
+     *
+     * @see \Chamilo\Core\Repository\ContentObject\File\Implementation\Rendition\Html\Extension\HtmlInlineMediaElementVideoRenditionImplementation::getMediaElementType()
+     */
+    public function getMediaElementType()
+    {
+        return 'video';
+    }
+}
