d5b4f4e554b9da6dc1389277e0ceae7d55fdde6b
Scaramanga <Scaramanga@localhost>
2016-05-09 14:57:10 +0200

    Eliminated the QuickForm Controller dependency for the Publication wizard in the Repository

--- src/Chamilo/Core/Repository/Publication/Component/PublisherComponent.php
+++ src/Chamilo/Core/Repository/Publication/Component/PublisherComponent.php
@@ -1,15 +1,17 @@
 <?php
 namespace Chamilo\Core\Repository\Publication\Component;
 
+use Chamilo\Core\Repository\Publication\Form\LocationForm;
 use Chamilo\Core\Repository\Publication\Manager;
-use Chamilo\Core\Repository\Publication\Wizard\PublisherWizard;
 use Chamilo\Libraries\Architecture\Interfaces\DelegateComponent;
 use Chamilo\Libraries\Format\Structure\BreadcrumbTrail;
+use Chamilo\Core\Repository\Publication\PublicationProcessor;
 
 /**
- * $Id: publisher.class.php 204 2009-11-13 12:51:30Z kariboe $
  *
- * @package repository.lib.repository_manager.component
+ * @package Chamilo\Core\Repository\Publication\Component
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
  */
 class PublisherComponent extends Manager implements DelegateComponent
 {
@@ -19,12 +21,45 @@ class PublisherComponent extends Manager implements DelegateComponent
      */
     public function run()
     {
-        $wizard = new PublisherWizard($this);
-        return $wizard->run();
+        $locationForm = new LocationForm(
+            $this,
+            $this->get_url(
+                array(
+                    \Chamilo\Core\Repository\Manager :: PARAM_CONTENT_OBJECT_ID => $this->getRequest()->get(
+                        \Chamilo\Core\Repository\Manager :: PARAM_CONTENT_OBJECT_ID))));
+
+        if ($locationForm->validate())
+        {
+            $publicationProcessor = new PublicationProcessor($this, $locationForm->exportValues());
+            return $publicationProcessor->run();
+        }
+        else
+        {
+            $html = array();
+
+            $html[] = $this->render_header();
+            $html[] = $locationForm->toHtml();
+            $html[] = $this->render_footer();
+
+            return implode(PHP_EOL, $html);
+        }
     }
 
+    /**
+     *
+     * @see \Chamilo\Libraries\Architecture\Application\Application::add_additional_breadcrumbs()
+     */
     public function add_additional_breadcrumbs(BreadcrumbTrail $breadcrumbtrail)
     {
         $breadcrumbtrail->add_help('repository_publisher');
+    }
+
+    /**
+     *
+     * @see \Chamilo\Libraries\Architecture\Application\Application::get_additional_parameters()
+     */
+    public function get_additional_parameters()
+    {
+        return array(\Chamilo\Core\Repository\Manager :: PARAM_CONTENT_OBJECT_ID);
     }
 }
