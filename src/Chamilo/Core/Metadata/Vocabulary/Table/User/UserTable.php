9cd9bd9c32017cf9dfde79c88fa5b70cf4fb7173
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2016-05-13 10:42:54 +0200

    Fixed a missing / incorrect constant reference

---
+++ src/Chamilo/Core/Metadata/Vocabulary/Table/User/UserTable.php
@@ -0,0 +1,40 @@
+<?php
+namespace Chamilo\Core\Metadata\Vocabulary\Table\User;
+
+use Chamilo\Core\Metadata\Vocabulary\Manager;
+use Chamilo\Libraries\Format\Table\Extension\RecordTable\RecordTable;
+use Chamilo\Libraries\Format\Table\FormAction\TableFormAction;
+use Chamilo\Libraries\Format\Table\FormAction\TableFormActions;
+use Chamilo\Libraries\Format\Table\Interfaces\TableFormActionsSupport;
+use Chamilo\Libraries\Platform\Translation;
+use Chamilo\Libraries\Utilities\Utilities;
+
+/**
+ * Table for the schema
+ *
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ */
+class UserTable extends RecordTable implements TableFormActionsSupport
+{
+    const TABLE_IDENTIFIER = Manager :: PARAM_USER_ID;
+
+    /**
+     * Returns the implemented form actions
+     *
+     * @return TableFormActions
+     */
+    public function get_implemented_form_actions()
+    {
+        $actions = new TableFormActions(__NAMESPACE__, self :: TABLE_IDENTIFIER);
+
+        $actions->add_form_action(
+            new TableFormAction(
+                $this->get_component()->get_url(
+                    array(
+                        Manager :: PARAM_ACTION => Manager :: ACTION_DELETE,
+                        \Chamilo\Core\Metadata\Element\Manager :: PARAM_ELEMENT_ID => $this->get_component()->getSelectedElementId())),
+                Translation :: get('RemoveSelected', null, Utilities :: COMMON_LIBRARIES)));
+
+        return $actions;
+    }
+}
---
+++ src/Chamilo/Core/User/UserDetails.php
@@ -0,0 +1,86 @@
+<?php
+namespace Chamilo\Core\User;
+
+use Chamilo\Libraries\Architecture\Application\Application;
+use Chamilo\Libraries\File\Redirect;
+use Chamilo\Libraries\Format\Theme;
+use Chamilo\Libraries\Platform\Translation;
+use Chamilo\Libraries\Utilities\StringUtilities;
+
+class UserDetails
+{
+
+    /**
+     * The user
+     */
+    private $user;
+
+    /**
+     * Indicates if a border should be included
+     */
+    private $border;
+
+    /**
+     * Constructor
+     *
+     * @param $user User
+     * @param $border boolean Indicates if a border should be included
+     */
+    public function __construct($user, $border = true)
+    {
+        $this->user = $user;
+        $this->border = $border;
+    }
+
+    /**
+     * Returns a HTML representation of the user details
+     *
+     * @return string
+     * @todo Implement further details
+     */
+    public function toHtml()
+    {
+        $html[] = '<div ';
+
+        if ($this->border)
+        {
+            $html[] = 'class="user_details"';
+        }
+        else
+        {
+            $html[] = 'class="vertical_space"';
+        }
+
+        $html[] = 'style="clear: both;background-image: url(' . Theme :: getInstance()->getImagePath(
+            __NAMESPACE__,
+            'Logo/22') . ');">';
+        
+        $profilePhotoUrl = new Redirect(
+            array(
+                Application :: PARAM_CONTEXT => \Chamilo\Core\User\Ajax\Manager :: context(),
+                Application :: PARAM_ACTION => \Chamilo\Core\User\Ajax\Manager :: ACTION_USER_PICTURE,
+                \Chamilo\Core\User\Manager :: PARAM_USER_USER_ID => $this->user->get_id()));
+        
+        $html[] = '<img src="' . $profilePhotoUrl->getUrl() . '" alt="' . $this->user->get_fullname() .
+             '" style="margin: 10px;max-height: 150px; border:1px solid black;float: right; display: inline;"/>';
+        $html[] = '<div class="title">';
+        $html[] = $this->user->get_fullname();
+        $html[] = '</div>';
+        $html[] = '<div class="description">';
+        $html[] = Translation :: get('Email') . ': ' .
+             StringUtilities :: getInstance()->encryptMailLink($this->user->get_email());
+        $html[] = '<br />' . Translation :: get('Username') . ': ' . $this->user->get_username();
+        $html[] = '<br />' . Translation :: get('Status') . ': ' .
+             ($this->user->get_status() == 1 ? Translation :: get('Teacher') : Translation :: get('Student'));
+
+        if ($this->user->is_platform_admin())
+        {
+            $html[] = ', ' . Translation :: get('PlatformAdministrator');
+        }
+
+        $html[] = '</div>';
+        $html[] = '<div style="clear:both;"><span></span></div>';
+        $html[] = '</div>';
+        return implode(PHP_EOL, $html);
+    }
+}
