ed087fa06f452efcc1c78903aed973f03818b05e
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-05-17 11:27:32 +0200

    [Course Copier] Changed form layout [Course Truncater] Changed form layout

---
+++ src/Chamilo/Libraries/File/PackagesContentFinder/PackagesClassFinder.php
@@ -0,0 +1,68 @@
+<?php
+namespace Chamilo\Libraries\File\PackagesContentFinder;
+
+/**
+ * Finds classes in packages based on a filename and classname.
+ * Uses a PHP-based caching system.
+ * Class PackagesClassFinder
+ *
+ * @package common\libraries
+ * @author Sven Vanpoucke - Hogeschool Gent
+ */
+class PackagesClassFinder extends PackagesContentFinder
+{
+
+    /**
+     * The path relative to the root of the package that needs to be searched
+     *
+     * @var string
+     */
+    private $relativeFilePath;
+
+    /**
+     * The class name that needs to be searched
+     *
+     * @var string
+     */
+    private $className;
+
+    /**
+     * Locates the classes by a given filepath and classname.
+     * Checks for each package if the path and the class exists.
+     *
+     * @param string $relativeFilePath - The path relative to the package root
+     * @param string $className - The classname relative to the package namespace (not the fully qualified one)
+     * @return string[]
+     */
+    public function findClasses($relativeFilePath, $className)
+    {
+        $this->relativeFilePath = $relativeFilePath;
+        $this->className = $className;
+
+        return $this->findContent();
+    }
+
+    /**
+     * Handles a single package
+     *
+     * @param string $package
+     *
+     * @return array
+     */
+    function handlePackage($package)
+    {
+        $classes = array();
+
+        $path = $this->getPackagePath($package) . $this->relativeFilePath;
+        if (file_exists($path))
+        {
+            $class = $package . '\\' . $this->className;
+            if (class_exists($class))
+            {
+                $classes[$package] = $class;
+            }
+        }
+
+        return $classes;
+    }
+}
