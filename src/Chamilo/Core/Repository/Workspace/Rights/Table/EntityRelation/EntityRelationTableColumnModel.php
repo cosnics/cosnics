cd082c3ce63d868ddfb6652f7ef2ac03dd633d20
vanpouckesven <vanpouckesven@localhost>
2016-06-01 13:47:36 +0200

    [Repository] Workspace fix fatal error in updater component [Repository] Workspace fix the manage rights in the updater component [Repository] Fixes of translations in workspace management

--- src/Chamilo/Core/Repository/Workspace/Rights/Table/EntityRelation/EntityRelationTableColumnModel.php
+++ src/Chamilo/Core/Repository/Workspace/Rights/Table/EntityRelation/EntityRelationTableColumnModel.php
@@ -1,6 +1,7 @@
 <?php
 namespace Chamilo\Core\Repository\Workspace\Rights\Table\EntityRelation;
 
+use Chamilo\Core\Repository\Workspace\Rights\Manager;
 use Chamilo\Core\Repository\Workspace\Service\RightsService;
 use Chamilo\Libraries\Format\Structure\ToolbarItem;
 use Chamilo\Libraries\Format\Table\Column\StaticTableColumn;
@@ -19,52 +20,72 @@ class EntityRelationTableColumnModel extends DataClassTableColumnModel implement
      */
     public function initialize_columns()
     {
-        $this->add_column(new StaticTableColumn(self :: COLUMN_ENTITY));
+        $this->add_column(
+            new StaticTableColumn(
+                self :: COLUMN_ENTITY, Translation::getInstance()->getTranslation('Entity', null, Manager::context())
+            )
+        );
         $this->add_column(
             new StaticTableColumn(
                 RightsService :: RIGHT_VIEW,
-                $this->getRightIcon(RightsService :: RIGHT_VIEW, 'ViewRight')));
+                $this->getRightIcon(RightsService :: RIGHT_VIEW, 'ViewRight')
+            )
+        );
         $this->add_column(
             new StaticTableColumn(
                 RightsService :: RIGHT_ADD,
-                $this->getRightIcon(RightsService :: RIGHT_ADD, 'AddRight')));
+                $this->getRightIcon(RightsService :: RIGHT_ADD, 'AddRight')
+            )
+        );
         $this->add_column(
             new StaticTableColumn(
                 RightsService :: RIGHT_EDIT,
-                $this->getRightIcon(RightsService :: RIGHT_EDIT, 'EditRight')));
+                $this->getRightIcon(RightsService :: RIGHT_EDIT, 'EditRight')
+            )
+        );
         $this->add_column(
             new StaticTableColumn(
                 RightsService :: RIGHT_DELETE,
-                $this->getRightIcon(RightsService :: RIGHT_DELETE, 'DeleteRight')));
+                $this->getRightIcon(RightsService :: RIGHT_DELETE, 'DeleteRight')
+            )
+        );
         $this->add_column(
             new StaticTableColumn(
                 RightsService :: RIGHT_USE,
-                $this->getRightIcon(RightsService :: RIGHT_USE, 'UseRight')));
+                $this->getRightIcon(RightsService :: RIGHT_USE, 'UseRight')
+            )
+        );
         $this->add_column(
             new StaticTableColumn(
                 RightsService :: RIGHT_COPY,
-                $this->getRightIcon(RightsService :: RIGHT_COPY, 'CopyRight')));
+                $this->getRightIcon(RightsService :: RIGHT_COPY, 'CopyRight')
+            )
+        );
         $this->add_column(
             new StaticTableColumn(
                 RightsService :: RIGHT_MANAGE,
-                $this->getRightIcon(RightsService :: RIGHT_MANAGE, 'ManageRight')));
+                $this->getRightIcon(RightsService :: RIGHT_MANAGE, 'ManageRight')
+            )
+        );
     }
 
     /**
      *
      * @param integer $right
      * @param string $translationVariable
+     *
      * @return string
      */
     private function getRightIcon($right, $translationVariable)
     {
-        return Theme :: getInstance()->getImage(
+        return Theme:: getInstance()->getImage(
             'Rights/' . $right,
             'png',
-            Translation :: get($translationVariable),
+            Translation:: get($translationVariable),
             null,
             ToolbarItem :: DISPLAY_ICON,
             false,
-            \Chamilo\Core\Repository\Workspace\Manager :: context());
+            \Chamilo\Core\Repository\Workspace\Manager:: context()
+        );
     }
 }
