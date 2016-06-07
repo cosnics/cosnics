f53a60ac452245ea186abe0f22da38eb6d4da534
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-06-07 08:28:30 +0200

    Some portfolio fixes

--- src/Chamilo/Core/Repository/ContentObject/Portfolio/Display/Component/ItemComponent.php
+++ src/Chamilo/Core/Repository/ContentObject/Portfolio/Display/Component/ItemComponent.php
@@ -53,7 +53,7 @@ abstract class ItemComponent extends Manager implements DelegateComponent
         $html = array();
 
         $html[] = parent :: render_header();
-        $html[] = '<div class="col-xs-12 col-md-3 col-lg-2">';
+        $html[] = '<div class="col-xs-12 col-sm-4 col-md-3">';
 
         if ($this->get_parent() instanceof PortfolioComplexRights &&
              $this->get_parent()->is_allowed_to_set_content_object_rights())
@@ -99,7 +99,7 @@ abstract class ItemComponent extends Manager implements DelegateComponent
         $html[] = '</div>';
         $html[] = '</div>';
 
-        $html[] = '<div class="col-xs-12 col-md-9 col-lg-10">';
+        $html[] = '<div class="col-xs-12 col-sm-8 col-md-9">';
 
         return implode(PHP_EOL, $html);
     }
