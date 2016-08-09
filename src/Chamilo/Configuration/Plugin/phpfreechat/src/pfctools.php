cea3b8149ec294c9439349d7cd06c93a9ef81852
vanpouckesven <vanpouckesven@localhost>
2016-08-09 14:10:44 +0200

    Fixed permissions

--- src/Chamilo/Configuration/Plugin/phpfreechat/src/pfctools.php
+++ src/Chamilo/Configuration/Plugin/phpfreechat/src/pfctools.php
@@ -188,7 +188,7 @@ function rm_r($dir)
  * @param string $dest Destination path
  * @return bool Returns TRUE on success, FALSE on failure
  */
-function copy_r($source, $dest, $modedir = 06770, $modefile = 06770)
+function copy_r($source, $dest, $modedir = 06770, $modefile = 0770)
 {
     // Simple copy for a file
     if (is_file($source))
