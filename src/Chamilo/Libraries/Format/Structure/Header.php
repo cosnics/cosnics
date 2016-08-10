d8ba057ec1a205b16bdabdab7109b9e5940b6986
vanpouckesven <vanpouckesven@localhost>
2016-08-10 14:41:19 +0200

    [Core] Adding google analytics with a setting

--- src/Chamilo/Libraries/Format/Structure/Header.php
+++ src/Chamilo/Libraries/Format/Structure/Header.php
@@ -231,6 +231,36 @@ class Header
              \Chamilo\Configuration\Configuration :: get('Chamilo\Core\Admin', 'site_name');
 
         $this->addHtmlHeader('<title>' . $pageTitle . '</title>');
+
+        $this->addGoogleAnalyticsTracking();
+    }
+
+    /**
+     * Adds the google analytics tracking to the header if configured
+     */
+    protected function addGoogleAnalyticsTracking()
+    {
+        $googleAnalyticsTrackingId = \Chamilo\Configuration\Configuration :: get(
+            'Chamilo\Core\Admin', 'google_analytics_tracking_id'
+        );
+
+        if(!empty($googleAnalyticsTrackingId))
+        {
+            $html = array();
+
+            $html[] = '<script>';
+            $html[] = '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){';
+            $html[] = '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),';
+            $html[] = 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)';
+            $html[] = '})(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');';
+            $html[] = '';
+            $html[] = 'ga(\'create\', \'' . $googleAnalyticsTrackingId . '\', \'auto\');';
+            $html[] = 'ga(\'send\', \'pageview\', location.pathname);';
+            $html[] = '';
+            $html[] = '</script>';
+
+            $this->addHtmlHeader(implode(PHP_EOL, $html));
+        }
     }
 
     /**
