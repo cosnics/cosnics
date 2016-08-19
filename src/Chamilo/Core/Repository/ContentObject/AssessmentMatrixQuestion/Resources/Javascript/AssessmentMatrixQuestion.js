d7a7687e5e3be3dee7f2d209062c5cc4deeddf72
vanpouckesven <vanpouckesven@localhost>
2016-08-19 10:07:12 +0200

    [Assessment Matrix Question] Fixed a few javascript / ajax bugs where options could not correctly be added / removed

--- src/Chamilo/Core/Repository/ContentObject/AssessmentMatrixQuestion/Resources/Javascript/AssessmentMatrixQuestion.js
+++ src/Chamilo/Core/Repository/ContentObject/AssessmentMatrixQuestion/Resources/Javascript/AssessmentMatrixQuestion.js
@@ -10,15 +10,18 @@ $(function() {
 			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
 			'Z' ];
 
+	var ajaxContext = 'Chamilo\\Core\\Repository\\ContentObject\\AssessmentMatrixQuestion\\Ajax';
+	var ajaxUri = getPath('WEB_PATH') + 'index.php';
+
 	function getDeleteIconMatches() {
 		return $('.table-data.matches > tbody > tr:first td:last .remove_match')
-				.attr('src').replace('_na.png', '.png');
+				.attr('src').replace('Na.png', '.png');
 	}
 
 	function getDeleteIconOptions() {
 		return $(
 				'.table-data.options > tbody > tr:first td:last .remove_option')
-				.attr('src').replace('_na.png', '.png');
+				.attr('src').replace('Na.png', '.png');
 	}
 
 	function getSelectOptions() {
@@ -31,7 +34,7 @@ $(function() {
 		var deleteImage, deleteField, rows, counter = 0;
 
 		deleteImage = '<img class="remove_match" src="'
-				+ getDeleteIconMatches().replace('.png', '_na.png') + '"/>';
+				+ getDeleteIconMatches().replace('.png', 'Na.png') + '"/>';
 		deleteField = '<input id="remove_match_$option_number" class="remove_match" type="image" src="'
 				+ getDeleteIconMatches()
 				+ '" name="remove_match[$option_number]" />';
@@ -65,7 +68,7 @@ $(function() {
 		var deleteImage, deleteField, rows, counter = 1;
 
 		deleteImage = '<img class="remove_option" src="'
-				+ getDeleteIconOptions().replace('.png', '_na.png') + '"/>';
+				+ getDeleteIconOptions().replace('.png', 'Na.png') + '"/>';
 		deleteField = '<input id="remove_option_$option_number" class="remove_option" type="image" src="'
 				+ getDeleteIconOptions()
 				+ '" name="remove_option[$option_number]" />';
@@ -102,8 +105,9 @@ $(function() {
 		destroyHtmlEditor('feedback[' + id + ']');
 		$('tr#option_' + id, tableBody).remove();
 		doAjaxPost(
-				"./repository/content_object/assessment_matrix_question/php/ajax/assessment_matrix_question.php",
+				ajaxUri,
 				{
+					application: ajaxContext,
 					action : 'skip_option',
 					value : id
 				});
@@ -181,8 +185,9 @@ $(function() {
 		$('tr#match_' + id, tableBody).remove();
 
 		doAjaxPost(
-				"./repository/content_object/assessment_matrix_question/php/ajax/assessment_matrix_question.php",
+				ajaxUri,
 				{
+					application: ajaxContext,
 					action : 'skip_match',
 					value : id
 				});
