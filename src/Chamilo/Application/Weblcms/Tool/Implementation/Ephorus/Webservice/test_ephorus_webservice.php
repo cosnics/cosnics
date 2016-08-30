bfac92b880b1392a358a833d3c1310569aa1268b
Scaramanga <ScaraMcDuck@users.noreply.github.com>
2016-08-30 10:04:54 +0200

    Fixes for Ephorus in PHP7

--- src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Webservice/test_ephorus_webservice.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Webservice/test_ephorus_webservice.php
@@ -1,27 +1,23 @@
 <?php
+use GuzzleHttp\Exception\ServerException;
+require_once __DIR__ . '/../../../../../../Libraries/Architecture/Bootstrap.php';
+\Chamilo\Libraries\Architecture\Bootstrap::getInstance()->setup();
 
-$url = 'http://edison.hogent.be/ephorus_test/application/weblcms/tool/ephorus/' .
-    'php/webservice/ephorus_reporting_service.php';
+$url = 'http://localhost/connect/src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Webservice/ephorus_reporting_service.php';
+
+$client = new \GuzzleHttp\Client(['base_url' => $url]);
 
 $contents = file_get_contents(__DIR__ . '/sample.xml');
 
-$ch = curl_init();
+$request = $client->createRequest('POST', '', array('body' => $contents));
+$request->setHeader('SOAPAction', 'http://reporting.ephorus.org/');
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $contents);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-curl_setopt(
-    $ch, CURLOPT_HTTPHEADER,
-    array(
-        'Content-Type: text/xml; charset=utf-8',
-        'Content-Length: ' . strlen($contents)
-    )
-);
-
-$result = curl_exec($ch);
-
-echo '<pre>';
-
-curl_close($ch);
+try
+{
+    $response = $client->send($request);
+    var_dump($response->getBody()->__toString());
+}
+catch (ServerException $exception)
+{
+    var_dump($exception->getResponse()->getBody()->__toString());
+}
