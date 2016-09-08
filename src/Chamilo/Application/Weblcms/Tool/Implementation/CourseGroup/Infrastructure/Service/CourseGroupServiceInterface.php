4a8c62bf3c8525e461113280950a2e4694d37545
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-09-08 15:48:00 +0200

    [CourseCopier] Added the possibility to copy course groups to other courses

---
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/CourseGroup/Infrastructure/Service/CourseGroupServiceInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Chamilo\Application\Weblcms\Tool\Implementation\CourseGroup\Infrastructure\Service;
+
+use Chamilo\Application\Weblcms\Tool\Implementation\CourseGroup\Storage\DataClass\CourseGroup;
+use Chamilo\Core\User\Storage\DataClass\User;
+
+/**
+ * Interface for a course group service
+ *
+ * @author Sven Vanpoucke - Hogeschool Gent
+ */
+interface CourseGroupServiceInterface
+{
+    /**
+     * Creates a document category for a given course group
+     *
+     * @param CourseGroup $courseGroup
+     *
+     * @throws \Exception
+     */
+    public function createDocumentCategoryForCourseGroup(CourseGroup $courseGroup);
+
+    /**
+     * Creates a forum for a given course group
+     *
+     * @param CourseGroup $courseGroup
+     * @param User $user
+     *
+     * @throws \Exception
+     */
+    public function createForumCategoryAndPublicationForCourseGroup(CourseGroup $courseGroup, User $user);
+}
