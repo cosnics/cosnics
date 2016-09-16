a7ce5c9d15cbcc999e5b754cf96351428e1083d7
Pieterjan Broekaert <pjbro@users.noreply.github.com>
2016-09-16 14:50:24 +0200

    added missing class

--- src/Chamilo/Core/Repository/ContentObject/AssessmentOpenQuestion/Integration/Chamilo/Core/Repository/ContentObject/Assessment/Display/Display.php
+++ src/Chamilo/Core/Repository/ContentObject/AssessmentOpenQuestion/Integration/Chamilo/Core/Repository/ContentObject/Assessment/Display/Display.php
@@ -169,7 +169,7 @@ class Display extends QuestionDisplay
 
             $html[] = '<div class="panel-body">';
             $html[] = '<a id="' . $hint_name .
-                 '" class="btn btn-default"><span class="glyphicon glyphicon-gift"></span> ' .
+                 '" class="btn btn-default hint_button"><span class="glyphicon glyphicon-gift"></span> ' .
                  Translation::get('GetAHint') . '</a>';
             $html[] = '</div>';
 
