cdc0c92b91acaa877fbea3163d83197c5623bd85
vanpouckesven <vanpouckesven@localhost>
2016-10-04 11:39:18 +0200

    [User] Fixes for the leave tracker

--- src/Chamilo/Core/User/Ajax/Component/LeaveComponent.php
+++ src/Chamilo/Core/User/Ajax/Component/LeaveComponent.php
@@ -19,21 +19,20 @@ class LeaveComponent extends \Chamilo\Core\User\Ajax\Manager
 
     public function run()
     {
-        if (strpos($_SERVER['REQUEST_URI'], 'LeaveComponent.php') !== false &&
-             strpos($_SERVER['REQUEST_URI'], 'ajax') !== false)
-        {
-            $tracker = Request :: post('tracker');
-            $user_id = \Chamilo\Libraries\Platform\Session\Session :: get_user_id();
 
-            $return = Event :: trigger(
-                'Leave',
-                Manager :: context(),
-                array(
-                    Visit :: PROPERTY_ID => $tracker,
-                    Visit :: PROPERTY_LOCATION => null,
-                    Visit :: PROPERTY_USER_ID => $user_id));
-        }
+        $tracker = Request:: post('tracker');
+        $user_id = \Chamilo\Libraries\Platform\Session\Session:: get_user_id();
 
-        JsonAjaxResult :: success();
+        Event:: trigger(
+            'Leave',
+            Manager:: context(),
+            array(
+                Visit :: PROPERTY_ID => $tracker,
+                Visit :: PROPERTY_LOCATION => null,
+                Visit :: PROPERTY_USER_ID => $user_id
+            )
+        );
+
+        JsonAjaxResult:: success();
     }
 }
