e087157e42a846337c6b1abce62fed0282bd33ea
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-10-06 14:18:15 +0200

    [User Tool] Adding a status screen when the system fails to subscribe users

--- src/Chamilo/Application/Weblcms/Tool/Implementation/User/Component/SubscribeComponent.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/User/Component/SubscribeComponent.php
@@ -4,75 +4,115 @@ namespace Chamilo\Application\Weblcms\Tool\Implementation\User\Component;
 use Chamilo\Application\Weblcms\Rights\CourseManagementRights;
 use Chamilo\Application\Weblcms\Rights\WeblcmsRights;
 use Chamilo\Application\Weblcms\Tool\Implementation\User\Manager;
+use Chamilo\Core\User\Storage\DataClass\User;
+use Chamilo\Core\User\Storage\Repository\UserRepository;
+use Chamilo\Libraries\Architecture\Exceptions\NoObjectSelectedException;
 use Chamilo\Libraries\Architecture\Exceptions\NotAllowedException;
 use Chamilo\Libraries\Platform\Session\Request;
 use Chamilo\Libraries\Platform\Translation;
+use Chamilo\Libraries\Storage\Query\Condition\EqualityCondition;
+use Chamilo\Libraries\Storage\Query\Condition\InCondition;
+use Chamilo\Libraries\Storage\Query\Variable\PropertyConditionVariable;
 
 /**
  * $Id: subscribe.class.php 218 2009-11-13 14:21:26Z kariboe $
  *
  * @package application.lib.weblcms.weblcms_manager.component
  */
+
 /**
  * Weblcms component which allows the user to manage his or her course subscriptions
  */
 class SubscribeComponent extends Manager
 {
+    const STATUS_SUCCESS = 1;
+    const STATUS_FAILED_NOT_ALLOWED = 2;
+    const STATUS_FAILED_REQUEST = 3;
+    const STATUS_FAILED_UNKNOWN = 4;
 
     /**
      * Runs this component and displays its output.
      */
     public function run()
     {
-        if (! $this->is_allowed(WeblcmsRights :: EDIT_RIGHT))
+        if (!$this->is_allowed(WeblcmsRights :: EDIT_RIGHT))
         {
             throw new NotAllowedException();
         }
 
         $course_id = $this->get_course_id();
-        $users = $this->getRequest()->get(self :: PARAM_OBJECTS);
-        if (isset($users) && ! is_array($users))
+        $userIds = $this->getRequest()->get(self :: PARAM_OBJECTS);
+        if (isset($userIds) && !is_array($userIds))
         {
-            $users = array($users);
+            $userIds = array($userIds);
         }
 
         if (isset($course_id))
         {
-            if (isset($users) && count($users) > 0)
+            if (isset($userIds) && count($userIds) > 0)
             {
                 $failures = 0;
+                $usersStatus = array();
 
-                $course_management_rights = CourseManagementRights :: get_instance();
+                $course_management_rights = CourseManagementRights:: get_instance();
 
-                foreach ($users as $user_id)
+                $users = $this->findUsersByIds($userIds);
+
+                foreach ($users as $user)
                 {
-                    $status = Request :: get(self :: PARAM_STATUS) ? Request :: get(self :: PARAM_STATUS) : 5;
+                    $userId = $user->getId();
 
-                    if (\Chamilo\Application\Weblcms\Course\Storage\DataManager :: is_user_direct_subscribed_to_course(
-                        $user_id,
-                        $course_id) || (! $this->get_user()->is_platform_admin() && ! $course_management_rights->is_allowed(
-                        CourseManagementRights :: TEACHER_DIRECT_SUBSCRIBE_RIGHT,
-                        $course_id,
-                        CourseManagementRights :: TYPE_COURSE,
-                        $user_id)))
+                    $status = Request:: get(self :: PARAM_STATUS) ? Request:: get(self :: PARAM_STATUS) : 5;
+
+                    if (\Chamilo\Application\Weblcms\Course\Storage\DataManager:: is_user_direct_subscribed_to_course(
+                            $userId,
+                            $course_id
+                        ) || (!$this->get_user()->is_platform_admin() && !$course_management_rights->is_allowed(
+                                CourseManagementRights :: TEACHER_DIRECT_SUBSCRIBE_RIGHT,
+                                $course_id,
+                                CourseManagementRights :: TYPE_COURSE,
+                                $userId
+                            ))
+                    )
                     {
+                        $requestRight = $course_management_rights->is_allowed(
+                            CourseManagementRights::TEACHER_REQUEST_SUBSCRIBE_RIGHT,
+                            $course_id,
+                            CourseManagementRights :: TYPE_COURSE,
+                            $userId
+                        );
+
                         $failures ++;
+
+                        if($requestRight)
+                        {
+                            $usersStatus[self::STATUS_FAILED_REQUEST][] = $user;
+                        }
+                        else
+                        {
+                            $usersStatus[self::STATUS_FAILED_NOT_ALLOWED][] = $user;
+                        }
+
                         continue;
                     }
 
-                    if (! \Chamilo\Application\Weblcms\Course\Storage\DataManager :: subscribe_user_to_course(
+                    if (!\Chamilo\Application\Weblcms\Course\Storage\DataManager:: subscribe_user_to_course(
                         $course_id,
                         $status,
-                        $user_id))
+                        $userId
+                    )
+                    )
                     {
                         $failures ++;
+                        $usersStatus[self::STATUS_FAILED_UNKNOWN][] = $user;
+                        continue;
                     }
+
+                    $userStatus[self::STATUS_SUCCESS][] = $user;
                 }
 
                 if ($failures == 0)
                 {
-                    $success = true;
-
                     if (count($users) == 1)
                     {
                         $message = 'UserSubscribedToCourse';
@@ -81,32 +121,171 @@ class SubscribeComponent extends Manager
                     {
                         $message = 'UsersSubscribedToCourse';
                     }
-                }
-                elseif ($failures == count($users))
-                {
-                    $success = false;
 
-                    if (count($users) == 1)
-                    {
-                        $message = 'UserNotSubscribedToCourse';
-                    }
-                    else
-                    {
-                        $message = 'UsersNotSubscribedToCourse';
-                    }
+                    $this->redirect(
+                        Translation:: get($message), false,
+                        array(
+                            \Chamilo\Application\Weblcms\Tool\Manager :: PARAM_ACTION =>
+                                self :: ACTION_SUBSCRIBE_USER_BROWSER
+                        )
+                    );
                 }
                 else
                 {
-                    $success = false;
-                    $message = 'PartialUsersNotSubscribedToCourse';
+                    return $this->renderStatusPage($usersStatus);
                 }
-
-                $this->redirect(
-                    Translation :: get($message),
-                    ($success ? false : true),
-                    array(
-                        \Chamilo\Application\Weblcms\Tool\Manager :: PARAM_ACTION => self :: ACTION_SUBSCRIBE_USER_BROWSER));
+            }
+            else
+            {
+                throw new NoObjectSelectedException(
+                    Translation::getInstance()->getTranslation(
+                        'Users', null, Manager::context()
+                    )
+                );
             }
         }
+        else
+        {
+
+            throw new NoObjectSelectedException(
+                Translation::getInstance()->getTranslation(
+                    'Course', null, \Chamilo\Application\Weblcms\Manager::context()
+                )
+            );
+        }
+    }
+
+    /**
+     * Renders a detailed status page when there are users that could not be subscribed
+     *
+     * @param user[][] $usersStatus
+     *
+     * @return string
+     */
+    protected function renderStatusPage($usersStatus = array())
+    {
+        $translator = Translation::getInstance();
+
+        $html = array();
+
+        $html[] = $this->render_header();
+
+        $html[] = $this->renderUserList(
+            'panel-info', $translator->getTranslation('SubscribedUsers', null, Manager::context()),
+            $usersStatus[self::STATUS_SUCCESS]
+        );
+
+        $html[] = $this->renderUserList(
+            'panel-danger', $translator->getTranslation('FailedUsersNotAllowed', null, Manager::context()),
+            $usersStatus[self::STATUS_FAILED_NOT_ALLOWED]
+        );
+
+        $html[] = $this->renderRequestUserList($translator, $usersStatus[self::STATUS_FAILED_REQUEST]);
+
+        $html[] = $this->render_footer();
+
+        return implode(PHP_EOL, $html);
+    }
+
+    /**
+     * Renders the user list for users that must be requested
+     *
+     * @param Translation $translator
+     * @param User[] $requestUsers
+     *
+     * @return string
+     */
+    protected function renderRequestUserList(Translation $translator, array $requestUsers = array())
+    {
+        $requestUserIds = array();
+
+        foreach ($requestUsers as $requestUser)
+        {
+            $requestUserIds[] = $requestUser->getId();
+        }
+
+        $requestUrl = $this->get_url(
+            array(self::PARAM_ACTION => self::ACTION_REQUEST_SUBSCRIBE_USERS, self::PARAM_OBJECTS => $requestUserIds)
+        );
+
+        $additionalItems = array(
+            '<a href="' . $requestUrl . '">' .
+            '<button type="button" class="btn btn-default pull-right">' .
+            $translator->getTranslation('RequestUsers', null, Manager::context()) .
+            '</button><div class="clearfix"></div></a>'
+        );
+
+        return $this->renderUserList(
+            'panel-warning', $translator->getTranslation('FailedUsersCanOnlyBeRequested', null, Manager::context()),
+            $requestUsers, $additionalItems
+        );
+    }
+
+    /**
+     * Finds user objects by a given array of user ids
+     *
+     * @param int[] $userIds
+     *
+     * @return \Chamilo\Core\User\Storage\DataClass\User[]
+     */
+    protected function findUsersByIds(array $userIds = array())
+    {
+        $userRepository = new UserRepository();
+
+        return $userRepository->findUsers(
+            new InCondition(new PropertyConditionVariable(User::class_name(), User::PROPERTY_ID), $userIds)
+        );
+    }
+
+    /**
+     * Renders a status user list
+     *
+     * @param string $statusClass
+     * @param string $statusTitle
+     * @param User[] $users
+     * @param array $additionalItems
+     *
+     * @return string
+     */
+    protected function renderUserList($statusClass, $statusTitle, $users = array(), array $additionalItems = array())
+    {
+        if(empty($users))
+        {
+            return '';
+        }
+
+        $html = array();
+
+        $html[] = '<div class="panel ' . $statusClass . '">';
+        $html[] = '<div class="panel-heading">';
+        $html[] = '<h3 class="panel-title">';
+        $html[] = $statusTitle;
+        $html[] = '</h3>';
+        $html[] = '</div>';
+
+        $html[] = '<ul class="list-group">';
+        foreach ($users as $failedUser)
+        {
+            $html[] = '<li class="list-group-item">';
+            $html[] = '<h5 class="list-group-item-heading">';
+            $html[] = $failedUser->get_fullname();
+            $html[] = '</h5>';
+            $html[] = '<p class="list-group-item-text">';
+            $html[] = $failedUser->get_email();
+            $html[] = '</p>';
+            $html[] = '</li>';
+        }
+
+        foreach($additionalItems as $additionalItem)
+        {
+            $html[] = '<li class="list-group-item">';
+            $html[] = $additionalItem;
+            $html[] = '</li>';
+        }
+
+        $html[] = '</ul>';
+        $html[] = '</div>';
+
+        return implode(PHP_EOL, $html);
     }
 }
