18b9e622fe2c259d46805ff51e9ce042ea6de463
vanpouckesven <vanpouckesven@localhost>
2016-11-23 16:07:17 +0100

    [File] Added an option for an inline view (db upgrade required)

--- src/Chamilo/Core/Repository/ContentObject/File/Common/Rendition/Html/HtmlDescriptionRenditionImplementation.php
+++ src/Chamilo/Core/Repository/ContentObject/File/Common/Rendition/Html/HtmlDescriptionRenditionImplementation.php
@@ -29,21 +29,6 @@ class HtmlDescriptionRenditionImplementation extends HtmlRenditionImplementation
      */
     public function get_description()
     {
-        $object = $this->get_content_object();
-
-        $class = __NAMESPACE__ . '\Extension\HtmlInline' .
-             (string) StringUtilities :: getInstance()->createString($object->get_extension())->upperCamelize() .
-             'RenditionImplementation';
-
-        if (! class_exists($class))
-        {
-            $document_type = $object->determine_type();
-            $class = __NAMESPACE__ . '\Type\HtmlInline' .
-                 (string) StringUtilities :: getInstance()->createString($document_type)->upperCamelize() .
-                 'RenditionImplementation';
-        }
-
-        $rendition = new $class($this->get_context(), $this->get_content_object());
-        return $rendition->render();
+        return $this->renderInline();
     }
 }
