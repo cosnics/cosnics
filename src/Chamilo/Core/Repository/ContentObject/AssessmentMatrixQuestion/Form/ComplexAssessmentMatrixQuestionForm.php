1d791113cad94425a9d4852d237b6ab0fb3455c8
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-12-05 14:49:42 +0100

    [Assessment] Fixed an error in the complex question form where the answer option was never selected

--- src/Chamilo/Core/Repository/ContentObject/AssessmentMatrixQuestion/Form/ComplexAssessmentMatrixQuestionForm.php
+++ src/Chamilo/Core/Repository/ContentObject/AssessmentMatrixQuestion/Form/ComplexAssessmentMatrixQuestionForm.php
@@ -70,7 +70,16 @@ class ComplexAssessmentMatrixQuestionForm extends ComplexContentObjectItemForm
             $defaults[ComplexAssessmentMatrixQuestion::PROPERTY_WEIGHT] = $complex_content_object_item->get_weight() ? $complex_content_object_item->get_weight() : 0;
             $defaults[self::PROPERTY_RECALCULATE_WEIGHT] = 1;
             $defaults[ComplexAssessmentMatrixQuestion::PROPERTY_RANDOM] = $complex_content_object_item->get_random() ? $complex_content_object_item->get_random() : 0;
-            $defaults[ComplexAssessmentMatrixQuestion::PROPERTY_SHOW_ANSWER_FEEDBACK] = $complex_content_object_item->get_show_answer_feedback();
+
+            if ($complex_content_object_item->get_show_answer_feedback() == Configuration::ANSWER_FEEDBACK_TYPE_NONE)
+            {
+                $defaults[self::PROPERTY_ANSWER_FEEDBACK_OPTION] = 0;
+            }
+            else
+            {
+                $defaults[self::PROPERTY_ANSWER_FEEDBACK_OPTION] = 1;
+                $defaults[Configuration::PROPERTY_SHOW_ANSWER_FEEDBACK] = $complex_content_object_item->get_show_answer_feedback();
+            }
         }
         
         return $defaults;
@@ -107,7 +116,11 @@ class ComplexAssessmentMatrixQuestionForm extends ComplexContentObjectItemForm
         }
         
         $complex_content_object_item->set_random($values[ComplexAssessmentMatrixQuestion::PROPERTY_RANDOM]);
-        $complex_content_object_item->set_show_answer_feedback(
-            $values[ComplexAssessmentMatrixQuestion::PROPERTY_SHOW_ANSWER_FEEDBACK]);
+
+        $answerFeedback = ($values[self::PROPERTY_ANSWER_FEEDBACK_OPTION] == 1) ?
+            $values[ComplexAssessmentMatrixQuestion::PROPERTY_SHOW_ANSWER_FEEDBACK] :
+            Configuration::ANSWER_FEEDBACK_TYPE_NONE;
+
+        $complex_content_object_item->set_show_answer_feedback($answerFeedback);
     }
 }
--- src/Chamilo/Core/Repository/ContentObject/FillInBlanksQuestion/Form/ComplexFillInBlanksQuestionForm.php
+++ src/Chamilo/Core/Repository/ContentObject/FillInBlanksQuestion/Form/ComplexFillInBlanksQuestionForm.php
@@ -70,7 +70,16 @@ class ComplexFillInBlanksQuestionForm extends ComplexContentObjectItemForm
             $defaults[ComplexFillInBlanksQuestion::PROPERTY_WEIGHT] = $complex_content_object_item->get_weight() ? $complex_content_object_item->get_weight() : 0;
             $defaults[self::PROPERTY_RECALCULATE_WEIGHT] = 1;
             $defaults[ComplexFillInBlanksQuestion::PROPERTY_RANDOM] = $complex_content_object_item->get_random() ? $complex_content_object_item->get_random() : 0;
-            $defaults[ComplexFillInBlanksQuestion::PROPERTY_SHOW_ANSWER_FEEDBACK] = $complex_content_object_item->get_show_answer_feedback();
+
+            if ($complex_content_object_item->get_show_answer_feedback() == Configuration::ANSWER_FEEDBACK_TYPE_NONE)
+            {
+                $defaults[self::PROPERTY_ANSWER_FEEDBACK_OPTION] = 0;
+            }
+            else
+            {
+                $defaults[self::PROPERTY_ANSWER_FEEDBACK_OPTION] = 1;
+                $defaults[Configuration::PROPERTY_SHOW_ANSWER_FEEDBACK] = $complex_content_object_item->get_show_answer_feedback();
+            }
         }
         
         return $defaults;
@@ -107,7 +116,11 @@ class ComplexFillInBlanksQuestionForm extends ComplexContentObjectItemForm
         }
         
         $complex_content_object_item->set_random($values[ComplexFillInBlanksQuestion::PROPERTY_RANDOM]);
-        $complex_content_object_item->set_show_answer_feedback(
-            $values[ComplexFillInBlanksQuestion::PROPERTY_SHOW_ANSWER_FEEDBACK]);
+
+        $answerFeedback = ($values[self::PROPERTY_ANSWER_FEEDBACK_OPTION] == 1) ?
+            $values[ComplexFillInBlanksQuestion::PROPERTY_SHOW_ANSWER_FEEDBACK] :
+            Configuration::ANSWER_FEEDBACK_TYPE_NONE;
+
+        $complex_content_object_item->set_show_answer_feedback($answerFeedback);
     }
 }
