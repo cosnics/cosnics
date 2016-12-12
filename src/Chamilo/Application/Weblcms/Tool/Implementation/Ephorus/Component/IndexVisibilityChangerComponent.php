5089b58b80524a33f47cf88a462f43d330a4ebb1
vanpouckesven <vanpouckesven@users.noreply.github.com>
2016-12-12 14:01:05 +0100

    [Multiple] Fixing a few minor merge issues

--- src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Component/IndexVisibilityChangerComponent.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Component/IndexVisibilityChangerComponent.php
@@ -141,7 +141,7 @@ class IndexVisibilityChangerComponent extends Manager implements RequestSupport
             $assignmentTableIds);
         
         $data_manager_class = $this->get_data_manager_class();
-        $doctrineExtension = new DoctrineExtension($data_manager_class::get_instance());
+        $doctrineExtension = new DoctrineExtension($data_manager_class::getInstance());
 
         $requests = $doctrineExtension->retrieve_results_by_assignment(
             new DataClassRetrievesParameters($condition)
