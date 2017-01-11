9bc326eaf707fb56c0d607f1d2ab60c66fc7f676
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-01-11 15:33:02 +0100

    [Repository] Excluding document downloader from authorization checker

--- src/Chamilo/Core/Repository/Manager.php
+++ src/Chamilo/Core/Repository/Manager.php
@@ -180,7 +180,7 @@ abstract class Manager extends Application
     {
         parent::__construct($applicationConfiguration);
 
-        if($this->getUser() && $this->get_action() != self::ACTION_DOWNLOAD_DOCUMENT)
+        if($this->getUser() && $this->getRequest()->get(self::PARAM_ACTION) != self::ACTION_DOWNLOAD_DOCUMENT)
         {
             $this->checkAuthorization(Manager::context());
         }
