1cb15996e512663ecc16da7f21f8069d5071888c
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-01-12 15:11:38 +0100

    [Weblcms] Removed course type from open courses list

--- src/Chamilo/Application/Weblcms/Course/OpenCourse/Table/OpenCourseTableColumnModel.php
+++ src/Chamilo/Application/Weblcms/Course/OpenCourse/Table/OpenCourseTableColumnModel.php
@@ -2,6 +2,7 @@
 namespace Chamilo\Application\Weblcms\Course\OpenCourse\Table;
 
 use Chamilo\Application\Weblcms\Course\OpenCourse\Manager;
+use Chamilo\Application\Weblcms\Course\Storage\DataClass\Course;
 use Chamilo\Application\Weblcms\Course\Table\CourseTable\CourseTableColumnModel;
 use Chamilo\Core\User\Roles\Storage\DataClass\Role;
 use Chamilo\Libraries\Format\Table\Column\DataClassPropertyTableColumn;
@@ -10,7 +11,7 @@ use Chamilo\Libraries\Platform\Translation;
 
 /**
  * This class describes the column model for the open course table
- * 
+ *
  * @author Sven Vanpoucke - Hogeschool Gent
  */
 class OpenCourseTableColumnModel extends CourseTableColumnModel implements TableColumnModelActionsColumnSupport
@@ -21,16 +22,23 @@ class OpenCourseTableColumnModel extends CourseTableColumnModel implements Table
      */
     public function initialize_columns()
     {
-        parent::initialize_columns();
-        
+        $this->add_column(new DataClassPropertyTableColumn(Course::class_name(), Course::PROPERTY_VISUAL_CODE));
+        $this->add_column(new DataClassPropertyTableColumn(Course::class_name(), Course::PROPERTY_TITLE));
+
+        $this->add_column(
+            new DataClassPropertyTableColumn(Course::class_name(), Course::PROPERTY_TITULAR_ID, null, false)
+        );
+
         if ($this->get_component()->isAuthorized(Manager::context(), 'ManageOpenCourses'))
         {
             $this->add_column(
                 new DataClassPropertyTableColumn(
-                    Role::class_name(), 
-                    Role::PROPERTY_ROLE, 
-                    Translation::getInstance()->getTranslation('Role', null, Manager::context()), 
-                    false));
+                    Role::class_name(),
+                    Role::PROPERTY_ROLE,
+                    Translation::getInstance()->getTranslation('Role', null, Manager::context()),
+                    false
+                )
+            );
         }
     }
 }
