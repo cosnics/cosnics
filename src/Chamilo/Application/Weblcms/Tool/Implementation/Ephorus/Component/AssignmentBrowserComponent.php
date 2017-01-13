9f7b84d7610523ca752e47acabc651adbc63714c
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-01-13 16:38:32 +0100

    [Ephorus] Fixed search conditions

--- src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Component/AssignmentBrowserComponent.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Component/AssignmentBrowserComponent.php
@@ -21,7 +21,7 @@ use Chamilo\Libraries\Storage\Query\Variable\StaticConditionVariable;
 
 /**
  * Assignment browser component for ephorus tool.
- * 
+ *
  * @author Tom Goethals - Hogeschool Gent
  */
 class AssignmentBrowserComponent extends Manager implements TableSupport, RequestTableInterface
@@ -38,7 +38,7 @@ class AssignmentBrowserComponent extends Manager implements TableSupport, Reques
      * Inherited functionality *
      * **************************************************************************************************************
      */
-    
+
     /**
      * Runs this component and displays it's output
      */
@@ -47,11 +47,11 @@ class AssignmentBrowserComponent extends Manager implements TableSupport, Reques
         if ($this->is_allowed(WeblcmsRights::EDIT_RIGHT))
         {
             $html = array();
-            
+
             $html[] = $this->render_header();
             $html[] = $this->as_html();
             $html[] = $this->render_footer();
-            
+
             return implode(PHP_EOL, $html);
         }
         else
@@ -65,10 +65,10 @@ class AssignmentBrowserComponent extends Manager implements TableSupport, Reques
      * Implemented functionality *
      * **************************************************************************************************************
      */
-    
+
     /**
      * Returns the condition for the object table
-     * 
+     *
      * @param $object_table_class_name string
      *
      * @return \libraries\storage\Condition
@@ -76,20 +76,28 @@ class AssignmentBrowserComponent extends Manager implements TableSupport, Reques
     public function get_table_condition($object_table_class_name)
     {
         if ($object_table_class_name ==
-             'Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Table\Assignment\AssignmentRequestTable')
+            'Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Table\Assignment\AssignmentRequestTable'
+        )
         {
             $search_conditions = $this->buttonToolbarRenderer->getConditions(
-                array(ContentObject::PROPERTY_TITLE, ContentObject::PROPERTY_DESCRIPTION));
+                array(
+                    new PropertyConditionVariable(ContentObject::class_name(), ContentObject::PROPERTY_TITLE),
+                    new PropertyConditionVariable(ContentObject::class_name(), ContentObject::PROPERTY_DESCRIPTION)
+                )
+            );
+
             $condition = new EqualityCondition(
                 new PropertyConditionVariable(
-                    AssignmentSubmission::class_name(), 
-                    AssignmentSubmission::PROPERTY_PUBLICATION_ID), 
-                new StaticConditionVariable($this->get_publication_id()));
+                    AssignmentSubmission::class_name(),
+                    AssignmentSubmission::PROPERTY_PUBLICATION_ID
+                ),
+                new StaticConditionVariable($this->get_publication_id())
+            );
             if ($search_conditions != null)
             {
                 $condition = new AndCondition(array($condition, $search_conditions));
             }
-            
+
             return $condition;
         }
     }
@@ -97,12 +105,13 @@ class AssignmentBrowserComponent extends Manager implements TableSupport, Reques
     public function get_publication_id()
     {
         return \Chamilo\Libraries\Platform\Session\Request::get(
-            \Chamilo\Application\Weblcms\Manager::PARAM_PUBLICATION);
+            \Chamilo\Application\Weblcms\Manager::PARAM_PUBLICATION
+        );
     }
 
     /**
      * Returns the url to the ephorus request component
-     * 
+     *
      * @param int $object_id
      *
      * @return string
@@ -111,8 +120,9 @@ class AssignmentBrowserComponent extends Manager implements TableSupport, Reques
     {
         $parameters[self::PARAM_ACTION] = self::ACTION_EPHORUS_REQUEST;
         $parameters[self::PARAM_CONTENT_OBJECT_IDS] = $object_id;
-        $parameters[\Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Request\Manager::PARAM_ACTION] = \Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Request\Manager::ACTION_VIEW_RESULT;
-        
+        $parameters[\Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Request\Manager::PARAM_ACTION] =
+            \Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Request\Manager::ACTION_VIEW_RESULT;
+
         return $this->get_url($parameters);
     }
 
@@ -121,49 +131,51 @@ class AssignmentBrowserComponent extends Manager implements TableSupport, Reques
      * Helper functionality *
      * **************************************************************************************************************
      */
-    
+
     /**
      * Returns this component as html
-     * 
+     *
      * @return string
      */
     protected function as_html()
     {
         $html = array();
-        
+
         $this->buttonToolbarRenderer = $this->getButtonToolbarRenderer();
         $html[] = $this->buttonToolbarRenderer->render();
-        
+
         $pub = \Chamilo\Application\Weblcms\Storage\DataManager::retrieve_by_id(
-            ContentObjectPublication::class_name(), 
-            $this->get_publication_id());
-        
+            ContentObjectPublication::class_name(),
+            $this->get_publication_id()
+        );
+
         $assignment = $pub->get_content_object();
-        
+
         $html[] = '<h3>' . Translation::get(
-            'EphorusSubmissionsForAssignment', 
-            array(), 
-            ClassnameUtilities::getInstance()->getNamespaceFromClassname(self::class_name())) . ': ' .
-             $assignment->get_title() . '</h3>';
+                'EphorusSubmissionsForAssignment',
+                array(),
+                ClassnameUtilities::getInstance()->getNamespaceFromClassname(self::class_name())
+            ) . ': ' .
+            $assignment->get_title() . '</h3>';
         $table = new AssignmentRequestTable($this);
         $html[] = $table->as_html();
-        
+
         return implode(PHP_EOL, $html);
     }
 
     /**
      * Returns the actionbar
-     * 
+     *
      * @return ButtonToolBarRenderer
      */
     protected function getButtonToolbarRenderer()
     {
-        if (! isset($this->buttonToolbarRenderer))
+        if (!isset($this->buttonToolbarRenderer))
         {
             $buttonToolbar = new ButtonToolBar($this->get_url());
             $this->buttonToolbarRenderer = new ButtonToolBarRenderer($buttonToolbar);
         }
-        
+
         return $this->buttonToolbarRenderer;
     }
 
