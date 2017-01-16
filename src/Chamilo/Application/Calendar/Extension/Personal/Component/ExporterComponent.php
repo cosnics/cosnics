e9629291c7e32c9ef093337fd31e5e9418be77d6
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-01-16 17:20:04 +0100

    [Calendar] Fixed the exporter

--- src/Chamilo/Application/Calendar/Extension/Personal/Component/ExporterComponent.php
+++ src/Chamilo/Application/Calendar/Extension/Personal/Component/ExporterComponent.php
@@ -7,6 +7,9 @@ use Chamilo\Application\Calendar\Extension\Personal\Storage\DataManager;
 use Chamilo\Core\Repository\Common\Export\ContentObjectExport;
 use Chamilo\Core\Repository\Common\Export\ContentObjectExportController;
 use Chamilo\Core\Repository\Common\Export\ExportParameters;
+use Chamilo\Core\Repository\Workspace\PersonalWorkspace;
+use Chamilo\Libraries\Architecture\Exceptions\NoObjectSelectedException;
+use Chamilo\Libraries\Architecture\Exceptions\ObjectNotExistException;
 use Chamilo\Libraries\Platform\Session\Request;
 use Chamilo\Libraries\Platform\Translation;
 use Chamilo\Libraries\Utilities\Utilities;
@@ -24,22 +27,31 @@ class ExporterComponent extends Manager
      */
     public function run()
     {
+        $objectTranslation = Translation::getInstance()->getTranslation(
+            'PersonalCalendarPublication', null, Manager::context()
+        );
+
         $id = Request::get(self::PARAM_PUBLICATION_ID);
-        if ($id)
+        if (!$id)
         {
-            $calendar_event_publication = DataManager::retrieve_by_id(Publication::class_name(), $id);
-            
-            $parameters = new ExportParameters(
-                $this->get_user_id(), 
-                ContentObjectExport::FORMAT_ICAL, 
-                array($calendar_event_publication->get_content_object_id()));
-            $exporter = ContentObjectExportController::factory($parameters);
-            $exporter->download();
+            throw new NoObjectSelectedException($objectTranslation);
         }
-        else
+
+        $calendar_event_publication = DataManager::retrieve_by_id(Publication::class_name(), $id);
+
+        if (!$calendar_event_publication instanceof Publication)
         {
-            return $this->display_error_page(
-                htmlentities(Translation::get('NoObjectsSelected', null, Utilities::COMMON_LIBRARIES)));
+            throw new ObjectNotExistException($objectTranslation, $id);
         }
+
+        $parameters = new ExportParameters(
+            new PersonalWorkspace($calendar_event_publication->get_publication_publisher()),
+            $this->getUser()->getId(),
+            ContentObjectExport::FORMAT_ICAL,
+            array($calendar_event_publication->get_content_object_id())
+        );
+
+        $exporter = ContentObjectExportController::factory($parameters);
+        $exporter->download();
     }
 }
