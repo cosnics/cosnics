4dcaeeda7704ac5d538ae883eabc00d88c897a60
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-01-16 16:41:31 +0100

    [Ephorus] Bugfix in the browser when searching

--- src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Component/BrowserComponent.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/Ephorus/Component/BrowserComponent.php
@@ -25,7 +25,7 @@ use Chamilo\Libraries\Storage\Query\Variable\StaticConditionVariable;
 
 /**
  * Browser component for ephorus tool.
- * 
+ *
  * @author Tom Goethals - Hogeschool Gent
  * @author Sven Vanpoucke - Hogeschool Gent
  */
@@ -43,18 +43,18 @@ class BrowserComponent extends Manager implements TableSupport, RequestTableInte
      * Inherited functionality *
      * **************************************************************************************************************
      */
-    
+
     /**
      * Runs this component and displays it's output
      */
     public function run()
     {
         $html = array();
-        
+
         $html[] = $this->render_header();
         $html[] = $this->as_html();
         $html[] = $this->render_footer();
-        
+
         return implode(PHP_EOL, $html);
     }
 
@@ -63,10 +63,10 @@ class BrowserComponent extends Manager implements TableSupport, RequestTableInte
      * Implemented functionality *
      * **************************************************************************************************************
      */
-    
+
     /**
      * Returns the condition for the object table
-     * 
+     *
      * @param $object_table_class_name string
      *
      * @return \libraries\storage\Condition
@@ -74,25 +74,32 @@ class BrowserComponent extends Manager implements TableSupport, RequestTableInte
     public function get_table_condition($object_table_class_name)
     {
         if ($object_table_class_name ==
-             'Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Table\Request\RequestTable')
+            'Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Table\Request\RequestTable'
+        )
         {
             $search_conditions = $this->buttonToolbarRenderer->getConditions(
-                array(ContentObject::PROPERTY_TITLE, ContentObject::PROPERTY_DESCRIPTION));
+                array(
+                    new PropertyConditionVariable(ContentObject::class_name(), ContentObject::PROPERTY_TITLE),
+                    new PropertyConditionVariable(ContentObject::class_name(), ContentObject::PROPERTY_DESCRIPTION)
+                )
+            );
+
             $condition = new EqualityCondition(
-                new PropertyConditionVariable(Request::class_name(), Request::PROPERTY_COURSE_ID), 
-                new StaticConditionVariable($this->get_course_id()));
+                new PropertyConditionVariable(Request::class_name(), Request::PROPERTY_COURSE_ID),
+                new StaticConditionVariable($this->get_course_id())
+            );
             if ($search_conditions != null)
             {
                 $condition = new AndCondition(array($condition, $search_conditions));
             }
-            
+
             return $condition;
         }
     }
 
     /**
      * Returns the url to the ephorus request component
-     * 
+     *
      * @param int $object_id
      *
      * @return string
@@ -101,8 +108,9 @@ class BrowserComponent extends Manager implements TableSupport, RequestTableInte
     {
         $parameters[self::PARAM_ACTION] = self::ACTION_EPHORUS_REQUEST;
         $parameters[self::PARAM_CONTENT_OBJECT_IDS] = $object_id;
-        $parameters[\Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Request\Manager::PARAM_ACTION] = \Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Request\Manager::ACTION_VIEW_RESULT;
-        
+        $parameters[\Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Request\Manager::PARAM_ACTION] =
+            \Chamilo\Application\Weblcms\Tool\Implementation\Ephorus\Request\Manager::ACTION_VIEW_RESULT;
+
         return $this->get_url($parameters);
     }
 
@@ -111,10 +119,10 @@ class BrowserComponent extends Manager implements TableSupport, RequestTableInte
      * Helper functionality *
      * **************************************************************************************************************
      */
-    
+
     /**
      * Returns this component as html
-     * 
+     *
      * @return string
      */
     protected function as_html()
@@ -124,10 +132,10 @@ class BrowserComponent extends Manager implements TableSupport, RequestTableInte
             $html = array();
             $this->buttonToolbarRenderer = $this->getButtonToolbarRenderer();
             $html[] = $this->buttonToolbarRenderer->render();
-            
+
             $table = new RequestTable($this);
             $html[] = $table->as_html();
-            
+
             return implode(PHP_EOL, $html);
         }
         else
@@ -138,33 +146,38 @@ class BrowserComponent extends Manager implements TableSupport, RequestTableInte
 
     /**
      * Returns the actionbar
-     * 
+     *
      * @return ButtonToolBarRenderer
      */
     protected function getButtonToolbarRenderer()
     {
-        if (! isset($this->buttonToolbarRenderer))
+        if (!isset($this->buttonToolbarRenderer))
         {
             $buttonToolbar = new ButtonToolBar($this->get_url());
             $commonActions = new ButtonGroup();
-            
+
             $commonActions->addButton(
                 new Button(
                     Translation::get(
-                        'AddDocument', 
-                        array(), 
-                        ClassnameUtilities::getInstance()->getNamespaceFromClassname(self::class_name())), 
-                    Theme::getInstance()->getCommonImagePath('Action/Add'), 
+                        'AddDocument',
+                        array(),
+                        ClassnameUtilities::getInstance()->getNamespaceFromClassname(self::class_name())
+                    ),
+                    Theme::getInstance()->getCommonImagePath('Action/Add'),
                     $this->get_url(
                         array(
-                            \Chamilo\Application\Weblcms\Tool\Manager::PARAM_ACTION => self::ACTION_PUBLISH_DOCUMENT)), 
-                    ToolbarItem::DISPLAY_ICON_AND_LABEL));
-            
+                            \Chamilo\Application\Weblcms\Tool\Manager::PARAM_ACTION => self::ACTION_PUBLISH_DOCUMENT
+                        )
+                    ),
+                    ToolbarItem::DISPLAY_ICON_AND_LABEL
+                )
+            );
+
             $buttonToolbar->addButtonGroup($commonActions);
-            
+
             $this->buttonToolbarRenderer = new ButtonToolBarRenderer($buttonToolbar);
         }
-        
+
         return $this->buttonToolbarRenderer;
     }
 
