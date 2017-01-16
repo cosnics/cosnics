4ef7db7f99966c7c263b500dda83086e8233bacf
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-01-16 17:25:48 +0100

    [Reporting] Link bugfix

--- src/Chamilo/Application/Weblcms/Integration/Chamilo/Core/Reporting/Block/Tool/LastAccessToToolsUserBlock.php
+++ src/Chamilo/Application/Weblcms/Integration/Chamilo/Core/Reporting/Block/Tool/LastAccessToToolsUserBlock.php
@@ -17,12 +17,12 @@ class LastAccessToToolsUserBlock extends LastAccessToToolsBlock
     public function count_data()
     {
         $reporting_data = ToolAccessBlock::count_data();
-        
+
         $reporting_data->add_row(Translation::get('ViewPublications'));
-        
+
         $img = '<img src="' . Theme::getInstance()->getCommonImagePath('Action/Reporting') . '" title="' .
-             Translation::get('ViewPublications') . '" />';
-        
+            Translation::get('ViewPublications') . '" />';
+
         $tool_names = $reporting_data->get_categories();
         foreach ($tool_names as $tool_name)
         {
@@ -30,33 +30,38 @@ class LastAccessToToolsUserBlock extends LastAccessToToolsBlock
             if ($publications > 0)
             {
                 $params = $this->get_parent()->get_parameters();
-                $params[\Chamilo\Application\Weblcms\Manager::PARAM_TEMPLATE_ID] = ToolPublicationsDetailTemplate::class_name();
+                $params[\Chamilo\Application\Weblcms\Manager::PARAM_TEMPLATE_ID] =
+                    ToolPublicationsDetailTemplate::class_name();
                 $params[\Chamilo\Application\Weblcms\Manager::PARAM_USERS] = $this->get_user_id();
-                $params[\Chamilo\Application\Weblcms\Tool\Implementation\Reporting\Manager::PARAM_REPORTING_TOOL] = $tool_name;
-                $link_pub = '<a href="' . $this->get_parent()->get_url($params) . '">' . $img . '</a>';
-                
+                $params[\Chamilo\Application\Weblcms\Tool\Implementation\Reporting\Manager::PARAM_REPORTING_TOOL] =
+                    $tool_name;
+                $link_pub = '<a href="' . $this->get_parent()->get_url(
+                        $params, array(\Chamilo\Core\Reporting\Viewer\Manager::PARAM_BLOCK_ID)
+                    ) . '">' . $img . '</a>';
+
                 $reporting_data->add_data_category_row($tool_name, Translation::get('ViewPublications'), $link_pub);
             }
         }
-        
+
         return $reporting_data;
     }
 
     /**
      * Returns the summary data for this course
-     * 
+     *
      * @return RecordResultSet
      */
     public function retrieve_course_summary_data()
     {
         return WeblcmsTrackingDataManager::retrieve_tools_access_summary_data(
-            $this->get_course_id(), 
-            $this->get_user_id());
+            $this->get_course_id(),
+            $this->get_user_id()
+        );
     }
 
     /**
      * Returns the condition for the tools publication count
-     * 
+     *
      * @param string $tool_name
      *
      * @return AndCondition
@@ -64,15 +69,17 @@ class LastAccessToToolsUserBlock extends LastAccessToToolsBlock
     public function get_tool_publications_condition($tool_name)
     {
         $conditions = array();
-        
+
         $conditions[] = parent::get_tool_publications_condition($tool_name);
-        
+
         $conditions[] = new EqualityCondition(
             new PropertyConditionVariable(
-                ContentObjectPublication::class_name(), 
-                ContentObjectPublication::PROPERTY_PUBLISHER_ID), 
-            new StaticConditionVariable($this->get_user_id()));
-        
+                ContentObjectPublication::class_name(),
+                ContentObjectPublication::PROPERTY_PUBLISHER_ID
+            ),
+            new StaticConditionVariable($this->get_user_id())
+        );
+
         return new AndCondition($conditions);
     }
 }
