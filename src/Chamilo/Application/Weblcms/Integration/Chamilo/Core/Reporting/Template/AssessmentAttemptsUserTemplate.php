5c3ba6148ba4da59491b6ce01e16a6cc0029ab86
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-01-23 15:51:06 +0100

    [Weblcms] Added an additional check if a publication exists or not

--- src/Chamilo/Application/Weblcms/Integration/Chamilo/Core/Reporting/Template/AssessmentAttemptsUserTemplate.php
+++ src/Chamilo/Application/Weblcms/Integration/Chamilo/Core/Reporting/Template/AssessmentAttemptsUserTemplate.php
@@ -5,6 +5,7 @@ use Chamilo\Application\Weblcms\Integration\Chamilo\Core\Reporting\Block\Assessm
 use Chamilo\Application\Weblcms\Integration\Chamilo\Core\Reporting\Block\Assessment\AssessmentUserInformationBlock;
 use Chamilo\Application\Weblcms\Storage\DataClass\ContentObjectPublication;
 use Chamilo\Core\Reporting\ReportingTemplate;
+use Chamilo\Libraries\Architecture\Exceptions\ObjectNotExistException;
 use Chamilo\Libraries\Format\Structure\Breadcrumb;
 use Chamilo\Libraries\Format\Structure\BreadcrumbTrail;
 use Chamilo\Libraries\Platform\Session\Request;
@@ -23,11 +24,11 @@ class AssessmentAttemptsUserTemplate extends ReportingTemplate
     public function __construct($parent)
     {
         parent::__construct($parent);
-        
+
         $this->initialize_parameters();
         $this->add_reporting_block(new AssessmentUserInformationBlock($this));
         $this->add_reporting_block(new AssessmentAttemptsUserBlock($this));
-        
+
         $this->add_breadcrumbs();
     }
 
@@ -35,17 +36,17 @@ class AssessmentAttemptsUserTemplate extends ReportingTemplate
     {
         $this->publication_id = Request::get(\Chamilo\Application\Weblcms\Manager::PARAM_PUBLICATION);
         $this->user_id = Request::get(\Chamilo\Application\Weblcms\Manager::PARAM_USERS);
-        
+
         if ($this->publication_id)
         {
             $this->set_parameter(\Chamilo\Application\Weblcms\Manager::PARAM_PUBLICATION, $this->publication_id);
         }
-        
+
         if ($this->user_id)
         {
             $this->set_parameter(\Chamilo\Application\Weblcms\Manager::PARAM_USERS, $this->user_id);
         }
-        
+
         $course_id = Request::get(\Chamilo\Application\Weblcms\Manager::PARAM_COURSE);
         if ($course_id)
         {
@@ -58,34 +59,51 @@ class AssessmentAttemptsUserTemplate extends ReportingTemplate
      */
     protected function add_breadcrumbs()
     {
-        $assessment = \Chamilo\Application\Weblcms\Storage\DataManager::retrieve_by_id(
-            ContentObjectPublication::class_name(), 
-            $this->publication_id)->get_content_object();
-        
+        $publication = \Chamilo\Application\Weblcms\Storage\DataManager::retrieve_by_id(
+            ContentObjectPublication::class_name(),
+            $this->publication_id
+        );
+
+        if (!$publication instanceof ContentObjectPublication)
+        {
+            throw new ObjectNotExistException(
+                Translation::getInstance()->getTranslation(
+                    'ContentObjectPublication', null, 'Chamilo\Application\Weblcms'
+                ), $this->publication_id
+            );
+        }
+
+        $assessment = $publication->get_content_object();
+
         $trail = BreadcrumbTrail::getInstance();
-        
+
         $trail->add(
             new Breadcrumb(
                 $this->get_url(
-                    array(\Chamilo\Core\Reporting\Viewer\Manager::PARAM_BLOCK_ID => 2), 
-                    array(\Chamilo\Application\Weblcms\Manager::PARAM_TEMPLATE_ID)), 
-                Translation::get('Assessments')));
-        
+                    array(\Chamilo\Core\Reporting\Viewer\Manager::PARAM_BLOCK_ID => 2),
+                    array(\Chamilo\Application\Weblcms\Manager::PARAM_TEMPLATE_ID)
+                ),
+                Translation::get('Assessments')
+            )
+        );
+
         $filters = array(\Chamilo\Application\Weblcms\Manager::PARAM_USERS);
-        
+
         $params = array();
         $params[\Chamilo\Application\Weblcms\Manager::PARAM_TEMPLATE_ID] = AssessmentAttemptsTemplate::class_name();
         $params[\Chamilo\Application\Weblcms\Manager::PARAM_PUBLICATION] = $this->publication_id;
-        
+
         $trail->add(new Breadcrumb($this->get_url($params, $filters), $assessment->get_title()));
-        
+
         $params[\Chamilo\Core\Reporting\Viewer\Manager::PARAM_BLOCK_ID] = 1;
-        
+
         $trail->add(new Breadcrumb($this->get_url($params, $filters), Translation::get('Users')));
-        
+
         $trail->add(
             new Breadcrumb(
-                $this->get_url(), 
-                \Chamilo\Core\User\Storage\DataManager::get_fullname_from_user($this->user_id)));
+                $this->get_url(),
+                \Chamilo\Core\User\Storage\DataManager::get_fullname_from_user($this->user_id)
+            )
+        );
     }
 }
