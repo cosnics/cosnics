26c04de0cb80df0fbd12e7195ed4dc08382ced9f
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-02-14 17:27:46 +0100

    [Ckeditor] Added the default css stylesheet to ckeditor due to views not beeing the same in rendering and ckeditor

--- src/Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor.js
+++ src/Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor.js
@@ -1,54 +1,57 @@
 ﻿/*
-Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
+ Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function(config)
-{
+CKEDITOR.editorConfig = function (config) {
     config.uiColor = '#F5F5F5';
-    config.plugins = 'a11yhelp,about,basicstyles,bidi,blockquote,button,clipboard,colorbutton,colordialog,contextmenu,dialog,dialogadvtab,dialogui,div,enterkey,entities,fakeobjects,find,flash,floatingspace,floatpanel,font,format,forms,horizontalrule,htmlwriter,iframe,image,indent,indentblock,indentlist,justify,link,list,listblock,liststyle,magicline,maximize,menu,menubutton,newpage,pagebreak,panel,panelbutton,pastefromword,pastetext,popup,preview,print,removeformat,resize,richcombo,save,scayt,selectall,showblocks,showborders,smiley,sourcearea,specialchar,stylescombo,tab,table,tabletools,templates,toolbar,undo,wsc,wysiwygarea,chamilo,chamilofakeobjects,eqneditor,oembed,widget';
-    
+    config.plugins =
+        'a11yhelp,about,basicstyles,bidi,blockquote,button,clipboard,colorbutton,colordialog,contextmenu,dialog,dialogadvtab,dialogui,div,enterkey,entities,fakeobjects,find,flash,floatingspace,floatpanel,font,format,forms,horizontalrule,htmlwriter,iframe,image,indent,indentblock,indentlist,justify,link,list,listblock,liststyle,magicline,maximize,menu,menubutton,newpage,pagebreak,panel,panelbutton,pastefromword,pastetext,popup,preview,print,removeformat,resize,richcombo,save,scayt,selectall,showblocks,showborders,smiley,sourcearea,specialchar,stylescombo,tab,table,tabletools,templates,toolbar,undo,wsc,wysiwygarea,chamilo,chamilofakeobjects,eqneditor,oembed,widget';
+
     config.menu_groups = 'clipboard,' + 'form,' + 'tablecell,tablecellproperties,tablerow,tablecolumn,table,'
-            + 'anchor,link,image,flash,'
-            + 'checkbox,radio,textfield,hiddenfield,imagebutton,button,select,textarea,div,' + 'chamilo';
-    
+        + 'anchor,link,image,flash,'
+        + 'checkbox,radio,textfield,hiddenfield,imagebutton,button,select,textarea,div,' + 'chamilo';
+
     config.toolbarCanCollapse = true;
-    
+
     config.toolbar_Full = [
-            [ 'Source', '-', 'Save', 'NewPage', 'Preview', '-', 'Templates' ],
-            [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Print', 'SpellChecker', 'Scayt' ],
-            [ 'Undo', 'Redo', '-', 'Find', 'Replace', '-', 'SelectAll', 'RemoveFormat' ],
-            [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ],
-            '/',
-            [ 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript' ],
-            [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote' ],
-            [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ],
-            [ 'Link', 'Unlink', 'Anchor' ],
-            [ 'Chamilo', 'Image', 'oembed', 'EqnEditor', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar',
-                    'PageBreak' ], '/', [ 'Styles', 'Format', 'Font', 'FontSize' ], [ 'TextColor', 'BGColor' ],
-            [ 'Maximize', 'ShowBlocks', '-', 'About' ] ];
-    
+        ['Source', '-', 'Save', 'NewPage', 'Preview', '-', 'Templates'],
+        ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Print', 'SpellChecker', 'Scayt'],
+        ['Undo', 'Redo', '-', 'Find', 'Replace', '-', 'SelectAll', 'RemoveFormat'],
+        ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+        '/',
+        ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript'],
+        ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'],
+        ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+        ['Link', 'Unlink', 'Anchor'],
+        ['Chamilo', 'Image', 'oembed', 'EqnEditor', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar',
+            'PageBreak'], '/', ['Styles', 'Format', 'Font', 'FontSize'], ['TextColor', 'BGColor'],
+        ['Maximize', 'ShowBlocks', '-', 'About']];
+
     config.toolbar_Basic = [
-            [ 'Maximize', '-', 'Styles', 'Format', 'Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline' ],
-            '/',
-            [ 'Preview', '-', 'Undo', 'Redo', '-', 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-',
-                    'NumberedList', 'BulletedList', 'HorizontalRule', '-', 'JustifyLeft', 'JustifyCenter',
-                    'JustifyRight', 'JustifyBlock', 'Outdent', 'Indent' ],
-            '/',
-            [ 'Link', 'Unlink', '-', 'TextColor', 'BGColor', '-', 'Table', 'Chamilo', 'Image', 'oembed', 'EqnEditor',
-                    'Smiley', '-', 'Templates', 'SpecialChar', '-', 'Source' ] ];
-    
+        ['Maximize', '-', 'Styles', 'Format', 'Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline'],
+        '/',
+        ['Preview', '-', 'Undo', 'Redo', '-', 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-',
+            'NumberedList', 'BulletedList', 'HorizontalRule', '-', 'JustifyLeft', 'JustifyCenter',
+            'JustifyRight', 'JustifyBlock', 'Outdent', 'Indent'],
+        '/',
+        ['Link', 'Unlink', '-', 'TextColor', 'BGColor', '-', 'Table', 'Chamilo', 'Image', 'oembed', 'EqnEditor',
+            'Smiley', '-', 'Templates', 'SpecialChar', '-', 'Source']];
+
     config.filebrowserChamiloBrowseUrl = web_path
-            + 'index.php?application=Chamilo\\Core\\Repository&go=html_editor_file&plugin=chamilo';
+        + 'index.php?application=Chamilo\\Core\\Repository&go=html_editor_file&plugin=chamilo';
     config.filebrowserChamiloHandbookLinkBrowseUrl = web_path
-            + 'index.php?application=Chamilo\\Application\Handbook&go=handbook_topic_picker';
+        + 'index.php?application=Chamilo\\Application\Handbook&go=handbook_topic_picker';
     config.latexDialogUrl = web_path
-            + 'Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor/release/ckeditor/plugins/latex/dialogs/latex.html?a=b';
-    
+        +
+        'Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor/release/ckeditor/plugins/latex/dialogs/latex.html?a=b';
+
     config.contentsCss = [
-            web_path + 'Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor/release/ckeditor/contents.css',
-            web_path + 'index.php?application=Chamilo\\Libraries\\Ajax&go=CkeditorCss&theme=' + getTheme() ];
-    
+        web_path + 'Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor/release/ckeditor/contents.css',
+        web_path + 'index.php?application=Chamilo\\Libraries\\Ajax&go=CkeditorCss&theme=' + getTheme(),
+        web_path + 'index.php?application=Chamilo%5CLibraries%5CAjax&go=resource&theme=aqua&type=css'
+    ];
+
     config.disableNativeSpellChecker = false;
     config.allowedContent = true;
     config.resize_dir = 'both';
