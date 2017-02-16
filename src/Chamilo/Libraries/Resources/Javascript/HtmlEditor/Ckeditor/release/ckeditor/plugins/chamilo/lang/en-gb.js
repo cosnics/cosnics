bf6b6c833e70dea58bde227487ba43b66058fbf9
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-02-16 10:44:32 +0100

    [CkEditor] Renaming content object to repository

--- src/Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor/release/ckeditor/plugins/chamilo/lang/en-gb.js
+++ src/Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor/release/ckeditor/plugins/chamilo/lang/en-gb.js
@@ -3,7 +3,5 @@ Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'chamilo', 'en-gb', {
-		label: 'Content object'
-
-	});
+		label: 'Repository'
 });
