3200c3c0180fbdbfc7ea9a15309bf19f24a999c5
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-03-14 13:37:50 +0100

    [Open Question] Fixed the result display

--- src/Chamilo/Core/Repository/ContentObject/AssessmentOpenQuestion/Integration/Chamilo/Core/Repository/ContentObject/Assessment/Display/ResultDisplay.php
+++ src/Chamilo/Core/Repository/ContentObject/AssessmentOpenQuestion/Integration/Chamilo/Core/Repository/ContentObject/Assessment/Display/ResultDisplay.php
@@ -26,9 +26,9 @@ class ResultDisplay extends AssessmentQuestionResultDisplay
         $type = $question->get_question_type();
         $answers = $this->get_answers();
         $configuration = $this->getViewerApplication()->get_configuration();
-        
+
         $html = array();
-        
+
         switch ($type)
         {
             case AssessmentOpenQuestion::TYPE_OPEN :
@@ -42,20 +42,31 @@ class ResultDisplay extends AssessmentQuestionResultDisplay
                 $html[] = $this->display_document_box($answers[2]);
                 break;
         }
-        
-        $html[] = '<div class="splitter" style="margin: -10px -10px 10px -10px; border-left: none; border-right: none; border-top: 1px solid #B5CAE7;">';
-        $html[] = Translation::get('Feedback');
-        $html[] = '</div>';
-        
+
+        $html[] = '<div style="padding: 0 10px 10px 10px; border-top: 1px dashed #dddddd;">';
+        $html[] = '<h5>' . Translation::get('Feedback') . '</h5>';
+
         if (AnswerFeedbackDisplay::allowed($configuration, $this->get_complex_content_object_question(), true, true))
         {
             $object_renderer = new ContentObjectResourceRenderer(
-                $this->getViewerApplication(), 
-                $question->get_feedback());
-            
-            $html[] = $object_renderer->run();
+                $this->getViewerApplication(),
+                $question->get_feedback()
+            );
+
+            $feedback = trim($object_renderer->run());
+
+            if (empty($feedback))
+            {
+                $feedback = '<p>' . Translation::getInstance()->getTranslation(
+                        'NoFeedback', null, 'Chamilo\Core\Repository\ContentObject\Assessment\Display'
+                    ) . '</p>';
+            }
+
+            $html[] = $feedback;
         }
-        
+
+        $html[] = '</div>';
+
         return implode(PHP_EOL, $html);
     }
 
@@ -67,12 +78,9 @@ class ResultDisplay extends AssessmentQuestionResultDisplay
     public function display_open($answer)
     {
         $html = array();
-        $html[] = '<div class="splitter" style="margin: -10px -10px 0 -10px; border-left: none; border-right: none;">';
-        $html[] = Translation::get('Answer');
-        $html[] = '</div>';
-        
-        $html[] = '<br />';
-        
+        $html[] = '<div style="padding: 0 10px 10px 10px;">';
+        $html[] = '<h5>' . Translation::get('Answer') . '</h5>';
+
         if ($answer && trim($answer) != '')
         {
             $html[] = $answer;
@@ -81,9 +89,10 @@ class ResultDisplay extends AssessmentQuestionResultDisplay
         {
             $html[] = '<p>' . Translation::get('NoAnswer') . '</p>';
         }
-        
+
         $html[] = '<div class="clear"></div>';
-        $html[] = '<br />';
+        $html[] = '</div>';
+
         return implode(PHP_EOL, $html);
     }
 
@@ -92,38 +101,41 @@ class ResultDisplay extends AssessmentQuestionResultDisplay
         $html = array();
         if ($with_open)
         {
-            $html[] = '<div class="splitter" style="margin: -10px -10px 0 -10px; border-left: none; border-right: none; border-top: 1px solid #B5CAE7;">';
+            $html[] = '<div style="padding: 0 10px 10px 10px;">';
         }
         else
         {
-            $html[] = '<div class="splitter" style="margin: -10px -10px 0 -10px; border-left: none; border-right: none;">';
+            $html[] = '<div style="padding: 0 10px 10px 10px;">';
         }
-        
-        $html[] = Translation::get('Document');
-        $html[] = '</div>';
-        
-        if (! $answer)
+
+        $html[] = '<h5>' . Translation::get('Document') . '</h5>';
+
+        if (!$answer)
         {
-            
-            $html[] = '<br /><p>' . Translation::get('NoDocument') . '</p><div class="clear"></div><br />';
+
+            $html[] = '<p>' . Translation::get('NoDocument') . '</p><div class="clear"></div><br />';
+
             return;
         }
-        
+
         $document = \Chamilo\Core\Repository\Storage\DataManager::retrieve_by_id(ContentObject::class_name(), $answer);
-        
+
         $html[] = '<br />';
-        
+
         $html[] = '<div style="position: relative; margin: 10px auto; margin-left: -350px; width: 700px;
 				  left: 50%; right: 50%; border-width: 1px; border-style: solid;
 				  background-color: #E5EDF9; border-color: #4171B5; padding: 15px; text-align:center;">';
-        
+
         $html[] = sprintf(Translation::get('LPDownloadDocument'), $document->get_filename(), $document->get_filesize());
-        $html[] .= '<br /><a target="about:blank" href="' . \Chamilo\Core\Repository\Manager::get_document_downloader_url(
-            $document->get_id(), 
-            $document->calculate_security_code()) . '">' . Translation::get('Download') . '</a>';
-        
+        $html[] .= '<br /><a target="about:blank" href="' .
+            \Chamilo\Core\Repository\Manager::get_document_downloader_url(
+                $document->get_id(),
+                $document->calculate_security_code()
+            ) . '">' . Translation::get('Download') . '</a>';
+
         $html[] = '</div>';
-        $html[] = '<br />';
+        $html[] = '</div>';
+
         return implode(PHP_EOL, $html);
     }
 
