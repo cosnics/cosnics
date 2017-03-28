6a98b2a2ed9fb9dac3dc77245fd271cd52cb0b0e
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-03-28 16:42:29 +0200

    [Repository] Small bugfix for the ContentObject Resource Fixer

--- src/Chamilo/Core/Repository/Service/ResourceFixer/ContentObjectDescriptionResourceFixer.php
+++ src/Chamilo/Core/Repository/Service/ResourceFixer/ContentObjectDescriptionResourceFixer.php
@@ -57,7 +57,7 @@ class ContentObjectDescriptionResourceFixer extends ResourceFixer
                         }
                         else
                         {
-                            $contentObject->update();
+                            $contentObject->update(false);
                         }
                     }
                 }
