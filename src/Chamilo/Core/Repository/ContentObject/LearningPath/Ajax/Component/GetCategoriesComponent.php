1a8314387155de49efdbb7605553f2a790455a17
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-05-19 16:40:34 +0200

    [Learning Path] Continued working on section copier

--- src/Chamilo/Core/Repository/ContentObject/LearningPath/Ajax/Component/GetCategoriesComponent.php
+++ src/Chamilo/Core/Repository/ContentObject/LearningPath/Ajax/Component/GetCategoriesComponent.php
@@ -1,13 +1,17 @@
 <?php
 
-
 namespace Chamilo\Core\Repository\ContentObject\LearningPath\Ajax\Component;
 
 use Chamilo\Core\Repository\ContentObject\LearningPath\Ajax\Manager;
 use Chamilo\Core\Repository\Menu\ContentObjectCategoryMenu;
 use Chamilo\Core\Repository\Workspace\Repository\WorkspaceRepository;
+use Chamilo\Core\Repository\Workspace\Service\RightsService;
 use Chamilo\Core\Repository\Workspace\Service\WorkspaceService;
+use Chamilo\Core\Repository\Workspace\Storage\DataClass\Workspace;
+use Chamilo\Libraries\Architecture\Exceptions\NotAllowedException;
+use Chamilo\Libraries\Architecture\Exceptions\ObjectNotExistException;
 use Chamilo\Libraries\Format\Menu\OptionsMenuRenderer;
+use Chamilo\Libraries\Platform\Translation;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
@@ -17,13 +21,21 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class GetCategoriesComponent extends Manager
 {
+    const PARAM_WORKSPACE_ID = 'workspace_id';
+
     /**
      * @inheritdoc
      */
     public function run()
     {
-        $response = new JsonResponse($this->getCategories());
-        $response->send();
+        try
+        {
+            return new JsonResponse($this->getCategories());
+        }
+        catch(\Exception $ex)
+        {
+            return $this->handleException($ex);
+        }
     }
 
     /**
@@ -32,7 +44,28 @@ class GetCategoriesComponent extends Manager
     protected function getCategories()
     {
         $workspaceService = new WorkspaceService(new WorkspaceRepository());
-        $repository = $workspaceService->getPersonalWorkspaceForUser($this->getUser());
+
+        $workspaceId = $this->getRequest()->get(self::PARAM_WORKSPACE_ID);
+        if (isset($workspaceId))
+        {
+            $repository = $workspaceService->getWorkspaceByIdentifier($workspaceId);
+            if (!$repository instanceof Workspace)
+            {
+                throw new ObjectNotExistException(
+                    Translation::getInstance()->getTranslation('Workspace'), $workspaceId
+                );
+            }
+
+            $rightsService = RightsService::getInstance();
+            if(!$rightsService->canViewContentObjects($this->getUser(), $repository))
+            {
+                throw new NotAllowedException();
+            }
+        }
+        else
+        {
+            $repository = $workspaceService->getPersonalWorkspaceForUser($this->getUser());
+        }
 
         $categorymenu = new ContentObjectCategoryMenu($repository);
         $renderer = new OptionsMenuRenderer();
@@ -41,7 +74,8 @@ class GetCategoriesComponent extends Manager
         $categories = $renderer->toArray();
         $categoriesArray = array();
 
-        foreach ($categories as $id => $name) {
+        foreach ($categories as $id => $name)
+        {
             $categoriesArray[] = ["id" => $id, "name" => $name];
         }
 
