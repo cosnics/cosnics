a71fd604162975e07e6c47f236f8406b786829e6
pjbro <pjbro@users.noreply.github.com>
2017-08-10 14:29:57 +0200

    [ckeditor] cache busting

--- src/Chamilo/Libraries/Resources/Javascript/HtmlEditor/CkeditorInstanceConfig.js
+++ src/Chamilo/Libraries/Resources/Javascript/HtmlEditor/CkeditorInstanceConfig.js
@@ -58,9 +58,9 @@ CKEDITOR.editorConfig = function(config)
     config.image2_alignClasses = [ 'align-left', 'align-center', 'align-right' ];
 
     config.contentsCss = [
-        web_path + 'Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor/contents.css',
-        web_path + 'index.php?application=Chamilo\\Libraries\\Ajax&go=CkeditorCss&theme=' + getTheme(),
-        web_path + 'index.php?application=Chamilo%5CLibraries%5CAjax&go=resource&type=css&theme=' + getTheme()
+        web_path + 'Chamilo/Libraries/Resources/Javascript/HtmlEditor/Ckeditor/contents.css?v=1',
+        web_path + 'index.php?application=Chamilo\\Libraries\\Ajax&go=CkeditorCss&theme=' + getTheme() + '?v=1',
+        web_path + 'index.php?application=Chamilo%5CLibraries%5CAjax&go=resource&type=css&theme=' + getTheme() + '?v=1'
     ];
 
     config.embed_provider = '//noembed.com/embed?url={url}&callback={callback}'; //free. Default is iframely.
