695dcd8b90d63f26f01f670b06520563c7c79913
Sven Vanpoucke <sven.vanpoucke@hogent.be>
2017-08-12 00:38:37 +0200

    [Libraries] Fixed CKEditor issues with quick questions

--- src/Chamilo/Libraries/Resources/Javascript/HtmlEditor/CkeditorExtra/Plugin/quickquestion/plugin.js
+++ src/Chamilo/Libraries/Resources/Javascript/HtmlEditor/CkeditorExtra/Plugin/quickquestion/plugin.js
@@ -6,15 +6,15 @@ CKEDITOR.plugins.add('quickquestion', {
     init: function (editor) {
 
         var pluginDirectory = this.path;
+
         editor.addContentsCss( pluginDirectory + 'plugin.css' );
 
         editor.widgets.add('quickquestion', {
             button: editor.lang.quickquestion.insertQuickQuestion,
-
             template: //@todo translation
             '<div class="quick-question">' +
             '<div class="a-question"><h4>' + editor.lang.quickquestion.askQuestion + '</h4></div>' +
-            '<button class="btn btn-primary" style="margin-bottom:5px; display: none" onclick="$(&quot;.an-answer&quot;).toggle()">' + editor.lang.quickquestion.showAnswer + '</button>' +
+            '<button type="button" class="btn btn-primary" style="margin-bottom:5px; display: none" onclick="$(\'.an-answer\', $(this).parent()).toggle()">' + editor.lang.quickquestion.showAnswer + '</button>' +
             '<div class="an-answer">' + editor.lang.quickquestion.addAnswer + '</div>' +
             '</div>',
 
@@ -32,6 +32,7 @@ CKEDITOR.plugins.add('quickquestion', {
                 if (element.name == 'div' && element.hasClass('quick-question')) {
                     element.children[1].attributes["style"] = "margin-bottom:5px; display: none"; //hide button
                     element.children[2].attributes["style"] = ""; //show answer
+
                     return true;
                 }
 
