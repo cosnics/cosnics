0d2542cd19fa8d834aaad9921b973b37f85db498
ScaraMcDuck <hans.de.bisschop@ehb.be>
2017-10-03 10:44:27 +0200

    [Survey] Removed applications and content objects [FAQ] Removed tool and content objects

--- src/Chamilo/Core/Repository/ContentObject/Survey/Integration/Chamilo/Core/Reporting/Package/Installer.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Chamilo\Core\Repository\ContentObject\Survey\Integration\Chamilo\Core\Reporting\Package;
-
-class Installer extends \Chamilo\Configuration\Package\Action\Installer
-{
-}
--- src/Chamilo/Core/Repository/ContentObject/Survey/Page/Question/Select/Implementation/ExportImplementation.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Chamilo\Core\Repository\ContentObject\Survey\Page\Question\Select\Common;
-
-use Chamilo\Core\Repository\Common\Export\ContentObjectExportImplementation;
-
-class ExportImplementation extends ContentObjectExportImplementation
-{
-}
-?>
