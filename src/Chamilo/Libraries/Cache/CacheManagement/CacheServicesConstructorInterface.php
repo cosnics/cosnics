328b539521bed66c618dc820288ab6e6e0fe942b
ScaraMcDuck <hans.de.bisschop@ehb.be>
2017-10-12 10:35:29 +0200

    [Core] Documentation of some core classes

--- src/Chamilo/Libraries/Cache/CacheManagement/CacheServicesConstructorInterface.php
+++ src/Chamilo/Libraries/Cache/CacheManagement/CacheServicesConstructorInterface.php
@@ -1,18 +1,19 @@
 <?php
-
 namespace Chamilo\Libraries\Cache\CacheManagement;
 
 /**
  * Interface for services to construct the cache services for the CacheManager
  *
+ * @package Chamilo\Libraries\Cache\CacheManagement
  * @author - Sven Vanpoucke - Hogeschool Gent
  */
 interface CacheServicesConstructorInterface
 {
+
     /**
      * Creates and adds the cache services to the given cache manager
      *
-     * @param CacheManager $cacheManager
+     * @param \Chamilo\Libraries\Cache\CacheManagement\CacheManager $cacheManager
      */
     public function createCacheServices(CacheManager $cacheManager);
 }
