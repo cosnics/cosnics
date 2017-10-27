d859dbf064483450e599e9f7de130a17f9d0254b
ScaraMcDuck <hans.de.bisschop@ehb.be>
2017-10-27 13:31:08 +0200

    Implementing JS Tables filter parameters

--- src/Chamilo/Core/Repository/Ajax/Tables/Component/ContentObjectComponent.php
+++ src/Chamilo/Core/Repository/Ajax/Tables/Component/ContentObjectComponent.php
@@ -5,11 +5,19 @@ use Chamilo\Core\Repository\Filter\FilterData;
 use Chamilo\Core\Repository\Filter\Renderer\ConditionFilterRenderer;
 use Chamilo\Core\Repository\Workspace\PersonalWorkspace;
 use Chamilo\Core\Repository\Storage\DataClass\ContentObject;
+use Chamilo\Libraries\Storage\Query\OrderBy;
+use Chamilo\Libraries\Storage\Query\Variable\PropertyConditionVariable;
+use Chamilo\Libraries\Storage\DataClass\DataClass;
 use Chamilo\Libraries\Architecture\JsonAjaxResult;
 
 class ContentObjectComponent extends \Chamilo\Core\Repository\Ajax\Manager
 {
-
+    const PARAM_CURRENT_PAGE = 'currentPage';
+    const PARAM_ITEMS_PER_PAGE = 'pageItems';
+    const PARAM_ORDER_BY_PROPERTY = 'orderBy';
+    const PARAM_ORDER_BY_DIRECTION = 'orderByReverse';
+    const PARAM_FILTER = 'filter';
+    const PARAM_FILTER_FIELDS = 'filterfields';
     const PROPERTY_CONTENT_OBJECT_DATA = 'content_object_data';
 
     /**
@@ -18,7 +26,63 @@ class ContentObjectComponent extends \Chamilo\Core\Repository\Ajax\Manager
      */
     public function getRequiredPostParameters()
     {
-        return array();
+        return array(
+            self::PARAM_CURRENT_PAGE, 
+            self::PARAM_ITEMS_PER_PAGE, 
+            self::PARAM_ORDER_BY_PROPERTY, 
+            self::PARAM_ORDER_BY_DIRECTION, 
+            self::PARAM_FILTER, 
+            self::PARAM_FILTER_FIELDS);
+    }
+
+    /**
+     *
+     * @return integer
+     */
+    protected function getCurrentPage()
+    {
+        return (int) $this->getPostDataValue(self::PARAM_CURRENT_PAGE);
+    }
+
+    /**
+     *
+     * @return integer
+     */
+    protected function getItemsPerPage()
+    {
+        return (int) $this->getPostDataValue(self::PARAM_ITEMS_PER_PAGE);
+    }
+
+    /**
+     *
+     * @return integer
+     */
+    protected function getOffset()
+    {
+        return $this->getCurrentPage() * $this->getItemsPerPage();
+    }
+
+    protected function getOrderby()
+    {
+        $orderByReverse = (boolean) $this->getPostDataValue(self::PARAM_ORDER_BY_DIRECTION);
+        $orderByPropertyParts = explode(':', $this->getPostDataValue(self::PARAM_ORDER_BY_PROPERTY));
+        
+        if (empty($orderByPropertyParts) || count($orderByPropertyParts) != 2)
+        {
+            throw new \InvalidArgumentException();
+        }
+        
+        $orderByClassName = str_replace('_', '\\', $orderByPropertyParts[0]);
+        
+        if (! class_exists($orderByClassName) || ! is_subclass_of($orderByClassName, DataClass::class))
+        {
+            throw new \InvalidArgumentException();
+        }
+        
+        return array(
+            new OrderBy(
+                new PropertyConditionVariable($orderByClassName, $orderByPropertyParts[1]), 
+                $orderByReverse ? SORT_DESC : SORT_ASC));
     }
 
     /**
@@ -30,21 +94,25 @@ class ContentObjectComponent extends \Chamilo\Core\Repository\Ajax\Manager
         $filterData = $this->getFilterData();
         $workspaceImplementation = $this->getWorkspaceImplementation();
         
-        $contentObjects = $this->getContentObjectService()->getContentObjectsByTypeForWorkspace($filterData->getTypeDataClass(), $workspaceImplementation, ConditionFilterRenderer::factory($filterData, $workspaceImplementation));
+        $contentObjects = $this->getContentObjectService()->getContentObjectsByTypeForWorkspace(
+            $filterData->getTypeDataClass(), 
+            $workspaceImplementation, 
+            ConditionFilterRenderer::factory($filterData, $workspaceImplementation), 
+            $this->getOffset(), 
+            $this->getItemsPerPage(), 
+            $this->getOrderBy());
         
         $contentObjectData = array();
         
-        while ($contentObject = $contentObjects->next_result()) {
+        while ($contentObject = $contentObjects->next_result())
+        {
             $contentObjectData[] = array(
-                ContentObject::PROPERTY_TITLE => $contentObject->get_title(),
-                ContentObject::PROPERTY_DESCRIPTION => $contentObject->get_description(),
-                ContentObject::PROPERTY_MODIFICATION_DATE => $contentObject->get_modification_date()
-            );
+                ContentObject::PROPERTY_TITLE => $contentObject->get_title(), 
+                ContentObject::PROPERTY_DESCRIPTION => $contentObject->get_description(), 
+                ContentObject::PROPERTY_MODIFICATION_DATE => $contentObject->get_modification_date());
         }
         
-        $properties = array(
-            self::PROPERTY_CONTENT_OBJECT_DATA => $contentObjectData
-        );
+        $properties = array(self::PROPERTY_CONTENT_OBJECT_DATA => $contentObjectData);
         
         $jsonAjaxResult = new JsonAjaxResult();
         $jsonAjaxResult->set_properties($properties);
