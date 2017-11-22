0e9b5cac95a198ffd7ad46720cd07b05428a0a06
ScaraMcDuck <hans.de.bisschop@ehb.be>
2017-11-22 11:09:08 +0100

    [Calendar] Adding a FullCalendar component for testing purposes

---
+++ src/Chamilo/Application/Calendar/Component/FullCalendarComponent.php
@@ -0,0 +1,49 @@
+<?php
+namespace Chamilo\Application\Calendar\Component;
+
+use Chamilo\Libraries\Calendar\Renderer\Type\FullCalendarRenderer;
+
+/**
+ *
+ * @package Chamilo\Application\Calendar\Component
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ */
+class FullCalendarComponent
+{
+
+    private $viewRenderer;
+
+    /**
+     *
+     * @see \Chamilo\Libraries\Architecture\Application\Application::run()
+     */
+    public function run()
+    {
+        $html = array();
+
+        $html[] = $this->render_header();
+
+        $html[] = '<div class="row">';
+        $html[] = $this->getViewRenderer()->render();
+        $html[] = '</div>';
+
+        $html[] = $this->render_footer();
+
+        return implode(PHP_EOL, $html);
+    }
+
+    /**
+     *
+     * @return \Chamilo\Libraries\Calendar\Renderer\Type\FullCalendarRenderer
+     */
+    protected function getViewRenderer()
+    {
+        if (! isset($this->viewRenderer))
+        {
+            $this->viewRenderer = new FullCalendarRenderer($this->getCurrentRendererTime());
+        }
+
+        return $this->viewRenderer;
+    }
+}
+
