0a10cd09369d52d87372141644d23ce0cba5e885
ScaraMcDuck <hans.de.bisschop@ehb.be>
2017-11-28 09:19:52 +0100

    [Calendar] Working on FullCalendar and implementing DI for the calendars in general

---
+++ src/Chamilo/Libraries/Calendar/Table/CalendarConfiguration.php
@@ -0,0 +1,150 @@
+<?php
+namespace Chamilo\Libraries\Calendar\Table;
+
+/**
+ *
+ * @package Chamilo\Libraries\Calendar\Table
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ */
+class CalendarConfiguration
+{
+
+    /**
+     *
+     * @var integer
+     */
+    private $workingHoursStart;
+
+    /**
+     *
+     * @var integer
+     */
+    private $workingHoursEnd;
+
+    /**
+     *
+     * @var boolean
+     */
+    private $hideNonWorkingHours;
+
+    /**
+     *
+     * @var integer
+     */
+    private $hourStep;
+
+    /**
+     *
+     * @var string
+     */
+    private $firstDayOfTheWeek;
+
+    /**
+     *
+     * @param integer $workingHoursStart
+     * @param integer $workingHoursEnd
+     * @param boolean $hideNonWorkingHours
+     * @param integer $hourStep
+     * @param string $firstDayOfTheWeek
+     */
+    public function __construct($workingHoursStart, $workingHoursEnd, $hideNonWorkingHours, $hourStep,
+        $firstDayOfTheWeek)
+    {
+        $this->workingHoursStart = $workingHoursStart;
+        $this->workingHoursEnd = $workingHoursEnd;
+        $this->hideNonWorkingHours = $hideNonWorkingHours;
+        $this->hourStep = $hourStep;
+        $this->firstDayOfTheWeek = $firstDayOfTheWeek;
+    }
+
+    /**
+     *
+     * @return integer
+     */
+    public function getWorkingHoursStart()
+    {
+        return $this->workingHoursStart;
+    }
+
+    /**
+     *
+     * @param integer $workingHoursStart
+     */
+    public function setWorkingHoursStart($workingHoursStart)
+    {
+        $this->workingHoursStart = $workingHoursStart;
+    }
+
+    /**
+     *
+     * @return integer
+     */
+    public function getWorkingHoursEnd()
+    {
+        return $this->workingHoursEnd;
+    }
+
+    /**
+     *
+     * @param integer $workingHoursEnd
+     */
+    public function setWorkingHoursEnd($workingHoursEnd)
+    {
+        $this->workingHoursEnd = $workingHoursEnd;
+    }
+
+    /**
+     *
+     * @return boolean
+     */
+    public function getHideNonWorkingHours()
+    {
+        return $this->hideNonWorkingHours;
+    }
+
+    /**
+     *
+     * @param boolean $hideNonWorkingHours
+     */
+    public function setHideNonWorkingHours($hideNonWorkingHours)
+    {
+        $this->hideNonWorkingHours = $hideNonWorkingHours;
+    }
+
+    /**
+     *
+     * @return integer
+     */
+    public function getHourStep()
+    {
+        return $this->hourStep;
+    }
+
+    /**
+     *
+     * @param integer $hourStep
+     */
+    public function setHourStep($hourStep)
+    {
+        $this->hourStep = $hourStep;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getFirstDayOfTheWeek()
+    {
+        return $this->firstDayOfTheWeek;
+    }
+
+    /**
+     *
+     * @param string $firstDayOfTheWeek
+     */
+    public function setFirstDayOfTheWeek($firstDayOfTheWeek)
+    {
+        $this->firstDayOfTheWeek = $firstDayOfTheWeek;
+    }
+}
+
