a20183ba75ca0f78848bf8c728d7d01eafae8825
ScaraMcDuck <hans.de.bisschop@ehb.be>
2017-12-01 09:02:39 +0100

    [Calendar] Working on FullCalendar and implementing DI for the calendars in general

---
+++ src/Chamilo/Libraries/Calendar/Event/EventSource.php
@@ -0,0 +1,149 @@
+<?php
+namespace Chamilo\Libraries\Calendar\Event;
+
+/**
+ *
+ * @package Chamilo\Libraries\Calendar\Event
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ */
+class EventSource
+{
+
+    /**
+     *
+     * @var string
+     */
+    private $id;
+
+    /**
+     *
+     * @var string
+     */
+    private $context;
+
+    /**
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     *
+     * @var string
+     */
+    private $content;
+
+    /**
+     *
+     * @var string
+     */
+    private $url;
+
+    /**
+     *
+     * @param string $id
+     * @param string $context
+     * @param string $title
+     * @param string $content
+     * @param string $url
+     */
+    public function __construct($id = null, $context = null, $title = null, $content = null, $url = null)
+    {
+        $this->id = $id;
+        $this->context = $context;
+        $this->title = $title;
+        $this->content = $content;
+        $this->url = $url;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     *
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    /**
+     *
+     * @param string $context
+     */
+    public function setContext($context)
+    {
+        $this->context = $context;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     *
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     *
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+}
+
