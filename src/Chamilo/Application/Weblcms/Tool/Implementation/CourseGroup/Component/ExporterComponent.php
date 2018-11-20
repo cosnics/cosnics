9bcbf3661b2ec331a1f8a2d3713f201f9df6766c
vanpouckesven <sven.vanpoucke@hogent.be>
2018-11-20 11:22:36 +0100

    [Weblcms] Added extra sort exporter where spaces are removed from names

--- src/Chamilo/Application/Weblcms/Tool/Implementation/CourseGroup/Component/ExporterComponent.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/CourseGroup/Component/ExporterComponent.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Chamilo\Application\Weblcms\Tool\Implementation\CourseGroup\Component;
 
 use Chamilo\Application\Weblcms\Course\Storage\DataManager as CourseDataManager;
@@ -34,12 +35,13 @@ use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 /**
  * Class To export users and course groups from a course
- * 
+ *
  * @author VUB - Original author
  * @author Sven Vanpoucke - Bugfixes, comments
  */
 class ExporterComponent extends Manager
 {
+    const PROPERTY_SORT_NAME = 'SortName';
 
     private $current_tab;
 
@@ -50,30 +52,30 @@ class ExporterComponent extends Manager
      */
     public function run()
     {
-        if (! $this->is_allowed(WeblcmsRights::EDIT_RIGHT))
+        if (!$this->is_allowed(WeblcmsRights::EDIT_RIGHT))
         {
             throw new NotAllowedException();
         }
-        
+
         if (Request::get(self::PARAM_COURSE_GROUP))
         {
             $course_group_id = Request::get(self::PARAM_COURSE_GROUP);
-            
+
             $course_group = DataManager::retrieve_by_id(CourseGroup::class_name(), $course_group_id);
-            
+
             $this->course_group = $course_group;
         }
-        
+
         $this->current_tab = SubscriptionsOverviewerComponent::TAB_USERS;
         if (Request::get(SubscriptionsOverviewerComponent::PARAM_TAB))
         {
             $this->current_tab = Request::get(SubscriptionsOverviewerComponent::PARAM_TAB);
         }
-        elseif (! is_null($this->course_group))
+        elseif (!is_null($this->course_group))
         {
             $this->current_tab = SubscriptionsOverviewerComponent::TAB_COURSE_GROUPS;
         }
-        
+
         switch ($this->current_tab)
         {
             case SubscriptionsOverviewerComponent::TAB_USERS :
@@ -83,44 +85,47 @@ class ExporterComponent extends Manager
                 $file_path = $this->render();
                 break;
         }
-        
+
         $this->send_as_download($file_path);
         Filesystem::remove($file_path);
     }
 
     /**
      * Exports the users with the new exporter
-     * 
+     *
      * @return string
      */
     public function export_users()
     {
         $user_records = CourseDataManager::retrieve_all_course_users(
-            $this->get_course_id(), 
-            null, 
-            null, 
-            null, 
+            $this->get_course_id(),
+            null,
+            null,
+            null,
             array(
-                new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_LASTNAME)), 
-                new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_FIRSTNAME))));
-        
+                new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_LASTNAME)),
+                new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_FIRSTNAME))
+            )
+        );
+
         $users = array();
-        
+
         while ($user_record = $user_records->next_result())
         {
             $users[] = DataClass::factory(User::class_name(), $user_record);
         }
-        
+
         $exporter = new UserExporter(
-            new ExcelUserExportRenderer(), 
-            array(new CourseGroupUserExportExtender($this->get_course_id())));
-        
+            new ExcelUserExportRenderer(),
+            array(new CourseGroupUserExportExtender($this->get_course_id()))
+        );
+
         return $exporter->export($users);
     }
 
     /**
      * Sends the exported file as download
-     * 
+     *
      * @param $file_path string
      */
     public function send_as_download($file_path)
@@ -128,7 +133,7 @@ class ExporterComponent extends Manager
         // Determines the filename that the user will see when they download the
         // file
         $course = $this->get_course();
-        
+
         if ($this->course_group)
         {
             $filename = $course->get_title() . '_' . $this->course_group->get_name() . "_" . date('Ymd') . '.xlsx';
@@ -137,48 +142,51 @@ class ExporterComponent extends Manager
         {
             $filename = $course->get_title() . date('Ymd') . '.xlsx';
         }
-        
+
         // Make safe name
         $filename = Filesystem::create_safe_name($filename);
         $filename = preg_replace('/[\s]+/', '_', $filename);
-        
+
         $type = 'application/vnd.openxmlformats';
-        
+
         // Send file for download
         Filesystem::file_send_for_download($file_path, true, $filename, $type);
     }
 
     /**
      * Returns the conditions when the course groups need to be exported
-     * 
+     *
      * @return AndCondition
      */
     public function get_condition()
     {
         $conditions = array();
-        
+
         if ($this->current_tab == SubscriptionsOverviewerComponent::TAB_COURSE_GROUPS)
         {
             $conditions[] = new EqualityCondition(
-                new PropertyConditionVariable(CourseGroup::class_name(), CourseGroup::PROPERTY_COURSE_CODE), 
-                new StaticConditionVariable($this->get_course_id()));
+                new PropertyConditionVariable(CourseGroup::class_name(), CourseGroup::PROPERTY_COURSE_CODE),
+                new StaticConditionVariable($this->get_course_id())
+            );
             $conditions[] = new NotCondition(
                 new EqualityCondition(
-                    new PropertyConditionVariable(CourseGroup::class_name(), CourseGroup::PROPERTY_PARENT_ID), 
-                    new StaticConditionVariable(0)));
+                    new PropertyConditionVariable(CourseGroup::class_name(), CourseGroup::PROPERTY_PARENT_ID),
+                    new StaticConditionVariable(0)
+                )
+            );
         }
-        
+
         if ($conditions)
         {
             return new AndCondition($conditions);
         }
-        
+
         return null;
     }
 
     /**
      * Gets the export data for the users
-     * 
+     *
      * @param $data User
      *
      * @return array
@@ -187,20 +195,24 @@ class ExporterComponent extends Manager
     {
         $table = array();
         $table[0][User::PROPERTY_OFFICIAL_CODE] = Translation::get(
-            'OfficialCode', 
-            null, 
-            \Chamilo\Core\User\Manager::context());
+            'OfficialCode',
+            null,
+            \Chamilo\Core\User\Manager::context()
+        );
         $table[0][User::PROPERTY_USERNAME] = Translation::get('Username', null, \Chamilo\Core\User\Manager::context());
         $table[0][User::PROPERTY_LASTNAME] = Translation::get('Lastname', null, \Chamilo\Core\User\Manager::context());
+        $table[0][self::PROPERTY_SORT_NAME] =
+            Translation::get('SortName', null, \Chamilo\Application\Weblcms\Manager::context());
         $table[0][User::PROPERTY_FIRSTNAME] = Translation::get(
-            'Firstname', 
-            null, 
-            \Chamilo\Core\User\Manager::context());
-        
+            'Firstname',
+            null,
+            \Chamilo\Core\User\Manager::context()
+        );
+
         $table[0][User::PROPERTY_EMAIL] = Translation::get('Email', null, \Chamilo\Core\User\Manager::context());
         $table[0][CourseGroupUserRelation::PROPERTY_SUBSCRIPTION_TIME] = Translation::get('SubscriptionTime');
         $table[0]['Course Groups'] = Translation::get('CourseGroups');
-        
+
         $index = 0;
         while ($block_data = $data->next_result())
         {
@@ -208,12 +220,13 @@ class ExporterComponent extends Manager
             // {
             // $block_data = DataClass:: factory(User:: class_name(), $block_data);
             // }
-            
+
             $index ++;
             // get the list of course_groups the user is subscribed
             $course_groups = DataManager::retrieve_course_groups_from_user(
-                $block_data[User::PROPERTY_ID], 
-                $this->get_course_id());
+                $block_data[User::PROPERTY_ID],
+                $this->get_course_id()
+            );
             $course_groups_subscribed = array();
             while ($course_group = $course_groups->next_result())
             {
@@ -231,38 +244,44 @@ class ExporterComponent extends Manager
                     $course_groups_string = $item;
                 }
             }
-            
+
             $table[$index][User::PROPERTY_OFFICIAL_CODE] = $block_data[User::PROPERTY_OFFICIAL_CODE];
             $table[$index][User::PROPERTY_USERNAME] = $block_data[User::PROPERTY_USERNAME];
             $table[$index][User::PROPERTY_LASTNAME] = $block_data[User::PROPERTY_LASTNAME];
             $table[$index][User::PROPERTY_FIRSTNAME] = $block_data[User::PROPERTY_FIRSTNAME];
+
+            $table[$index][self::PROPERTY_SORT_NAME] = strtoupper(
+                str_replace(' ', '', $block_data[User::PROPERTY_LASTNAME] . ',' . $block_data[User::PROPERTY_FIRSTNAME])
+            );
+
             $table[$index][User::PROPERTY_EMAIL] = $block_data[User::PROPERTY_EMAIL];
             $table[$index][CourseGroupUserRelation::PROPERTY_SUBSCRIPTION_TIME] = DatetimeUtilities::format_locale_date(
-                Translation::getInstance()->getTranslation('SubscriptionTimeFormat', null, Manager::context()), 
-                $block_data[CourseGroupUserRelation::PROPERTY_SUBSCRIPTION_TIME]);
-            
+                Translation::getInstance()->getTranslation('SubscriptionTimeFormat', null, Manager::context()),
+                $block_data[CourseGroupUserRelation::PROPERTY_SUBSCRIPTION_TIME]
+            );
+
             $table[$index]['Course Groups'] = $course_groups_string;
         }
-        
+
         return $table;
     }
 
     /**
      * Retrieves the course groups when exporting all the course groups
-     * 
+     *
      * @param $worksheet <type>
      */
     protected function get_course_groups_tab($worksheet)
     {
         $courseGroupRoot = DataManager::retrieve_course_group_root($this->get_course_id());
         $course_groups = $courseGroupRoot->get_children(false);
-        
+
         $this->handle_course_groups($course_groups, $worksheet);
     }
 
     /**
      * Handles a resultset of course groups and their children
-     * 
+     *
      * @param ResultSet $course_groups
      * @param $worksheet
      * @param int $rowcount
@@ -274,70 +293,73 @@ class ExporterComponent extends Manager
         while ($course_group = $course_groups->next_result())
         {
             $course_group_users = DataManager::retrieve_course_group_users_with_subscription_time(
-                $course_group->get_id(), 
-                null, 
-                null, 
-                null, 
+                $course_group->get_id(),
+                null,
+                null,
+                null,
                 array(
-                    new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_LASTNAME)), 
-                    new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_FIRSTNAME))));
-            
+                    new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_LASTNAME)),
+                    new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_FIRSTNAME))
+                )
+            );
+
             $users_table = $this->get_users_table($course_group_users);
             $title = $course_group->get_name();
-            
+
             $rowcount = $this->render_table(
-                $worksheet, 
-                $title, 
-                $course_group->get_description(), 
-                $users_table, 
-                $rowcount);
-            
+                $worksheet,
+                $title,
+                $course_group->get_description(),
+                $users_table,
+                $rowcount
+            );
+
             $rowcount = $this->handle_course_groups($course_group->get_children(), $worksheet, $rowcount);
         }
-        
+
         return $rowcount;
     }
 
     /**
      * Renders the worksheet
-     * 
+     *
      * @return string - the path to the rendered worksheet
      */
     public function render()
     {
         $excel = new Spreadsheet();
         $worksheet = $excel->getSheet(0)->setTitle('Export');
-        
+
         $this->get_data($worksheet);
-        
+
         $objWriter = IOFactory::createWriter($excel, 'Xlsx');
-        
+
         $temp_dir = Path::getInstance()->getTemporaryPath() . 'excel/';
-        
-        if (! is_dir($temp_dir))
+
+        if (!is_dir($temp_dir))
         {
             mkdir($temp_dir, 0777, true);
         }
-        
+
         $filename = 'export_course_group_' . $this->get_course_id();
-        
+
         if ($this->course_group)
         {
             $filename .= '_' . $this->course_group->get_id();
         }
-        
+
         $filename .= '_' . time();
-        
+
         $file_path = $temp_dir . $filename;
-        
+
         $objWriter->save($file_path);
-        
+
         return $file_path;
     }
 
     /**
      * Gets the data for the worksheet
-     * 
+     *
      * @param $worksheet Worksheet
      */
     public function get_data($worksheet)
@@ -355,7 +377,7 @@ class ExporterComponent extends Manager
 
     /**
      * Gets the course group subscriptions for the given course group
-     * 
+     *
      * @param $worksheet <type>
      */
     private function get_course_group_subscription($worksheet)
@@ -364,14 +386,16 @@ class ExporterComponent extends Manager
         // retrieve_all_course_users($this->get_course_id(),
         // $this->get_condition(), null, null, null);
         $data = DataManager::retrieve_course_group_users_with_subscription_time(
-            $this->course_group->get_id(), 
-            null, 
-            null, 
-            null, 
+            $this->course_group->get_id(),
+            null,
+            null,
+            null,
             array(
-                new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_LASTNAME)), 
-                new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_FIRSTNAME))));
-        
+                new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_LASTNAME)),
+                new OrderBy(new PropertyConditionVariable(User::class_name(), User::PROPERTY_FIRSTNAME))
+            )
+        );
+
         $table = $this->get_users_table($data);
         $title = Translation::get('SubscriptionList');
         $rowcount = 0;
@@ -380,7 +404,7 @@ class ExporterComponent extends Manager
 
     /**
      * Renders the data
-     * 
+     *
      * @param $worksheet Worksheet
      * @param $title String
      * @param $table String[]
@@ -394,10 +418,11 @@ class ExporterComponent extends Manager
         $column1 = 2;
         $column2 = 3;
         $color = Color::COLOR_BLUE;
-        
+
         $styleArray = array(
-            'font' => array('underline' => Font::UNDERLINE_SINGLE, 'color' => array('argb' => $color)));
-        
+            'font' => array('underline' => Font::UNDERLINE_SINGLE, 'color' => array('argb' => $color))
+        );
+
         $block_row ++;
         $block_row ++;
 
@@ -405,24 +430,26 @@ class ExporterComponent extends Manager
         // $this->wrap_text($worksheet, $column, $block_row);
         $worksheet->mergeCells('A' . $block_row . ':G' . $block_row);
         $worksheet->getStyleByColumnAndRow($column, $block_row)->getAlignment()->setHorizontal(
-            Alignment::HORIZONTAL_CENTER);
+            Alignment::HORIZONTAL_CENTER
+        );
         $worksheet->getStyleByColumnAndRow($column, $block_row)->getFont()->setBold(true);
-        
+
         $block_row ++;
-        
+
         if ($description)
         {
             $block_row ++;
-            
+
             $worksheet->mergeCells('A' . $block_row . ':G' . $block_row);
             $worksheet->getStyleByColumnAndRow($column, $block_row)->getAlignment()->setHorizontal(
-                Alignment::HORIZONTAL_CENTER);
-            
+                Alignment::HORIZONTAL_CENTER
+            );
+
             $worksheet->setCellValueByColumnAndRow($column, $block_row, $description);
-            
+
             $block_row ++;
         }
-        
+
         // moved this block outside the loop, since it has nothing to do with the data
         {
             $worksheet->getColumnDimension('A')->setWidth(20);
@@ -432,7 +459,7 @@ class ExporterComponent extends Manager
             $worksheet->getColumnDimension('E')->setWidth(50);
             $worksheet->getColumnDimension('F')->setWidth(50);
             $worksheet->getColumnDimension('G')->setWidth(50);
-            
+
             $worksheet->getStyleByColumnAndRow($column, $block_row + 1)->applyFromArray($styleArray);
             $worksheet->getStyleByColumnAndRow($column1, $block_row + 1)->applyFromArray($styleArray);
             $worksheet->getStyleByColumnAndRow($column2, $block_row + 1)->applyFromArray($styleArray);
@@ -441,9 +468,9 @@ class ExporterComponent extends Manager
             $worksheet->getStyleByColumnAndRow($column2 + 3, $block_row + 1)->applyFromArray($styleArray);
             $worksheet->getStyleByColumnAndRow($column2 + 4, $block_row + 1)->applyFromArray($styleArray);
         }
-        
+
         // $i = 0;
-        
+
         foreach ($table as $entry)
         {
             // $i++;
@@ -452,15 +479,17 @@ class ExporterComponent extends Manager
             $worksheet->setCellValueByColumnAndRow($column1, $block_row, $entry[User::PROPERTY_USERNAME]);
             $worksheet->setCellValueByColumnAndRow($column2, $block_row, $entry[User::PROPERTY_LASTNAME]);
             $worksheet->setCellValueByColumnAndRow($column2 + 1, $block_row, $entry[User::PROPERTY_FIRSTNAME]);
-            $worksheet->setCellValueByColumnAndRow($column2 + 2, $block_row, $entry[User::PROPERTY_EMAIL]);
+            $worksheet->setCellValueByColumnAndRow($column2 + 2, $block_row, $entry[self::PROPERTY_SORT_NAME]);
+            $worksheet->setCellValueByColumnAndRow($column2 + 3, $block_row, $entry[User::PROPERTY_EMAIL]);
             $worksheet->setCellValueByColumnAndRow(
-                $column2 + 3, 
-                $block_row, 
-                $entry[CourseGroupUserRelation::PROPERTY_SUBSCRIPTION_TIME]);
-            $worksheet->setCellValueByColumnAndRow($column2 + 4, $block_row, $entry['Course Groups']);
+                $column2 + 4,
+                $block_row,
+                $entry[CourseGroupUserRelation::PROPERTY_SUBSCRIPTION_TIME]
+            );
+            $worksheet->setCellValueByColumnAndRow($column2 + 5, $block_row, $entry['Course Groups']);
             // if ($i == 1)
         }
-        
+
         // $block_row++;
         return $block_row;
     }
