ad80c561e906ee706718b68a446153fc28e92cce
vanpouckesven <sven.vanpoucke@hogent.be>
2019-11-28 15:43:33 +0100

    [Libraries] Webpack Tryout

--- src/Chamilo/Build/javascript/webpack.config.js
+++ src/Chamilo/Build/javascript/webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    vendorPath: path.resolve(__dirname, '../../../node_modules'),
     entry: {
-        'common.js': [
-            path.resolve(this.vendorPath, 'vue/dist/vue.common.prod.js'),
-            path.resolve(this.vendorPath, 'bootstrap-vue/dist/bootstrap-vue.js')
+        'common': [
+            path.resolve(__dirname, '../../../../node_modules/vue/dist/vue.common.prod.js'),
+            path.resolve(__dirname, '../../../../node_modules/bootstrap-vue/dist/bootstrap-vue.js')
         ]
-    }
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'common.js'
+    },
+    mode: 'production'
 };
