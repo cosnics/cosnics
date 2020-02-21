49be734c09b19dcd6f3f60266ab08803d12f642d
ScaraMcDuck <hans.de.bisschop@ehb.be>
2020-02-21 10:54:21 +0100

    [Libraries] Replacing some image-based icons by FontAwesome-based icons

--- src/Chamilo/Application/Weblcms/CourseType/Table/CourseType/CourseTypeTableCellRenderer.php
+++ src/Chamilo/Application/Weblcms/CourseType/Table/CourseType/CourseTypeTableCellRenderer.php
@@ -4,6 +4,7 @@ namespace Chamilo\Application\Weblcms\CourseType\Table\CourseType;
 use Chamilo\Application\Weblcms\CourseType\Manager;
 use Chamilo\Application\Weblcms\CourseType\Storage\DataClass\CourseType;
 use Chamilo\Application\Weblcms\CourseType\Storage\DataManager;
+use Chamilo\Libraries\Format\Structure\Glyph\FontAwesomeGlyph;
 use Chamilo\Libraries\Format\Structure\Toolbar;
 use Chamilo\Libraries\Format\Structure\ToolbarItem;
 use Chamilo\Libraries\Format\Table\Extension\DataClassTable\DataClassTableCellRenderer;
@@ -14,7 +15,7 @@ use Chamilo\Libraries\Utilities\Utilities;
 
 /**
  * This class describes the default cell renderer for the course type table
- * 
+ *
  * @package \application\weblcms\course_type
  * @author Yannick & Tristan
  * @author Sven Vanpoucke - Hogeschool Gent - Refactoring
@@ -27,10 +28,10 @@ class CourseTypeTableCellRenderer extends DataClassTableCellRenderer implements 
      * Inherited Functionality *
      * **************************************************************************************************************
      */
-    
+
     /**
      * Renders a cell for a given object
-     * 
+     *
      * @param $column \libraries\ObjectTableColumn
      *
      * @param $course_type CourseType
@@ -49,17 +50,19 @@ class CourseTypeTableCellRenderer extends DataClassTableCellRenderer implements 
                 {
                     $name_short = mb_substr($name_short, 0, 50) . '&hellip;';
                 }
+
                 return '<a href="' . $this->get_component()->get_view_course_type_url($course_type->get_id()) .
-                     '" title="' . htmlentities($name) . '">' . $name_short . '</a>';
-            
+                    '" title="' . htmlentities($name) . '">' . $name_short . '</a>';
+
             case CourseType::PROPERTY_DESCRIPTION :
                 $description = strip_tags(parent::render_cell($column, $course_type));
                 if (strlen($description) > 175)
                 {
                     $description = mb_substr($description, 0, 170) . '&hellip;';
                 }
+
                 return $description;
-            
+
             case CourseType::PROPERTY_ACTIVE :
                 if ($course_type->is_active())
                 {
@@ -70,13 +73,13 @@ class CourseTypeTableCellRenderer extends DataClassTableCellRenderer implements 
                     Return Translation::get('ConfirmFalse', null, Utilities::COMMON_LIBRARIES);
                 }
         }
-        
+
         return parent::render_cell($column, $course_type);
     }
 
     /**
      * Returns the actions toolbar
-     * 
+     *
      * @param $course_type CourseType
      *
      * @return String
@@ -84,98 +87,99 @@ class CourseTypeTableCellRenderer extends DataClassTableCellRenderer implements 
     public function get_actions($course_type)
     {
         $toolbar = new Toolbar(Toolbar::TYPE_HORIZONTAL);
-        
+
         if ($course_type->is_active())
         {
             $activation_translation = Translation::get('Deactivate', null, Utilities::COMMON_LIBRARIES);
-            $activation_image = Theme::getInstance()->getCommonImagePath('Action/Visible');
+            $glyph = new FontAwesomeGlyph('eye');
         }
         else
         {
             $activation_translation = Translation::get('Activate', null, Utilities::COMMON_LIBRARIES);
-            $activation_image = Theme::getInstance()->getCommonImagePath('Action/Invisible');
+            $glyph = new FontAwesomeGlyph('eye', array('text-muted'));
         }
-        
+
         $toolbar->add_item(
             new ToolbarItem(
-                $activation_translation, 
-                $activation_image, 
-                $this->get_component()->get_change_course_type_activation_url($course_type->get_id()), 
-                ToolbarItem::DISPLAY_ICON));
-        
+                $activation_translation, $glyph,
+                $this->get_component()->get_change_course_type_activation_url($course_type->get_id()),
+                ToolbarItem::DISPLAY_ICON
+            )
+        );
+
         $toolbar->add_item(
             new ToolbarItem(
-                Translation::get('Edit', null, Utilities::COMMON_LIBRARIES), 
-                Theme::getInstance()->getCommonImagePath('Action/Edit'), 
-                $this->get_component()->get_update_course_type_url($course_type->get_id()), 
-                ToolbarItem::DISPLAY_ICON));
-        
-        if (! DataManager::has_course_type_courses($course_type->get_id()))
+                Translation::get('Edit', null, Utilities::COMMON_LIBRARIES), new FontAwesomeGlyph('pencil'),
+                $this->get_component()->get_update_course_type_url($course_type->get_id()), ToolbarItem::DISPLAY_ICON
+            )
+        );
+
+        if (!DataManager::has_course_type_courses($course_type->get_id()))
         {
             $toolbar->add_item(
                 new ToolbarItem(
-                    Translation::get('Delete', null, Utilities::COMMON_LIBRARIES), 
-                    Theme::getInstance()->getCommonImagePath('Action/Delete'), 
-                    $this->get_component()->get_delete_course_type_url($course_type->get_id()), 
-                    ToolbarItem::DISPLAY_ICON, 
-                    true));
+                    Translation::get('Delete', null, Utilities::COMMON_LIBRARIES), new FontAwesomeGlyph('times'),
+                    $this->get_component()->get_delete_course_type_url($course_type->get_id()),
+                    ToolbarItem::DISPLAY_ICON, true
+                )
+            );
         }
         else
         {
             $toolbar->add_item(
                 new ToolbarItem(
-                    Translation::get('DeleteNA', null, Utilities::COMMON_LIBRARIES), 
-                    Theme::getInstance()->getCommonImagePath('Action/DeleteNa'), 
-                    null, 
-                    ToolbarItem::DISPLAY_ICON));
+                    Translation::get('DeleteNA', null, Utilities::COMMON_LIBRARIES),
+                    new FontAwesomeGlyph('times', array('text-muted')), null, ToolbarItem::DISPLAY_ICON
+                )
+            );
         }
-        
+
         $display_order = $course_type->get_display_order();
-        
+
         if ($display_order > 1)
         {
             $toolbar->add_item(
                 new ToolbarItem(
-                    Translation::get('MoveUp', null, Utilities::COMMON_LIBRARIES), 
-                    Theme::getInstance()->getCommonImagePath('Action/Up'), 
+                    Translation::get('MoveUp', null, Utilities::COMMON_LIBRARIES), new FontAwesomeGlyph('sort-up'),
                     $this->get_component()->get_move_course_type_url(
-                        $course_type->get_id(), 
-                        Manager::MOVE_DIRECTION_UP), 
-                    ToolbarItem::DISPLAY_ICON));
+                        $course_type->get_id(), Manager::MOVE_DIRECTION_UP
+                    ), ToolbarItem::DISPLAY_ICON
+                )
+            );
         }
         else
         {
             $toolbar->add_item(
                 new ToolbarItem(
-                    Translation::get('MoveUpNA', null, Utilities::COMMON_LIBRARIES), 
-                    Theme::getInstance()->getCommonImagePath('Action/UpNa'), 
-                    null, 
-                    ToolbarItem::DISPLAY_ICON));
+                    Translation::get('MoveUpNA', null, Utilities::COMMON_LIBRARIES),
+                    new FontAwesomeGlyph('sort-up', array('text-muted')), null, ToolbarItem::DISPLAY_ICON
+                )
+            );
         }
-        
+
         $max_objects = $this->get_table()->get_data_provider()->count_data();
-        
+
         if ($display_order < $max_objects)
         {
             $toolbar->add_item(
                 new ToolbarItem(
-                    Translation::get('MoveDown', null, Utilities::COMMON_LIBRARIES), 
-                    Theme::getInstance()->getCommonImagePath('Action/Down'), 
+                    Translation::get('MoveDown', null, Utilities::COMMON_LIBRARIES), new FontAwesomeGlyph('sort-down'),
                     $this->get_component()->get_move_course_type_url(
-                        $course_type->get_id(), 
-                        Manager::MOVE_DIRECTION_DOWN), 
-                    ToolbarItem::DISPLAY_ICON));
+                        $course_type->get_id(), Manager::MOVE_DIRECTION_DOWN
+                    ), ToolbarItem::DISPLAY_ICON
+                )
+            );
         }
         else
         {
             $toolbar->add_item(
                 new ToolbarItem(
-                    Translation::get('MoveDownNA', null, Utilities::COMMON_LIBRARIES), 
-                    Theme::getInstance()->getCommonImagePath('Action/DownNa'), 
-                    null, 
-                    ToolbarItem::DISPLAY_ICON));
+                    Translation::get('MoveDownNA', null, Utilities::COMMON_LIBRARIES),
+                    new FontAwesomeGlyph('sort-down', array('text-muted')), null, ToolbarItem::DISPLAY_ICON
+                )
+            );
         }
-        
+
         return $toolbar->as_html();
     }
 }
