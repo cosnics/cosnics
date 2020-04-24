ffc45e6911ab947b52f59d1921404dc0d181aa2d
Stefan Gabriels <stefan.gabriels@gmail.com>
2020-04-24 08:22:17 +0200

    Copy fields from js config object automatically as members of APIConfiguration

--- src/Chamilo/Core/Repository/ContentObject/Rubric/Resources/Source/src/Connector/APIConfiguration.ts
+++ src/Chamilo/Core/Repository/ContentObject/Rubric/Resources/Source/src/Connector/APIConfiguration.ts
@@ -1,26 +1,19 @@
 export default class APIConfiguration {
-    public addLevelURL : string = '';
-    public addTreeNodeURL: string = '';
-    public deleteLevelURL: string = '';
-    public deleteTreeNodeURL: string = '';
-    public moveLevelURL: string = '';
-    public moveTreeNodeURL: string = '';
-    public updateChoiceURL: string = '';
-    public updateLevelURL: string = '';
-    public updateTreeNodeURL: string = '';
+    public readonly addLevelURL = '';
+    public readonly addTreeNodeURL = '';
+    public readonly deleteLevelURL = '';
+    public readonly deleteTreeNodeURL = '';
+    public readonly moveLevelURL = '';
+    public readonly moveTreeNodeURL = '';
+    public readonly updateChoiceURL = '';
+    public readonly updateLevelURL = '';
+    public readonly updateTreeNodeURL = '';
 
-    public static fromJSON(config: any): APIConfiguration {
-        let apiConfiguration = new APIConfiguration();
-        apiConfiguration.addLevelURL = config.addLevelURL;
-        apiConfiguration.addTreeNodeURL = config.addTreeNodeURL;
-        apiConfiguration.deleteLevelURL = config.deleteLevelURL;
-        apiConfiguration.deleteTreeNodeURL = config.deleteTreeNodeURL;
-        apiConfiguration.moveLevelURL = config.moveLevelURL;
-        apiConfiguration.moveTreeNodeURL = config.moveTreeNodeURL;
-        apiConfiguration.updateChoiceURL = config.updateChoiceURL;
-        apiConfiguration.updateLevelURL = config.updateLevelURL;
-        apiConfiguration.updateTreeNodeURL = config.updateTreeNodeURL;
-
-        return apiConfiguration;
+    private constructor(config: Object) {
+        Object.assign(this, config);
     }
-}
+
+    public static fromJSON(config: Object) : APIConfiguration {
+        return new APIConfiguration(config);
+    }
+};
