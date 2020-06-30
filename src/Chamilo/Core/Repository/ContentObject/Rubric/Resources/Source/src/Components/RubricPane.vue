7584d8b0d95e984b1758a42542f0755a2adf77ff
Stefan Gabriels <stefan.gabriels@gmail.com>
2020-06-30 11:08:25 +0200

    Added in some future scroll handler code.

--- src/Chamilo/Core/Repository/ContentObject/Rubric/Resources/Source/src/Components/RubricPane.vue
+++ src/Chamilo/Core/Repository/ContentObject/Rubric/Resources/Source/src/Components/RubricPane.vue
@@ -99,6 +99,34 @@
             window.setTimeout(() => {
                 this.categoriesTransitionName = 'selected-fade';
             }, 100);
+
+            this.initScrollHandler();
+        }
+
+        initScrollHandler() {
+            /* // todo: Integrate this code, currently it's conflicting with the mouse handler in the template
+            const slider = this.$refs['cluster-content'];
+            if (!slider) { return; }
+            let startX: number;
+            let scrollLeft: number;
+            let isDown: boolean;
+            slider.addEventListener('mousedown', e => {
+                isDown = true;
+                startX = e.pageX - slider.offsetLeft;
+                scrollLeft = slider.scrollLeft;
+            });
+            slider.addEventListener('mouseleave', () => {
+                isDown = false;
+            });
+            slider.addEventListener('mouseup', () => {
+                isDown = false;
+            });
+            slider.addEventListener('mousemove', e => {
+                if (!isDown) return;
+                e.preventDefault();
+                const x = e.pageX - slider.offsetLeft;
+                slider.scrollLeft = scrollLeft - (x - startX);
+            }); */
         }
 
         get clusters() : Cluster[] {
