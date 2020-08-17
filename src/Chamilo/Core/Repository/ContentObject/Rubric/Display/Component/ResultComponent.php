1add167a3a4094c1cbc8cf7f269ed6398f324fc6
vanpouckesven <sven.vanpoucke@hogent.be>
2020-08-17 15:05:13 +0200

    [Rubric] Layout fixes + adding results from rubrics to result viewer

--- src/Chamilo/Core/Repository/ContentObject/Rubric/Display/Component/ResultComponent.php
+++ src/Chamilo/Core/Repository/ContentObject/Rubric/Display/Component/ResultComponent.php
@@ -3,6 +3,7 @@
 namespace Chamilo\Core\Repository\ContentObject\Rubric\Display\Component;
 
 use Chamilo\Core\Repository\ContentObject\Rubric\Display\Manager;
+use Chamilo\Core\Repository\ContentObject\Rubric\Service\RubricResultJSONGenerator;
 use Chamilo\Libraries\Architecture\Interfaces\DelegateComponent;
 use Chamilo\Core\Repository\ContentObject\Rubric\Storage\Entity\RubricData;
 use Chamilo\Core\Repository\ContentObject\Rubric\Storage\Entity\CategoryNode;
@@ -28,53 +29,27 @@ class ResultComponent extends Manager implements DelegateComponent
      */
     function run()
     {
-        $rubricData = new RubricData('Een tweede rubric', true);
-        $rubricData->setId(30);
-        $rubricData->setVersion(1);
-        $rubricData->getRootNode()->setId(40);
+        $rubric = $this->getRubric();
+        $rubricData = $this->getRubricService()->getRubric($rubric->getActiveRubricDataId());
 
-        $node1 = new ClusterNode('Cluster', $rubricData);
-        $node1->setId(38);
-
-        $node2 = new ClusterNode('Cluster 2', $rubricData);
-        $node2->setId(39);
-
-        $node3 = new CategoryNode('Category 1', $rubricData);
-        $node3->setId(41);
-        $node3->setColor('#FF8800');
-        $node1->addChild($node3);
-
-        $node4 = new CriteriumNode('Criterium 1', $rubricData);
-        $node4->setId(37);
-        $node3->addChild($node4);
-
-        $node5 = new CriteriumNode('Criterium 2', $rubricData);
-        $node5->setId(42);
-        $node3->addChild($node5);
-
-        $rubricData->getRootNode()->addChild($node1)->addChild($node2);
-
-        $level = new Level($rubricData);
-        $level->setId(4);
-        $level->setTitle('Good');
-        $level->setScore(10);
-
-        $level = new Level($rubricData);
-        $level->setId(5);
-        $level->setTitle('Medium');
-        $level->setScore(6);
-
-        $level = new Level($rubricData);
-        $level->setId(6);
-        $level->setTitle('Bad');
-        $level->setScore(2);
+        $results = $this->getRubricResultJSONGenerator()->generateRubricResultsJSON(
+            $rubricData, $this->getRubricBridge()->getContextIdentifier()
+        );
 
         return $this->getTwig()->render(
             'Chamilo\Core\Repository\ContentObject\Rubric:RubricResult.html.twig',
             [
                 'RUBRIC_DATA_JSON' => $this->getSerializer()->serialize($rubricData, 'json'),
-                'RUBRIC_RESULTS_JSON' => '[{"user":{"id":2,"name":"Sonia","role":"teacher"},"target_user":{"id":5,"name":"joske","role":"student"},"results":[{"tree_node_id":37,"level_id":4,"comment":"Dit is mijn feedback","score":10},{"tree_node_id":42,"level_id":5,"comment":"Nog meer feedback","score":6},{"tree_node_id":41,"level_id":null,"comment":null,"score":16},{"tree_node_id":38,"level_id":null,"comment":null,"score":16},{"tree_node_id":40,"level_id":null,"comment":null,"score":16}]}]'
+                'RUBRIC_RESULTS_JSON' => $this->getSerializer()->serialize($results, 'json')
             ]
         );
+    }
+
+    /**
+     * @return RubricResultJSONGenerator
+     */
+    protected function getRubricResultJSONGenerator()
+    {
+        return $this->getService(RubricResultJSONGenerator::class);
     }
 }
