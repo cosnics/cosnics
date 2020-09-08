e2880eec4cbeb7b7766ec4fae7d296adf5107f59
vanpouckesven <sven.vanpoucke@hogent.be>
2020-09-08 16:31:43 +0200

    [Rubric] Performance upgrades

--- src/Chamilo/Core/Repository/ContentObject/Rubric/Storage/Repository/RubricDataRepository.php
+++ src/Chamilo/Core/Repository/ContentObject/Rubric/Storage/Repository/RubricDataRepository.php
@@ -4,13 +4,17 @@ namespace Chamilo\Core\Repository\ContentObject\Rubric\Storage\Repository;
 
 use Chamilo\Core\Repository\ContentObject\Rubric\Domain\Exceptions\InvalidChildTypeException;
 use Chamilo\Core\Repository\ContentObject\Rubric\Storage\Entity\Choice;
+use Chamilo\Core\Repository\ContentObject\Rubric\Storage\Entity\CriteriumNode;
 use Chamilo\Core\Repository\ContentObject\Rubric\Storage\Entity\Level;
 use Chamilo\Core\Repository\ContentObject\Rubric\Storage\Entity\RubricData;
 use Chamilo\Core\Repository\ContentObject\Rubric\Storage\Entity\TreeNode;
 use Chamilo\Libraries\Storage\DataManager\Doctrine\ORM\CommonEntityRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\DBAL\LockMode;
+use Doctrine\DBAL\Logging\EchoSQLLogger;
+use Doctrine\ORM\PersistentCollection;
 use Doctrine\ORM\PessimisticLockException;
+use Doctrine\ORM\Query\Expr\Join;
 
 /**
  * Class RubricDataRepository
@@ -101,20 +105,21 @@ class RubricDataRepository extends CommonEntityRepository
      */
     public function findEntireRubricById(int $rubricDataId, int $expectedVersion = null)
     {
+        //$this->getEntityManager()->getConnection()->getConfiguration()->setSQLLogger(new EchoSQLLogger());
+
         $qb = $this->createQueryBuilder('rd')
             ->addSelect('tn')
             ->addSelect('rn')
-            ->addSelect('lv')
-            ->addSelect('ch')
+            ->addSelect('cn')
             ->join('rd.treeNodes', 'tn')
             ->join('rd.rootNode', 'rn')
-            ->leftJoin('rd.levels', 'lv')
-            ->leftJoin('rd.choices', 'ch')
+            ->leftJoin('tn.children', 'cn')
             ->where('rd.id = :id')
             ->orderBy('tn.depth')
             ->addOrderBy('tn.sort')
             ->setParameter('id', $rubricDataId);
 
+        /** @var RubricData $rubricData */
         $rubricData = $qb->getQuery()->getSingleResult();
 
         if(!is_null($expectedVersion))
@@ -128,6 +133,26 @@ class RubricDataRepository extends CommonEntityRepository
                 // The doc throws a pessimistic lock exception which is impossible, just catching it here so it doesn't go to the other docblocks
             }
         }
+
+        // preload for performance - voodoo magic
+        $rubricData->getLevels()[0];
+        $rubricData->getChoices()[0];
+
+        $choices = $rubricData->getChoices();
+        foreach($choices as $choice)
+        {
+            $criteriumChoices = $choice->getCriterium()->getChoices();
+
+            if($criteriumChoices instanceof PersistentCollection && !$criteriumChoices->isInitialized())
+            {
+                $criteriumChoices->setInitialized(true);
+                $criteriumChoices->clear();
+            }
+
+            $criteriumChoices->add($choice);
+        }
+        // end preload for performance - thank you doctrine for not caching on foreign keys and not being able to join on a subclass of an inheritance
+        // (maybe we should change the domain model?)
 
         return $rubricData;
     }
