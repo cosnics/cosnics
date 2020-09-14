8c43c2c42eadbb4cb76de49fbbdb646f5be5e22f
vanpouckesven <sven.vanpoucke@hogent.be>
2020-09-14 14:05:19 +0200

    [Rubrics] BETA warning + release prep

--- changes2020.sql
+++ changes2020.sql
@@ -1,3 +1,11 @@
 ALTER TABLE `repository_assignment` ADD `page_template` TEXT NULL AFTER `allowed_types`, ADD `last_entry_as_template` TINYINT(3) NOT NULL DEFAULT '0' AFTER `page_template`;
 ALTER TABLE `weblcms_platform_group_team` ADD `active` tinyint(3) NOT NULL DEFAULT '1'
 ALTER TABLE `weblcms_course_team_relation` ADD `active` tinyint(3) NOT NULL DEFAULT '1'
+
+CREATE TABLE `repository_assignment_rubric` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `assignment_id` int(11) NOT NULL,
+  `rubric_id` int(11) NOT NULL,
+  `self_evaluation_allowed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
