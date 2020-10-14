311ab4d396ab6fc4f07d5b7ded83786f557959a8
vanpouckesven <sven.vanpoucke@hogent.be>
2020-10-14 18:40:13 +0200

    Performance upgrades

--- src/Chamilo/Application/Weblcms/Integration/Chamilo/Core/Home/Type/FilteredCourseList.php
+++ src/Chamilo/Application/Weblcms/Integration/Chamilo/Core/Home/Type/FilteredCourseList.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Chamilo\Application\Weblcms\Integration\Chamilo\Core\Home\Type;
 
 use Chamilo\Application\Weblcms\CourseType\Storage\DataClass\CourseType;
@@ -7,6 +8,7 @@ use Chamilo\Application\Weblcms\Integration\Chamilo\Core\Home\Block;
 use Chamilo\Application\Weblcms\Service\CourseService;
 use Chamilo\Application\Weblcms\Service\CourseUserCategoryService;
 use Chamilo\Core\Home\Architecture\ConfigurableInterface;
+use Chamilo\Core\Home\Interfaces\AsynchronousBlockInterface;
 use Chamilo\Core\Home\Interfaces\StaticBlockTitleInterface;
 use Chamilo\Libraries\Architecture\Application\Application;
 use Chamilo\Libraries\File\Redirect;
@@ -14,14 +16,15 @@ use Chamilo\Libraries\Translation\Translation;
 
 /**
  * This class represents a block to show the course list filtered in a given course type and optionally a given category
- * 
+ *
  * @author Sven Vanpoucke - Hogeschool Gent
  */
-class FilteredCourseList extends Block implements ConfigurableInterface, StaticBlockTitleInterface
+class FilteredCourseList extends Block
+    implements ConfigurableInterface, StaticBlockTitleInterface, AsynchronousBlockInterface
 {
     const CONFIGURATION_SHOW_NEW_ICONS = 'show_new_icons';
     const CONFIGURATION_COURSE_TYPE = 'course_type';
-    
+
     /**
      * **************************************************************************************************************
      * Parameters *
@@ -34,17 +37,17 @@ class FilteredCourseList extends Block implements ConfigurableInterface, StaticB
      * Properties *
      * **************************************************************************************************************
      */
-    
+
     /**
      * The cached course type id
-     * 
+     *
      * @var int
      */
     private $courseTypeId;
 
     /**
      * The cached user course category id
-     * 
+     *
      * @var int
      */
     private $userCourseCategoryId;
@@ -58,18 +61,19 @@ class FilteredCourseList extends Block implements ConfigurableInterface, StaticB
      */
     public function getCourseListRenderer()
     {
-        if (! isset($this->courseListRenderer))
+        if (!isset($this->courseListRenderer))
         {
-            $this->courseListRenderer = new \Chamilo\Application\Weblcms\Renderer\CourseList\Type\FilteredCourseListRenderer(
-                $this, 
-                $this->getLinkTarget(), 
-                $this->getCourseTypeId(), 
-                $this->getUserCourseCategoryId(),
-                $this->getCourseService(),
-                $this->getCourseUserCategoryService()
-            );
+            $this->courseListRenderer =
+                new \Chamilo\Application\Weblcms\Renderer\CourseList\Type\FilteredCourseListRenderer(
+                    $this,
+                    $this->getLinkTarget(),
+                    $this->getCourseTypeId(),
+                    $this->getUserCourseCategoryId(),
+                    $this->getCourseService(),
+                    $this->getCourseUserCategoryService()
+                );
         }
-        
+
         return $this->courseListRenderer;
     }
 
@@ -80,7 +84,7 @@ class FilteredCourseList extends Block implements ConfigurableInterface, StaticB
     public function renderContentHeader()
     {
         return '<div class="portal-block-content portal-block-course-list' .
-             ($this->getBlock()->isVisible() ? '' : ' hidden') . '">';
+            ($this->getBlock()->isVisible() ? '' : ' hidden') . '">';
     }
 
     /**
@@ -90,59 +94,62 @@ class FilteredCourseList extends Block implements ConfigurableInterface, StaticB
     public function renderContentFooter()
     {
         $html = array();
-        
+
         $html[] = '</div>';
-        
-        if (! $this->getBlock()->getSetting(self::CONFIGURATION_SHOW_NEW_ICONS, true))
+
+        if (!$this->getBlock()->getSetting(self::CONFIGURATION_SHOW_NEW_ICONS, true))
         {
             $courseTypeLink = new Redirect(
                 array(
-                    Application::PARAM_CONTEXT => \Chamilo\Application\Weblcms\Manager::package(), 
-                    \Chamilo\Application\Weblcms\Renderer\CourseList\Type\CourseTypeCourseListRenderer::PARAM_SELECTED_COURSE_TYPE => $this->getCourseTypeId()));
-            
+                    Application::PARAM_CONTEXT => \Chamilo\Application\Weblcms\Manager::package(),
+                    \Chamilo\Application\Weblcms\Renderer\CourseList\Type\CourseTypeCourseListRenderer::PARAM_SELECTED_COURSE_TYPE => $this->getCourseTypeId(
+                    )
+                )
+            );
+
             $html[] = '<div class="panel-footer">';
             $html[] = Translation::get('CheckWhatsNew', array('URL' => $courseTypeLink->getUrl()));
             $html[] = '</div>';
         }
-        
+
         return implode(PHP_EOL, $html);
     }
 
     /**
      * Displays the content
-     * 
+     *
      * @return string
      */
     public function displayContent()
     {
         $this->loadSettings();
-        
+
         $renderer = $this->getCourseListRenderer();
-        
+
         if ($this->getBlock()->getSetting(self::CONFIGURATION_SHOW_NEW_ICONS, true))
         {
             $renderer->show_new_publication_icons();
         }
-        
+
         return $renderer->as_html();
     }
 
     /**
      * Returns the title of this block Changes the default title of the block to the title of the course type and
      * (optionally) the title of the selected user course category
-     * 
+     *
      * @return string
      */
     public function getTitle()
     {
         $this->loadSettings();
-        
+
         $course_type_id = $this->getCourseTypeId();
 
         if ($course_type_id > 0)
         {
             $course_type = CourseTypeDataManager::retrieve_by_id(CourseType::class_name(), $course_type_id);
-            
+
             if ($course_type)
             {
                 $course_type_title = $course_type->get_title();
@@ -152,29 +159,31 @@ class FilteredCourseList extends Block implements ConfigurableInterface, StaticB
                 return Translation::get('NoSuchCourseType');
             }
         }
-        elseif($course_type_id)
+        elseif ($course_type_id)
         {
             $course_type_title = Translation::get('AllCourses');
         }
-        else {
+        else
+        {
             $course_type_title = Translation::get('NoCourseType');
         }
-        
+
         $user_course_category_id = $this->getUserCourseCategoryId();
-        
+
         if ($user_course_category_id > 0)
         {
-            
+
             $course_user_category = \Chamilo\Application\Weblcms\Storage\DataManager::retrieve_by_id(
-                \Chamilo\Application\Weblcms\Storage\DataClass\CourseUserCategory::class_name(), 
-                $user_course_category_id);
-            
+                \Chamilo\Application\Weblcms\Storage\DataClass\CourseUserCategory::class_name(),
+                $user_course_category_id
+            );
+
             if ($course_user_category)
             {
                 $course_user_category_title = ' - ' . $course_user_category->get_title();
             }
         }
-        
+
         return $course_type_title . $course_user_category_title;
     }
 
@@ -183,10 +192,10 @@ class FilteredCourseList extends Block implements ConfigurableInterface, StaticB
      * Helper Functionality *
      * **************************************************************************************************************
      */
-    
+
     /**
      * Returns the selected course type id
-     * 
+     *
      * @return int
      */
     public function getCourseTypeId()
@@ -196,7 +205,7 @@ class FilteredCourseList extends Block implements ConfigurableInterface, StaticB
 
     /**
      * Returns the selected user course category id (if any)
-     * 
+     *
      * @return int
      */
     public function getUserCourseCategoryId()
@@ -211,13 +220,13 @@ class FilteredCourseList extends Block implements ConfigurableInterface, StaticB
     {
         $courseTypeIds = json_decode($this->getBlock()->getSetting(self::CONFIGURATION_COURSE_TYPE));
 
-        if (! is_array($courseTypeIds))
+        if (!is_array($courseTypeIds))
         {
             $courseTypeIds = array($courseTypeIds);
         }
-        
+
         $this->courseTypeId = $courseTypeIds[0];
-        
+
         // TODO: Fix this?
         $this->userCourseCategoryId = $courseTypeIds[1];
     }
