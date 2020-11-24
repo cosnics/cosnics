05bb827236c92406973f556861aeffd750289e23
vanpouckesven <sven.vanpoucke@hogent.be>
2020-11-24 11:23:56 +0100

    [CourseGroup] Randomizer updated to equally distribute in the created groups

--- src/Chamilo/Application/Weblcms/Tool/Implementation/CourseGroup/Infrastructure/Service/SubscriptionRandomizer.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/CourseGroup/Infrastructure/Service/SubscriptionRandomizer.php
@@ -5,6 +5,7 @@ namespace Chamilo\Application\Weblcms\Tool\Implementation\CourseGroup\Infrastruc
 use Chamilo\Application\Weblcms\Course\Storage\DataManager as CourseDataManager;
 use Chamilo\Application\Weblcms\Tool\Implementation\CourseGroup\Infrastructure\Service\CourseGroupDecorator\CourseGroupDecoratorsManager;
 use Chamilo\Application\Weblcms\Tool\Implementation\CourseGroup\Storage\DataClass\CourseGroup;
+use Chamilo\Application\Weblcms\Tool\Implementation\CourseGroup\Storage\DataManager;
 use Chamilo\Core\User\Storage\DataClass\User;
 
 /**
@@ -13,6 +14,11 @@ use Chamilo\Core\User\Storage\DataClass\User;
  * @package Chamilo\Application\Weblcms\Tool\Implementation\CourseGroup\Infrastructure\Service
  *
  * @author - Sven Vanpoucke - Hogeschool Gent
+ *
+ * TODO: refactor the randomization to optimize the process for multiple groups at once.
+ *  Retrieve all the possible users (without the already subscribed users) and divide them first between the available
+ *  groups (keep the already subscribed users into account so the groups are equally divided). Subscribe them after
+ *  the randomization instead of for each group
  */
 class SubscriptionRandomizer
 {
@@ -32,21 +38,43 @@ class SubscriptionRandomizer
     }
 
     /**
-     * @param CourseGroup $courseGroup
+     * @param array $courseGroups
      * @param CourseGroup $parentCourseGroup
      */
-    public function subscribeRandomUsersInCourseGroup(CourseGroup $courseGroup, CourseGroup $parentCourseGroup)
+    public function subscribeRandomUsersInCourseGroups(array $courseGroups, CourseGroup $parentCourseGroup)
+    {
+        $numberOfGroupsToRandomize = count($courseGroups);
+
+        foreach ($courseGroups as $courseGroup)
+        {
+            $this->subscribeRandomUsersInCourseGroup($courseGroup, $parentCourseGroup, $numberOfGroupsToRandomize);
+            DataManager::clear_course_group_users_cache();
+
+            // Each group that has been processed is a group less to randomize because the possible users array is
+            // recalculated
+            $numberOfGroupsToRandomize--;
+        }
+    }
+
+    /**
+     * @param CourseGroup $courseGroup
+     * @param CourseGroup $parentCourseGroup
+     * @param int $numberOfGroupsToRandomize
+     */
+    public function subscribeRandomUsersInCourseGroup(
+        CourseGroup $courseGroup, CourseGroup $parentCourseGroup, int $numberOfGroupsToRandomize = 1
+    )
     {
         $possibleUsers = $this->getPossibleUsers($courseGroup, $parentCourseGroup);
-        if(empty($possibleUsers))
+        if (empty($possibleUsers))
         {
             return;
         }
 
-        $usersToSubscribe = $this->selectRandomUsers($possibleUsers, $courseGroup);
+        $usersToSubscribe = $this->selectRandomUsers($possibleUsers, $courseGroup, $numberOfGroupsToRandomize);
         $courseGroup->subscribe_users($usersToSubscribe);
 
-        foreach($usersToSubscribe as $user)
+        foreach ($usersToSubscribe as $user)
         {
             $this->courseGroupDecoratorsManager->subscribeUser($courseGroup, $user);
         }
@@ -61,7 +89,7 @@ class SubscriptionRandomizer
     protected function getPossibleUsers(CourseGroup $courseGroup, CourseGroup $parentCourseGroup)
     {
         $numberOfParentMembers = $parentCourseGroup->count_members();
-        if($numberOfParentMembers == 0)
+        if ($numberOfParentMembers == 0)
         {
             $possibleUsers = $this->getAllCourseUsers($courseGroup->get_course_code());
         }
@@ -85,7 +113,7 @@ class SubscriptionRandomizer
         $parentCourseGroupMemberIds = $parentCourseGroup->get_members();
         $parentCourseGroupMembers = array();
 
-        foreach($parentCourseGroupMemberIds as $id)
+        foreach ($parentCourseGroupMemberIds as $id)
         {
             $userData = [User::PROPERTY_ID => $id];
             $parentCourseGroupMembers[$id] = new User($userData);
@@ -114,7 +142,7 @@ class SubscriptionRandomizer
         }
 
         return $courseUsers;
-}
+    }
 
     /**
      * @param CourseGroup $parentCourseGroup
@@ -122,7 +150,8 @@ class SubscriptionRandomizer
      *
      * @return User[]
      */
-    protected function filterPossibleUsersByCourseGroupAndSiblings(CourseGroup $parentCourseGroup, array $possibleUsers): array
+    protected function filterPossibleUsersByCourseGroupAndSiblings(CourseGroup $parentCourseGroup, array $possibleUsers
+    ): array
     {
         /** @var CourseGroup[] $childGroups */
         $childGroups = $parentCourseGroup->get_children(false)->as_array();
@@ -152,33 +181,39 @@ class SubscriptionRandomizer
         }
 
         return $possibleUsers;
-
     }
 
     /**
      * @param array $possibleUsers
      * @param CourseGroup $courseGroup
+     * @param int $numberOfGroupsToRandomize
      *
      * @return User[]
      */
-    protected function selectRandomUsers(array $possibleUsers, CourseGroup $courseGroup): array
+    protected function selectRandomUsers(
+        array $possibleUsers, CourseGroup $courseGroup, int $numberOfGroupsToRandomize = 1
+    ): array
     {
         $totalNumberOfPossibleUsers = count($possibleUsers);
+        $numberOfPossibleUsersPerGroup = $totalNumberOfPossibleUsers / $numberOfGroupsToRandomize;
+
+        // Determine the maximum users for this group based on the minimum value between the setting of the group
+        // and the maximum users that can be subscribed between the available groups that will be randomized.
+        // This method makes sure that the groups are equally randomized rather than filled until their max
+        $numberOfMaximumMembersSetting = $courseGroup->get_max_number_of_members();
+        $numberOfMaximumMembers = $numberOfMaximumMembersSetting == 0 ? $numberOfPossibleUsersPerGroup :
+            min($numberOfMaximumMembersSetting, $numberOfPossibleUsersPerGroup);
 
         // shuffle removes the indexes of the array and replaces them with simple numeric indexes so user id is no longer the key in the array
         shuffle($possibleUsers);
 
         $numberOfCurrentlySubscribedUsers = $courseGroup->count_members();
-        $numberOfMaximumMembers = $courseGroup->get_max_number_of_members();
         $numberOfUsersToSelect = $numberOfMaximumMembers - $numberOfCurrentlySubscribedUsers;
 
-        // Clamp this value to the maximum number of possible users due to issues with array_rand when asking for more items than there are available in the array
-        $clampedNumberOfUsersToSelect = min($numberOfUsersToSelect, $totalNumberOfPossibleUsers);
-
-        $randomUserIndexes = array_rand($possibleUsers, $clampedNumberOfUsersToSelect);
+        $randomUserIndexes = array_rand($possibleUsers, $numberOfUsersToSelect);
         $randomUsers = [];
 
-        foreach($randomUserIndexes as $randomUserIndex)
+        foreach ($randomUserIndexes as $randomUserIndex)
         {
             $randomUsers[] = $possibleUsers[$randomUserIndex];
         }
