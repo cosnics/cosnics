67d4a235cf37ea71da3fdf28dd5932f9e38a8e03
Stefan Gabriels <stefan.gabriels@gmail.com>
2021-08-17 09:58:44 +0200

    [Presence] Presence tool

---
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/Presence/Storage/DataClass/Publication.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Chamilo\Application\Weblcms\Tool\Implementation\Presence\Storage\DataClass;
+
+use Chamilo\Libraries\Storage\DataClass\DataClass;
+
+/**
+ * @package Chamilo\Application\Weblcms\Tool\Implementation\Presence\Storage\DataClass
+ *
+ * @author Sven Vanpoucke - Hogeschool Gent
+ */
+class Publication extends DataClass
+{
+    const PROPERTY_PUBLICATION_ID = 'publication_id';
+
+    /**
+     * Get the default properties
+     *
+     * @return array The property names.
+     */
+    public static function get_default_property_names($extended_property_names = array())
+    {
+        return parent::get_default_property_names(
+            array(
+                self::PROPERTY_PUBLICATION_ID
+            )
+        );
+    }
+
+    /**
+     * @return int
+     */
+    public function getPublicationId(): int
+    {
+        return $this->get_default_property(self::PROPERTY_PUBLICATION_ID);
+    }
+
+    /**
+     * @param int $publicationId
+     */
+    public function setPublicationId(int $publicationId)
+    {
+        $this->set_default_property(self::PROPERTY_PUBLICATION_ID, $publicationId);
+    }
+
+    /**
+     * Old method for the course copier
+     *
+     * @param int $publicationId
+     */
+    public function set_publication_id(int $publicationId)
+    {
+        $this->setPublicationId($publicationId);
+    }
+
+    public static function get_table_name()
+    {
+        return 'weblcms_presence_publication';
+    }
+}
+
