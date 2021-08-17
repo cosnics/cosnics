5d2a4799b1dcb87c88fbbd5d5ab96566a8d70c68
Stefan Gabriels <stefan.gabriels@gmail.com>
2021-08-17 09:56:22 +0200

    [Presence] Schema's for presence tables

--- src/Chamilo/Core/Repository/ContentObject/Presence/Form/PresenceForm.php
+++ src/Chamilo/Core/Repository/ContentObject/Presence/Form/PresenceForm.php
@@ -3,14 +3,15 @@ namespace Chamilo\Core\Repository\ContentObject\Presence\Form;
 
 use Chamilo\Core\Repository\ContentObject\Presence\Storage\DataClass\Presence;
 use Chamilo\Core\Repository\Form\ContentObjectForm;
-use Chamilo\Libraries\Format\Form\FormValidatorHtmlEditorOptions;
 
 /**
  *
- * @package repository.lib.content_object.presence
+ * @package repository.lib.content_object.evaluation
+ * @author Hans De Bisschop
+ * @author Dieter De Neef
  */
 /**
- * This class represents a form to create or update presences
+ * A form to create/update a presence
  */
 class PresenceForm extends ContentObjectForm
 {
@@ -21,24 +22,5 @@ class PresenceForm extends ContentObjectForm
         $object = new Presence();
         $this->set_content_object($object);
         return parent::create_content_object();
-    }
-
-    protected function build_creation_form($htmleditor_options = array(), $in_tab = false)
-    {
-        parent::build_creation_form($this->getHtmlEditorOptions(), $in_tab);
-    }
-
-    protected function build_editing_form($htmleditor_options = array(), $in_tab = false)
-    {
-        parent::build_editing_form($this->getHtmlEditorOptions(), $in_tab);
-    }
-
-    protected function getHtmlEditorOptions()
-    {
-        $htmleditor_options = array();
-
-        $htmleditor_options[FormValidatorHtmlEditorOptions::OPTION_HEIGHT] = '500';
-
-        return $htmleditor_options;
     }
 }
--- src/Chamilo/Core/Repository/ContentObject/Presence/Storage/DataClass/Presence.php
+++ src/Chamilo/Core/Repository/ContentObject/Presence/Storage/DataClass/Presence.php
@@ -3,7 +3,6 @@ namespace Chamilo\Core\Repository\ContentObject\Presence\Storage\DataClass;
 
 use Chamilo\Core\Repository\Storage\DataClass\ContentObject;
 use Chamilo\Libraries\Architecture\ClassnameUtilities;
-use Chamilo\Libraries\Architecture\Interfaces\AttachmentSupport;
 use Chamilo\Libraries\Architecture\Interfaces\Versionable;
 
 /**
@@ -13,11 +12,39 @@ use Chamilo\Libraries\Architecture\Interfaces\Versionable;
 /**
  * This class represents a presence
  */
-class Presence extends ContentObject implements Versionable, AttachmentSupport
+class Presence extends ContentObject implements Versionable
 {
+
+    const PROPERTY_OPTIONS = 'options';
 
     public static function get_type_name()
     {
         return ClassnameUtilities::getInstance()->getClassNameFromNamespace(self::class_name(), true);
+    }
+
+    public static function get_additional_property_names()
+    {
+        $propertyNames = parent::get_additional_property_names();
+        $propertyNames[] = self::PROPERTY_OPTIONS;
+        return $propertyNames;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOptions(): string
+    {
+        return $this->get_additional_property(self::PROPERTY_OPTIONS);
+    }
+
+    /**
+     * @param string $options
+     *
+     * @return $this
+     */
+    public function setOptions(string $options): Presence
+    {
+        $this->set_additional_property(self::PROPERTY_OPTIONS, $options);
+        return $this;
     }
 }
