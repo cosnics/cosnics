457c38ffd847c47c86aa5057f4c63f7a9bb3640f
Stefan Gabriels <stefan.gabriels@gmail.com>
2021-08-24 10:52:16 +0200

    [Presence] Removed unnecessary style rules

--- src/Chamilo/Core/Repository/ContentObject/Presence/Resources/Source/src/components/Main.vue
+++ src/Chamilo/Core/Repository/ContentObject/Presence/Resources/Source/src/components/Main.vue
@@ -852,62 +852,11 @@ export default class Main extends Vue {
     --text-color: var(--text-color-light);
     --selected-color: #{lighten($color, 55%)};
 }
-
 </style>
 
 <style>
-.popover {
-    top: 0;
-    left: 0;
-    z-index: 1060;
-    max-width: 276px;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 1.5;
-    text-align: left;
-    text-decoration: none;
-    text-shadow: none;
-    text-transform: none;
-    letter-spacing: normal;
-    word-break: normal;
-    word-spacing: normal;
-    white-space: normal;
-    line-break: auto;
-    font-size: .875rem;
-    word-wrap: break-word;
-    background-color: #fff;
-    background-clip: padding-box;
-    border: 1px solid rgba(0, 0, 0, .2);
-    border-radius: .3rem;
-}
-
-.popover, .popover .arrow {
-    position: absolute;
-    display: block;
-}
-
 .bs-popover-auto[x-placement^=right], .bs-popover-right {
     margin-left: .5rem;
-}
-
-.popover.b-popover {
-    display: block;
-    opacity: 1;
-    outline: 0;
-}
-
-.popover .arrow {
-    width: 1rem;
-    height: .5rem;
-    margin: 0 .3rem;
-}
-
-.bs-popover-auto[x-placement^=right] > .arrow, .bs-popover-right > .arrow {
-    left: calc(-.5rem - 1px);
-    width: .5rem;
-    height: 1rem;
-    margin: .3rem 0;
 }
 
 .bs-popover-auto[x-placement^=left], .bs-popover-left {
@@ -920,61 +869,5 @@ export default class Main extends Vue {
 
 .bs-popover-auto[x-placement^=top], .bs-popover-top {
     margin-bottom: .5rem;
-}
-
-.popover .arrow:after, .popover .arrow:before {
-    position: absolute;
-    display: block;
-    content: "";
-    border-color: transparent;
-    border-style: solid;
-}
-
-.bs-popover-auto[x-placement^=right] > .arrow:before, .bs-popover-right > .arrow:before {
-    left: 0;
-    border-width: .5rem .5rem .5rem 0;
-    border-right-color: rgba(0, 0, 0, .25);
-}
-
-.bs-popover-auto[x-placement^=right] > .arrow:after, .bs-popover-right > .arrow:after {
-    left: 1px;
-    border-width: .5rem .5rem .5rem 0;
-    border-right-color: #fff;
-}
-
-.bs-popover-auto[x-placement^=bottom] > .arrow:before, .bs-popover-bottom > .arrow:before {
-    top: 0;
-    border-width: 0 .5rem .5rem;
-    border-bottom-color: rgba(0, 0, 0, .25);
-}
-
-.bs-popover-auto[x-placement^=bottom] > .arrow:after, .bs-popover-bottom > .arrow:after {
-    top: 1px;
-    border-width: 0 .5rem .5rem;
-    border-bottom-color: #fff;
-}
-
-.bs-popover-auto[x-placement^=top] > .arrow:before, .bs-popover-top > .arrow:before {
-    bottom: 0;
-    border-width: .5rem .5rem 0;
-    border-top-color: rgba(0, 0, 0, .25);
-}
-
-.bs-popover-auto[x-placement^=top] > .arrow:after, .bs-popover-top > .arrow:after {
-    bottom: 1px;
-    border-width: .5rem .5rem 0;
-    border-top-color: #fff;
-}
-
-.bs-popover-auto[x-placement^=left] > .arrow:before, .bs-popover-left > .arrow:before {
-    right: 0;
-    border-width: .5rem 0 .5rem .5rem;
-    border-left-color: rgba(0, 0, 0, .25);
-}
-
-.bs-popover-auto[x-placement^=left] > .arrow:after, .bs-popover-left > .arrow:after {
-    right: 1px;
-    border-width: .5rem 0 .5rem .5rem;
-    border-left-color: #fff;
 }
 </style>
