8a21afeeea3e9c484a8d4880c9e4ddb7972dfd39
Stefan Gabriels <stefan.gabriels@gmail.com>
2021-09-01 09:03:56 +0200

    [Presence] Add the loader ellipsis

--- src/Chamilo/Core/Repository/ContentObject/Presence/Resources/Source/src/components/Main.vue
+++ src/Chamilo/Core/Repository/ContentObject/Presence/Resources/Source/src/components/Main.vue
@@ -464,6 +464,83 @@ export default class Main extends Vue {
     top: 8px;
     width: 1px;
 }
+
+.table.mod-entry + .lds-ellipsis {
+    display: none;
+    left: calc(50% - 20px);
+    position: absolute;
+    top: 40px;
+}
+
+.table.mod-entry[aria-busy=true] + .lds-ellipsis {
+    display: inline-block;
+}
+</style>
+
+<style>
+.lds-ellipsis {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+}
+
+.lds-ellipsis div {
+    position: absolute;
+    top: 13px;
+    width: 13px;
+    height: 13px;
+    border-radius: 50%;
+    background: hsla(190, 40%, 45%, 1);
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+
+.lds-ellipsis div:nth-child(1) {
+    left: 8px;
+    animation: lds-ellipsis1 0.6s infinite;
+}
+
+.lds-ellipsis div:nth-child(2) {
+    left: 8px;
+    animation: lds-ellipsis2 0.6s infinite;
+}
+
+.lds-ellipsis div:nth-child(3) {
+    left: 32px;
+    animation: lds-ellipsis2 0.6s infinite;
+}
+
+.lds-ellipsis div:nth-child(4) {
+    left: 56px;
+    animation: lds-ellipsis3 0.6s infinite;
+}
+
+@keyframes lds-ellipsis1 {
+    0% {
+        transform: scale(0);
+    }
+    100% {
+        transform: scale(1);
+    }
+}
+
+@keyframes lds-ellipsis3 {
+    0% {
+        transform: scale(1);
+    }
+    100% {
+        transform: scale(0);
+    }
+}
+
+@keyframes lds-ellipsis2 {
+    0% {
+        transform: translate(0, 0);
+    }
+    100% {
+        transform: translate(24px, 0);
+    }
+}
 </style>
 
 <style lang="scss">
