87a23f6ab51c4b4885644837d7f48c3069f5f5a4
stebanos <stefan.gabriels@hogent.be>
2022-06-15 11:27:21 +0200

    [GradeBook] sql updates

--- changes2022.sql
+++ changes2022.sql
@@ -1,3 +1,10 @@
 ALTER TABLE `repository_rubric_level` ADD COLUMN `criterium_id` int(11) DEFAULT NULL;
 ALTER TABLE `repository_rubric_result` ADD COLUMN `level_id` int(11) DEFAULT NULL;
 ALTER TABLE `repository_rubric_result` CHANGE `score` `score` INT(11) NULL;
+
+CREATE TABLE `repository_gradebook` (
+    `id` int(10) NOT NULL,
+    `active_gradebook_data_id` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `repository_gradebook` ADD PRIMARY KEY (`id`);
