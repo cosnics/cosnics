680983fed0827cf2dc7141c642bd9a39aeb1ee15
stebanos <stefan.gabriels@hogent.be>
2022-06-30 11:15:51 +0200

    [GradeBook] CSRF get

--- src/Chamilo/Core/Repository/ContentObject/GradeBook/Display/Ajax/Component/LoadGradeBookDataComponent.php
+++ src/Chamilo/Core/Repository/ContentObject/GradeBook/Display/Ajax/Component/LoadGradeBookDataComponent.php
@@ -3,13 +3,14 @@
 namespace Chamilo\Core\Repository\ContentObject\GradeBook\Display\Ajax\Component;
 
 use Chamilo\Core\Repository\ContentObject\GradeBook\Display\Ajax\Manager;
+use Chamilo\Libraries\Platform\Security\Csrf\CsrfComponentInterface;
 
 /**
  * @package Chamilo\Core\Repository\ContentObject\GradeBook\Display\Ajax\Component
  *
  * @author Stefan Gabriëls - Hogeschool Gent
  */
-class LoadGradeBookDataComponent extends Manager
+class LoadGradeBookDataComponent extends Manager implements CsrfComponentInterface
 {
     function runAjaxComponent()
     {
