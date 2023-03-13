04d75cdd22bf997700afbaa9c36577c98d482ebb
Hans De Bisschop <hans.de.bisschop@ehb.be>
2023-03-13 16:42:00 +0100

    [Repository] Implementing the new TableRenderers

--- src/Chamilo/Core/Repository/Table/ContentObjectTableRenderer.php
+++ src/Chamilo/Core/Repository/Table/ContentObjectTableRenderer.php
@@ -1,11 +1,15 @@
 <?php
 namespace Chamilo\Core\Repository\Table;
 
+use Chamilo\Core\Repository\Filter\FilterData;
 use Chamilo\Core\Repository\Manager;
 use Chamilo\Core\Repository\Storage\DataClass\ContentObject;
-use Chamilo\Core\Repository\Workspace\PersonalWorkspace;
+use Chamilo\Core\User\Service\UserService;
+use Chamilo\Libraries\Architecture\Application\Application;
 use Chamilo\Libraries\Architecture\Application\Routing\UrlGenerator;
+use Chamilo\Libraries\Architecture\Interfaces\Versionable;
 use Chamilo\Libraries\Format\Structure\Glyph\FontAwesomeGlyph;
+use Chamilo\Libraries\Format\Structure\Glyph\IdentGlyph;
 use Chamilo\Libraries\Format\Structure\Toolbar;
 use Chamilo\Libraries\Format\Table\Column\DataClassPropertyTableColumn;
 use Chamilo\Libraries\Format\Table\Column\StaticTableColumn;
@@ -17,6 +21,8 @@ use Chamilo\Libraries\Format\Table\Interfaces\TableRowActionsSupport;
 use Chamilo\Libraries\Format\Table\ListHtmlTableRenderer;
 use Chamilo\Libraries\Format\Table\Pager;
 use Chamilo\Libraries\Format\Table\TableResultPosition;
+use Chamilo\Libraries\Utilities\DatetimeUtilities;
+use Chamilo\Libraries\Utilities\StringUtilities;
 use Symfony\Component\Translation\Translator;
 
 /**
@@ -34,11 +40,24 @@ class ContentObjectTableRenderer extends DataClassListTableRenderer
 
     public const TABLE_IDENTIFIER = Manager::PARAM_CONTENT_OBJECT_ID;
 
+    protected StringUtilities $stringUtilities;
+
+    protected UserService $userService;
+
     public function __construct(
-        Translator $translator, UrlGenerator $urlGenerator, ListHtmlTableRenderer $htmlTableRenderer, Pager $pager
+        StringUtilities $stringUtilities, UserService $userService, Translator $translator, UrlGenerator $urlGenerator,
+        ListHtmlTableRenderer $htmlTableRenderer, Pager $pager
     )
     {
+        $this->stringUtilities = $stringUtilities;
+        $this->userService = $userService;
+
         parent::__construct($translator, $urlGenerator, $htmlTableRenderer, $pager);
+    }
+
+    public function getStringUtilities(): StringUtilities
+    {
+        return $this->stringUtilities;
     }
 
     public function getTableActions(): TableActions
@@ -49,6 +68,11 @@ class ContentObjectTableRenderer extends DataClassListTableRenderer
         $actions = new TableActions(__NAMESPACE__, self::TABLE_IDENTIFIER);
 
         return $actions;
+    }
+
+    public function getUserService(): UserService
+    {
+        return $this->userService;
     }
 
     protected function initializeColumns()
@@ -66,12 +90,9 @@ class ContentObjectTableRenderer extends DataClassListTableRenderer
             new DataClassPropertyTableColumn(ContentObject::class, ContentObject::PROPERTY_DESCRIPTION)
         );
 
-        if (!$this->get_component()->get_repository_browser()->getWorkspace() instanceof PersonalWorkspace)
-        {
-            $this->addColumn(
-                new DataClassPropertyTableColumn(ContentObject::class, ContentObject::PROPERTY_OWNER_ID)
-            );
-        }
+        $this->addColumn(
+            new DataClassPropertyTableColumn(ContentObject::class, ContentObject::PROPERTY_OWNER_ID)
+        );
 
         $this->addColumn(
             new DataClassPropertyTableColumn(ContentObject::class, ContentObject::PROPERTY_MODIFICATION_DATE)
@@ -83,16 +104,95 @@ class ContentObjectTableRenderer extends DataClassListTableRenderer
     }
 
     /**
-     * @param \Chamilo\Core\Repository\Workspace\Storage\DataClass\Workspace $workspace
+     * @param \Chamilo\Core\Repository\Storage\DataClass\ContentObject $contentObject
      *
      * @throws \Exception
      */
-    protected function renderCell(TableColumn $column, TableResultPosition $resultPosition, $workspace): string
+    protected function renderCell(TableColumn $column, TableResultPosition $resultPosition, $contentObject): string
     {
         $translator = $this->getTranslator();
         $urlGenerator = $this->getUrlGenerator();
+        $stringUtilities = $this->getStringUtilities();
 
-        return parent::renderCell($column, $resultPosition, $workspace);
+        switch ($column->get_name())
+        {
+            case self::PROPERTY_TYPE :
+                $image = $contentObject->get_icon_image(
+                    IdentGlyph::SIZE_MINI, true, ['fa-fw']
+                );
+
+                $typeUrl = $urlGenerator->fromParameters([
+                    Application::PARAM_CONTEXT => Manager::CONTEXT,
+                    Application::PARAM_ACTION => Manager::ACTION_BROWSE_CONTENT_OBJECTS,
+                    FilterData::FILTER_TYPE => $contentObject->get_template_registration_id()
+                ]);
+
+                return '<a href="' . htmlentities($typeUrl) . '" title="' .
+                    htmlentities($contentObject->get_type_string()) . '">' . $image . '</a>';
+            case ContentObject::PROPERTY_TITLE :
+                $title = parent::renderCell($column, $resultPosition, $contentObject);
+                $title_short = $stringUtilities->truncate($title, 50);
+
+                $viewUrl = $urlGenerator->fromParameters(
+                    [
+                        Application::PARAM_CONTEXT => Manager::CONTEXT,
+                        Application::PARAM_ACTION => Manager::ACTION_VIEW_CONTENT_OBJECTS,
+                        Manager::PARAM_CONTENT_OBJECT_ID => $contentObject->getId(),
+                        FilterData::FILTER_CATEGORY => $contentObject->get_parent_id()
+                    ]
+                );
+
+                return '<a href="' . htmlentities($viewUrl) . '" title="' . htmlentities($title) . '">' . $title_short .
+                    '</a>';
+            case ContentObject::PROPERTY_DESCRIPTION :
+                return $stringUtilities->truncate(
+                    html_entity_decode($contentObject->get_description()), 50
+                );
+            case ContentObject::PROPERTY_OWNER_ID :
+                return $this->getUserService()->getUserFullNameByIdentifier((string) $contentObject->get_owner_id());
+            case ContentObject::PROPERTY_CREATION_DATE :
+                return DatetimeUtilities::getInstance()->formatLocaleDate(
+                    $translator->trans('DateTimeFormatLong', [], StringUtilities::LIBRARIES),
+                    $contentObject->get_creation_date()
+                );
+            case ContentObject::PROPERTY_MODIFICATION_DATE :
+                return DatetimeUtilities::getInstance()->formatLocaleDate(
+                    $translator->trans('DateTimeFormatLong', [], StringUtilities::LIBRARIES),
+                    $contentObject->get_modification_date()
+                );
+            case self::PROPERTY_VERSION :
+                if ($contentObject instanceof Versionable)
+                {
+                    if ($contentObject->has_versions())
+                    {
+                        $number = $contentObject->get_version_count();
+                        $title = $translator->trans('VersionsAvailable', ['NUMBER' => $number], Manager::CONTEXT);
+                        $glyph = new FontAwesomeGlyph(
+                            'check', ['text-primary'], $title, 'fas'
+                        );
+                    }
+                    else
+                    {
+                        $title = $translator->trans('NoVersionsAvailable', [], Manager::CONTEXT);
+                        $glyph = new FontAwesomeGlyph(
+                            'check', ['text-muted'], $title, 'fas'
+                        );
+                    }
+
+                    return $glyph->render();
+                }
+                else
+                {
+                    $title = $translator->trans('NotVersionable', [], Manager::CONTEXT);
+                    $glyph = new FontAwesomeGlyph(
+                        'check', ['text-muted'], $title, 'fas'
+                    );
+
+                    return $glyph->render();
+                }
+        }
+
+        return parent::renderCell($column, $resultPosition, $contentObject);
     }
 
     /**
