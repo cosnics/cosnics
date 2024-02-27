c5e086f522849fa7dc2f066d277be1cd8c83ad61
Hans De Bisschop <hans.de.bisschop@ehb.be>
2024-02-27 16:11:25 +0100

    [Repository] Remove Bookmark content object

--- src/Chamilo/Core/Repository/ContentObject/Bookmark/Common/Rendition/HtmlRenditionImplementation.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Chamilo\Core\Repository\ContentObject\Bookmark\Common\Rendition;
-
-use Chamilo\Core\Repository\ContentObject\Bookmark\Common\RenditionImplementation;
-
-class HtmlRenditionImplementation extends RenditionImplementation
-{
-}
